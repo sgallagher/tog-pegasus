96d6b21da3456c9141f83c166d47f7879c0a9797
karl <karl>
2001-02-16 13:05:18 +0000

    Clean up and remove an extra include

--- src/Pegasus/Server/tests/MyProvider/MyProvider.cpp
+++ src/Pegasus/Server/tests/MyProvider/MyProvider.cpp
@@ -1,6 +1,45 @@
+//BEGIN_LICENSE
+//
+// Copyright (c) 2000 The Open Group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//END_LICENSE
+//BEGIN_HISTORY
+//
+// Author:
+//
+// $Log: MyProvider.cpp,v $
+// Revision 1.5  2001/02/16 13:05:18  karl
+// Clean up and remove an extra include
+//
+//
+//
+//END_HISTORY
+/* MyProvider Provider	- This provider was written as a simple test provider 
+to allow a first demonstration of the interfaces and of instances.  It uses a 
+completely artifical class today (Process) and creates a couple of new 
+instances on receipt of the instance requests.  It is not intended to ever 
+become a real provider.
+
+*/
 #include <iostream>
 #include <Pegasus/Common/Config.h>
-#include <Pegasus/Provider/CIMProvider.h>
+#include <Pegasus/Provider/Provider.h>
+
 
 using namespace std;
 
@@ -28,7 +67,7 @@ public:
 	Boolean includeClassOrigin = false,
 	const Array<String>& propertyList = _getStringArray())
     {
-	cout << "MyProvider::getInstance() called" << endl;
+	cout << "__NamespaceProvider::getInstance() called" << endl;
 
 	String tmp;
 	CIMReference::referenceToInstanceName(instanceName, tmp);

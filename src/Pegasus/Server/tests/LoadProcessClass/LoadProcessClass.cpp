65e76ab1bf54ce0a1988b9a3cda9c3245ed32f5f
mike <mike>
2001-02-18 19:00:55 +0000

    fixed overwrite of LoadProcessClass

--- src/Pegasus/Server/tests/LoadProcessClass/LoadProcessClass.cpp
+++ src/Pegasus/Server/tests/LoadProcessClass/LoadProcessClass.cpp
@@ -23,8 +23,8 @@
 // Author:
 //
 // $Log: LoadProcessClass.cpp,v $
-// Revision 1.6  2001/02/18 14:55:36  karl
-// Add __NameSpace Class creation
+// Revision 1.7  2001/02/18 19:00:55  mike
+// fixed overwrite of LoadProcessClass
 //
 // Revision 1.5  2001/02/16 18:17:11  mike
 // new
@@ -43,13 +43,12 @@
 
 #include <cassert>
 #include <Pegasus/Server/ProviderTable.h>
-#include <Pegasus/Repository/Repository.h>
+#include <Pegasus/Repository/CIMRepository.h>
 
 using namespace Pegasus;
 using namespace std;
 
 const char NAMESPACE[] = "root/cimv20";
-const char ROOTNAMESPACE[] = "root";
 
 int main(int argc, char** argv)
 {
@@ -61,7 +60,7 @@ int main(int argc, char** argv)
 
     try
     {
-	Repository r(argv[1]);
+	CIMRepository r(argv[1]);
 
 	try
 	{
@@ -83,38 +82,6 @@ int main(int argc, char** argv)
 	c.addProperty(CIMProperty("age", Uint32(0)));
 
 	r.createClass(NAMESPACE, c);
-
-
-	cout << "Created Process class with MyProvider Qualifier" <<endl;
-
-	// Create the __NameSpace Class in /root namespace
-	//delete the class if it exists
-	try
-	{
-	    r.deleteClass(ROOTNAMESPACE, "__Namespace");
-	}
-	catch (Exception&)
-	{
-	    // Ignore no such class error
-	}
-
-	// Now create the __Namespace class
-	// with name property key.
-	// Note that this requires qualifier definition in Root
-	
-	CIMClass cn("__Namespace");
-
-	cn.addQualifier(CIMQualifier("provider", "__NamespaceProvider"));
-	cn.addQualifier(CIMQualifier("Description", 
-				     "Namespace manipulation"));
-
-	cn.addProperty(CIMProperty("name", "")
-	    .addQualifier(CIMQualifier("key", true))
-	     .addQualifier(CIMQualifier("Description", 
-					"Namespace Name")));
-
-	r.createClass(ROOTNAMESPACE, cn);
-	cout << "Created __Namespace class with Provider Qualfier" << endl;
     }
     catch(Exception& e)
     {

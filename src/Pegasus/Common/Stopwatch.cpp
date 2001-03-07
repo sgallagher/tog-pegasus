720a17f03bde55e8fe3513ba25b0079b093f42e0
mike <mike>
2001-03-07 09:46:44 +0000

    Added stopwatch back but changed it to compile on Linux

--- src/Pegasus/Common/Stopwatch.cpp
+++ src/Pegasus/Common/Stopwatch.cpp
@@ -1,7 +1,10 @@
-#include <time.h>
-#include <windows.h>
 #include <iostream>
 #include "Stopwatch.h"
+
+#ifdef PEGASUS_OS_TYPE_WINDOWS
+# include <time.h>
+# include <windows.h>
+#endif
 
 PEGASUS_NAMESPACE_BEGIN
 
@@ -12,12 +15,20 @@ Stopwatch::Stopwatch()
 
 void Stopwatch::reset() 
 {
+#ifdef PEGASUS_OS_TYPE_WINDOWS
     _start = clock();
+#else
+    _start = 0;
+#endif
 }
 
 double Stopwatch::getElapsed() const
 {
+#ifdef PEGASUS_OS_TYPE_WINDOWS
     return (double)(clock() - (clock_t)_start) / CLOCKS_PER_SEC;
+#else
+    return 0.0;
+#endif
 }
 
 void Stopwatch::printElapsed()
@@ -27,7 +38,9 @@ void Stopwatch::printElapsed()
 
 void Stopwatch::sleep(double seconds)
 {
+#ifdef PEGASUS_OS_TYPE_WINDOWS
     Sleep(seconds * 1000);
+#endif
 }
 
 PEGASUS_NAMESPACE_END

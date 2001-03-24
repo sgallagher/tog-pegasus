dd3ab27fac7078b2a22a955b2b0337b28ac31f50
bob <bob>
2001-03-24 01:45:00 +0000

    Try to make this file cure itself in CVS

--- src/Pegasus/Compiler/parserExceptions.h
+++ src/Pegasus/Compiler/parserExceptions.h
@@ -1,32 +1,33 @@
-#ifndef _ParserExceptions_h
-#define _ParserExceptions_h
+// Exceptions that can occur in processing the cimmof compiler command line
+//
 
-#include <cstring>
-#include <cstdlib>
+#ifndef _PARSEREXCEPTION_H_
+#define _PARSEREXCEPTION_H_
 
-namespace ParserExceptions
-{
+// NOTE:  This exception does not use the Exception class from
+// Pegasus/Common because it has to do with the base class parser,
+// which should be reusable outside Pegasus.
 
-class ParserLexException
-{
-public:
+#include <string>
 
-    ParserLexException(const char* message)
-    {
-	_message = strcpy(new char[strlen(message) + 1], message);
-    }
+namespace ParserExceptions {
 
-    ~ParserLexException()
-    {
-	delete [] _message;
-    }
+class PEGASUS_COMPILER_LINKAGE ParserException {
+ private:
+  const string _msg;
+ public:
+  ParserException(const char *msg) : _msg(msg) {} ;
+  ParserException(const string &msg) :  _msg(msg) {};
+  const string &getMessage() const { return _msg; };
+};
 
-    const char* getMessage() const { return _message; }
-
-private:
-    char* _message;
+class PEGASUS_COMPILER_LINKAGE ParserLexException : public ParserException  {
+ public:
+  ParserLexException(const char *msg) : ParserException(msg) {};
+  ParserLexException(const string &msg) : ParserException(msg) {};
+  ~ParserLexException() {};
 };
 
 }
 
-#endif /* _ParserExceptions_h */
+#endif

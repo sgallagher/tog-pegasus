41e02431d0486b2edc8af9e11ae692814ecb437d
mike <mike>
2001-04-27 02:09:44 +0000

    new

---
+++ src/Pegasus/Repository/tests/InheritanceTree/InheritanceTree.cpp
@@ -0,0 +1,27 @@
+#include <Pegasus/Repository/InheritanceTree.h>
+
+using namespace std;
+using namespace Pegasus;
+
+int main()
+{
+    InheritanceTree it;
+
+    //
+    //        A
+    //      /   \
+    //     B     C
+    //   /   \     \
+    //  D     E     F
+    //
+
+    it.insert("D", "B");
+    it.insert("E", "B");
+    it.insert("B", "A");
+    it.insert("C", "A");
+    it.insert("F", "C");
+
+    it.print();
+
+    return 0;
+}

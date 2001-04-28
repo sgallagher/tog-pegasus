d3941c40c3bba2513f2c39f647360f2497316d17
mike <mike>
2001-04-28 00:00:08 +0000

    new

---
+++ src/utils/dog/dog.c
@@ -0,0 +1,13 @@
+
+/* This program runs in five seconds on a 1 GHZ Pentium III */
+
+int main()
+{
+    int i;
+    int j;
+    int k;
+
+    for (i = 0; i < 100000; i++)
+	for (j = 0; j < 10000; j++)
+	    ;
+}

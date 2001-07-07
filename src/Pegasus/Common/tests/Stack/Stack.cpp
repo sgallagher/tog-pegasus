63f078bb5746ec4615755b55c5387d469dcad71e
karl <karl>
2001-07-07 13:01:47 +0000

    Import Test code

---
+++ src/Pegasus/Common/tests/Stack/Stack.cpp
@@ -0,0 +1,91 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Karl Schopmeyer (k.schopmeyer@opengroup.org)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+
+#include <cstdlib>
+#include <cassert>
+#include <Pegasus/Common/Stack.h>
+#include <Pegasus/Common/Stopwatch.h>
+
+PEGASUS_USING_PEGASUS;
+PEGASUS_USING_STD;
+
+int main()
+{
+    try
+    {
+	// Simple test with Uint32 Stack of push, pop, top, and tests.
+	Stack<Uint32> s1;
+	assert (s1.isEmpty());
+
+	s1.push(1);
+	assert(s1.size() == 1);
+	assert (!s1.isEmpty());
+
+	s1.push(2);
+	assert(s1.size() == 2);
+	assert (!s1.isEmpty());
+
+	assert(s1.top() = 2);
+
+	s1.pop();
+	assert(s1.size() == 1);
+	assert (!s1.isEmpty());
+
+	s1.pop();
+	assert(s1.size() == 0);
+	assert (s1.isEmpty());
+
+	// Performance tests
+        
+	Stopwatch sw;
+    
+	Stack<Uint32> s2;
+	Uint32 stackSize = 1000000;
+    
+	for (Uint32 i = 0; i < stackSize; i++)
+	    s2.push(i);
+    
+	assert(s2.size() == stackSize);
+	for (Uint32 i = 0; i < stackSize; i++)
+	    s2.pop();
+    	cout << "Stack file and empty of " << stackSize << " integers in " << 
+		sw.getElapsed() << " Seconds" << endl;
+
+
+    }
+    catch (Exception& e)
+    {
+	cout << "Exception: " << e.getMessage() << endl;
+	exit(1);
+    }
+
+    cout << "+++++ passed all tests" << endl;
+
+    return 0;
+}

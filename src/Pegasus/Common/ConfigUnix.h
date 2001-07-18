46df537ecff24eca5427f2675f14872031ee03d4
kumpf <kumpf>
2001-07-18 22:23:46 +0000

    (Nitin Upasani - HP) Implementation of handleIndication() to deliver    indication by defining IndicationDispatcher, HandlerTable, CIMHandler    and ExportClient classes. the indication.
       Also implemented CIMxmlIndicationHandler and sendmailIndicationHandler to
       make the delivery of indications over Xml-HTTP and email respectively.
    
       Provided examples for HandlerTable, XmlIndicationDelivery and
       SendMailIndicationDelivery to support this implementation.

---
+++ src/Pegasus/Common/ConfigUnix.h
@@ -0,0 +1,54 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 BMC Software, Hewlett-Packard Company, IBM,
+// The Open Group, Tivoli Systems
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Mike Brasher (mbrasher@bmc.com)
+//
+// Modified By: 
+//         Nitin Upasani, Hewlett-Packard Company (Nitin_Upasani@hp.com)
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_ConfigUnix_h
+#define Pegasus_ConfigUnix_h
+
+#include <cstddef>
+
+#ifndef PEGASUS_OS_TYPE_UNIX
+#define PEGASUS_OS_TYPE_UNIX
+#endif
+#define PEGASUS_EXPORT /* empty */
+#define PEGASUS_IMPORT /* empty */
+#define PEGASUS_IOS_BINARY /* empty */
+#define PEGASUS_COMMON_LINKAGE /* empty */
+#define PEGASUS_REPOSITORY_LINKAGE /* empty */
+#define PEGASUS_PROTOCOL_LINKAGE /* empty */
+#define PEGASUS_SERVER_LINKAGE /* empty */
+#define PEGASUS_CLIENT_LINKAGE /* empty */
+#define PEGASUS_COMPILER_LINKAGE /* empty */
+#define PEGASUS_GETOOPT_LINKAGE /* empty */
+#define PEGASUS_PROVIDER_LINKAGE /* empty */
+#define PEGASUS_CMDLINE_LINKAGE /* empty */
+#define PEGASUS_HANDLER_LINKAGE /* empty */
+#define PEGASUS_EXPORTCLIENT_LINKAGE /* empty */
+
+#endif  /* Pegasus_ConfigUnix_h */

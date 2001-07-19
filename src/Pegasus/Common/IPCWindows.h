e5c1809253324f59905841fbe8b7f6d47a907a11
sage <sage>
2001-07-19 13:19:07 +0000

    Threading: Platform dependent definitions (empty and meaningless by now)

---
+++ src/Pegasus/Common/IPCWindows.h
@@ -0,0 +1,10 @@
+// Empty, meaningless structure to get IPC.cpp compiled on Windows
+#define PEGASUS_MUTEX_TYPE void *
+#define PEGASUS_COND_HANDLE void *
+#define PEGASUS_SEM_HANDLE void *
+#define PEGASUS_MUTEX_HANDLE void *
+#define PEGASUS_RWLOCK_HANDLE void *
+#define PEGASUS_CLEANUP_HANDLE void *
+#define PEGASUS_THREAD_RETURN void *
+#define PEGASUS_THREAD_CDECL
+#define PEGASUS_THREAD_HANDLE void *

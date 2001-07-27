08ac348e3042c3f852dd44994dd560dfe0417374
chip <chip>
2001-07-27 17:02:16 +0000

    moved CIMOMHandle

--- src/Pegasus/Provider/CIMProvider.h
+++ src/Pegasus/Provider/CIMProvider.h
@@ -1,131 +1,196 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%/////////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//==============================================================================
 //
-//////////////////////////////////////////////////////////////////////////
+// Author: Mike Brasher (mbrasher@bmc.com)
+//
+// Modified By:
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
-#ifndef Pegasus_CIMProvider_h
-#define Pegasus_CIMProvider_h
+#ifndef PegasusProvider_Provider_h
+#define PegasusProvider_Provider_h
 
 #include <Pegasus/Common/Config.h>
-#include <Pegasus/Common/OperationContext.h>
-#include <Pegasus/Provider/CIMOMHandle.h>
-#include <Pegasus/Provider/ProviderException.h>
-#include <Pegasus/Common/ResponseHandler.h>
-#include <Pegasus/Provider/Linkage.h>
+#include <Pegasus/Common/CIMOperations.h>
+#include <Pegasus/Common/CIMOMHandle.h>
 
 PEGASUS_NAMESPACE_BEGIN
 
-/**
-    CIMProvider is the base class for the provider interface.  A provider
-    must implement all the interfaces of the CIMProvider class and one or
-    more of its subclasses (such as CIMInstanceProvider).  If a provider
-    does not support any of the operations specified by the interfaces,
-    it may just throw a CIMNotSupportedException.
+class CIMRepository;
 
-    <p>Some of the parameters that are common to the provider operation
-    interfaces are:
-
-    <ul>
-        <li>The OperationContext parameter contains information about the
-        context for the specified operation, including the user name of the
-        client requesting the operation.  It is the provider's responsibility
-        to determine whether the user is authorized to perform the operation.
-        If the operation should not be permitted, the provider must throw
-        a CIMAccessDeniedException.</li>
-
-        <li>A CIMObjectPath specifies the CIM object on which the operation
-        is to be performed.  This parameter specifies the hostname, namespace,
-        classname, and key values that uniquely identify a CIM instance.</li>
-
-        <li>A ResponseHandler is a callback handle used by the provider to
-        deliver response data to the CIM Server.</li>
-    </ul>
-*/
-class PEGASUS_PROVIDER_LINKAGE CIMProvider
+class PEGASUS_PROVIDER_LINKAGE CIMProvider : public CIMOperations
 {
 public:
-    /**
-        Default constructor for the abstract CIMProvider class.
-    */
+
     CIMProvider();
 
-    /**
-        Destructs the CIMProvider instance.
-    */
     virtual ~CIMProvider();
 
+    virtual CIMClass getClass(
+        const String& nameSpace,
+        const String& className,
+        Boolean localOnly = true,
+        Boolean includeQualifiers = true,
+        Boolean includeClassOrigin = false,
+        const Array<String>& propertyList = EmptyStringArray());
+
+    virtual CIMInstance getInstance(
+	const String& nameSpace,
+	const CIMReference& instanceName,
+	Boolean localOnly = true,
+	Boolean includeQualifiers = false,
+	Boolean includeClassOrigin = false,
+	const Array<String>& propertyList = EmptyStringArray());
+
+    virtual void deleteClass(
+	const String& nameSpace,
+	const String& className);
+
+    virtual void deleteInstance(
+	const String& nameSpace,
+	const CIMReference& instanceName);
+
+    virtual void createClass(
+	const String& nameSpace,
+	const CIMClass& newClass);
+
+    virtual void createInstance(
+	const String& nameSpace,
+	const CIMInstance& newInstance);
+
+    virtual void modifyClass(
+	const String& nameSpace,
+	const CIMClass& modifiedClass);
+
+    virtual void modifyInstance(
+	const String& nameSpace,
+	const CIMInstance& modifiedInstance);
+
+    virtual Array<CIMClass> enumerateClasses(
+	const String& nameSpace,
+	const String& className = String::EMPTY,
+	Boolean deepInheritance = false,
+	Boolean localOnly = true,
+	Boolean includeQualifiers  = true,
+	Boolean includeClassOrigin = false);
+
+    virtual Array<String> enumerateClassNames(
+       const String& nameSpace,
+       const String& className = String::EMPTY,
+       Boolean deepInheritance = false);
+
+    virtual Array<CIMInstance> enumerateInstances(
+       const String& nameSpace,
+       const String& className,
+       Boolean deepInheritance = true,
+       Boolean localOnly = true,
+       Boolean includeQualifiers = false,
+       Boolean includeClassOrigin = false,
+       const Array<String>& propertyList = EmptyStringArray());
+
+    virtual Array<CIMReference> enumerateInstanceNames(
+       const String& nameSpace,
+       const String& className);
+
+    virtual Array<CIMInstance> execQuery(
+       const String& queryLanguage,
+       const String& query) ;
+
+    virtual Array<CIMObjectWithPath> associators(
+       const String& nameSpace,
+       const CIMReference& objectName,
+       const String& assocClass = String::EMPTY,
+       const String& resultClass = String::EMPTY,
+       const String& role = String::EMPTY,
+       const String& resultRole = String::EMPTY,
+       Boolean includeQualifiers = false,
+       Boolean includeClassOrigin = false,
+       const Array<String>& propertyList = EmptyStringArray());
+
+    virtual Array<CIMReference> associatorNames(
+       const String& nameSpace,
+       const CIMReference& objectName,
+       const String& assocClass = String::EMPTY,
+       const String& resultClass = String::EMPTY,
+       const String& role = String::EMPTY,
+       const String& resultRole = String::EMPTY);
+
+    virtual Array<CIMObjectWithPath> references(
+	const String& nameSpace,
+	const CIMReference& objectName,
+	const String& resultClass = String::EMPTY,
+	const String& role = String::EMPTY,
+	Boolean includeQualifiers = false,
+	Boolean includeClassOrigin = false,
+	const Array<String>& propertyList = EmptyStringArray());
+
+    virtual Array<CIMReference> referenceNames(
+       const String& nameSpace,
+       const CIMReference& objectName,
+       const String& resultClass = String::EMPTY,
+       const String& role = String::EMPTY);
+
+    virtual CIMValue getProperty(
+       const String& nameSpace,
+       const CIMReference& instanceName,
+       const String& propertyName);
+
+    virtual void setProperty(
+       const String& nameSpace,
+       const CIMReference& instanceName,
+       const String& propertyName,
+       const CIMValue& newValue = CIMValue());
+
+    virtual CIMQualifierDecl getQualifier(
+       const String& nameSpace,
+       const String& qualifierName);
+
+    virtual void setQualifier(
+       const String& nameSpace,
+       const CIMQualifierDecl& qualifierDecl);
+
+    virtual void deleteQualifier(
+       const String& nameSpace,
+       const String& qualifierName);
+
+    virtual Array<CIMQualifierDecl> enumerateQualifiers(
+       const String& nameSpace);
+
+    virtual CIMValue invokeMethod(
+       const String& nameSpace,
+       const CIMReference& instanceName,
+       const String& methodName,
+       const Array<CIMValue>& inParameters,
+       Array<CIMValue>& outParameters);
+
     /**
-        Initializes the provider.  This method is called each time the
-        provider is loaded and must complete before any of the other
-        provider methods (other than terminate) are called.
-
-        An exception thrown from this method indicates a provider
-        initialization failure which prevents it from processing operation
-        requests.
-
-        @param cimom Reserved for future use.
+       This method is invoked to initialize the provider. Eventually we
+       will pass a loca interface to the CIMOM, but for now we are just
+       passing the repository.
     */
-    virtual void initialize(CIMOMHandle & cimom) = 0;
 
-    /**
-        Performs any cleanup required before the provider is unloaded.
-        This method may be called by the CIM Server at any time, including
-        during initialization.  No other provider methods are called after
-        this method is invoked.
-
-        <p>Examples of cleanup a provider may perform in this method include:
-        <ul>
-            <li>Closing files and/or I/O streams</li>
-            <li>Releasing resources such as shared memory</li>
-            <li>Informing concurrently executing requests to complete
-                immediately (perhaps by setting a global flag)</li>
-            <li>Terminating subprocesses</li>
-        </ul>
-
-        <p>If the PegasusCreateProvider function dynamically instantiates the
-        provider with the "new" operator, then the terminate() method must
-        "delete" it:
-
-            <pre>
-            void MyProvider::terminate()
-            {
-                ...
-                delete this;
-            }
-            </pre>
-
-        <p>An exception thrown from this method is considered to be a provider
-        error and is ignored.
-    */
-    virtual void terminate() = 0;
+    virtual void initialize(CIMOMHandle& cimomHandle);
 };
 
 PEGASUS_NAMESPACE_END
 
-#endif
+#endif /* PegasusProvider_Provider_h */

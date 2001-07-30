3491f32a872a7a989ccc6a20f81b31b4a5e8c87f
mday <mday>
2001-07-30 12:06:49 +0000

    platform thread sources

---
+++ src/Pegasus/Common/ThreadUnix.cpp
@@ -0,0 +1,300 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Markus Mueller (sedgewick_de@yahoo.de)
+//
+// Modified By: Mike Day (mdday@us.ibm.com)
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+
+PEGASUS_NAMESPACE_BEGIN
+
+////////////////////////////////////////////////////////////////////////////
+
+SimpleThread::SimpleThread(
+    void * (*start) (void *), void * parameter, Boolean detached) :
+    _suspend()
+{
+    _start = start;
+    _thread_parm = parameter;
+    _is_detached = detached;
+    pthread_attr_init(&_handle.thatt);
+    _handle.thid = 0;
+}
+
+SimpleThread::~SimpleThread()
+{
+    if (!_is_detached)
+        pthread_join(_handle.thid,NULL);
+}
+
+void SimpleThread::run()
+{
+    if (_is_detached)
+        pthread_attr_setdetachstate(&_handle.thatt, PTHREAD_CREATE_DETACHED);
+    pthread_create(&_handle.thid, &_handle.thatt, _start, this);
+}
+
+Uint32 SimpleThread::threadId()
+{
+    return (Uint32) _handle.thid;
+}
+
+void * SimpleThread::get_parm(void)
+{
+    return _thread_parm;
+}
+
+void SimpleThread::kill(int signum)
+{
+    pthread_kill(_handle.thid, signum);
+}
+
+void SimpleThread::cancel()
+{
+    pthread_cancel(_handle.thid);
+}
+
+void SimpleThread::join(PEGASUS_THREAD_RETURN * ret_val)
+{
+    pthread_join(_handle.thid,ret_val);
+}
+
+void SimpleThread::test_cancel()
+{
+    pthread_testcancel();
+}
+
+void SimpleThread::thread_switch()
+{
+#ifdef PEGASUS_PLATFORM_HPUX_PARISC_ACC
+    sched_yield();
+#else
+    pthread_yield();
+#endif
+}
+
+//implemented later on using SIGUSR1
+void SimpleThread::suspend()
+{
+
+    //_suspend.wait();
+    pthread_kill(_handle.thid,SIGSTOP);
+}
+
+//implemented later on using SIGUSR2
+void SimpleThread::resume()
+{
+
+    //_suspend.signal();
+    pthread_kill(_handle.thid,SIGCONT);
+}
+
+void SimpleThread::sleep(Uint32 msec)
+{
+    struct timespec timeout;
+    timeout.tv_sec = msec / 1000;
+    timeout.tv_nsec = (msec & 1000) * 1000;
+    nanosleep(&timeout,NULL);
+}
+
+
+PEGASUS_THREAD_TYPE SimpleThread::self(void) { return(_handle.thid); }
+
+#ifdef PEGASUS_PLATFORM_HPUX_PARISC_ACC
+
+int pthread_mutex_timedlock(
+        pthread_mutex_t *mutex,
+        const struct timespec *abstime)
+{
+        return pthread_mutex_lock (mutex);
+}
+
+int pthread_rwlock_timedrdlock(
+        pthread_rwlock_t *rwlock,
+        const struct timespec *abstime)
+{
+        return pthread_rwlock_rdlock(rwlock);
+}
+
+int pthread_rwlock_timedwrlock(
+        pthread_rwlock_t *rwlock,
+        const struct timespec *abstime)
+{
+        return pthread_rwlock_wrlock(rwlock);
+}
+
+int sem_timedwait(sem_t *sem,
+        const struct timespec *abstime)
+{
+        return sem_wait(sem);
+}
+
+#endif
+
+Thread::Thread( PEGASUS_THREAD_RETURN (PEGASUS_THREAD_CDECL *start )(void *),
+		void *parameter, Boolean detached ) : _is_detached(detached), 
+	      _cancel_enabled(false), _cancelled(false),
+	      _suspend_count(), _start(start), 
+	      _tsd(), _thread_parm(parameter), _exit_code(0)
+{
+
+    pthread_attr_init(&_handle.thatt);
+    // set cancel state to enabled
+    // set cancel type to deferred 
+
+    _handle.thid = 0;
+}
+
+Thread::~Thread()
+{
+    if (!_is_detached)
+        pthread_join(_handle.thid,NULL);
+}
+
+void Thread::run()
+{
+    if (_is_detached)
+        pthread_attr_setdetachstate(&_handle.thatt, PTHREAD_CREATE_DETACHED);
+    pthread_create(&_handle.thid, &_handle.thatt, _start, this);
+}
+
+void * Thread::get_parm(void)
+{
+    return _thread_parm;
+}
+
+void Thread::kill(int signum)
+{
+    pthread_kill(_handle.thid, signum);
+}
+
+void Thread::cancel()
+{
+    pthread_cancel(_handle.thid);
+}
+
+void Thread::test_cancel()
+{
+    pthread_testcancel();
+}
+
+void Thread::thread_switch()
+{
+    pthread_yield();
+}
+
+void Thread::suspend()
+{
+}
+
+void Thread::resume()
+{
+}
+
+void Thread::sleep(Uint32 msec)
+{
+    struct timespec timeout;
+    timeout.tv_sec = msec / 1000;
+    timeout.tv_nsec = (msec & 1000) * 1000;
+    nanosleep(&timeout,NULL);
+}
+
+void Thread::join(void) { pthread_join(_handle.thid, &_exit_code) ; }
+
+
+
+// *****----- native cleanup routines -----***** //
+#if defined(PEGASUS_PLATFORM_LINUX_IX86_GNU)
+#define PEGASUS_THREAD_CLEANUP_NATIVE 
+void Thread::cleanup_push( void (*routine)(void *), void *parm) throw(IPCException)
+{
+  cleanup_handler *cu = new cleanup_handler(routine, parm);
+  try { _cleanup.insert_first(cu); } 
+  catch(IPCException& e) { delete cu; throw; }
+  _pthread_cleanup_push(&(cu->_cleanup_buffer), routine, parm);
+  return;
+}
+
+void Thread::cleanup_pop(Boolean execute = true) throw(IPCException)
+{
+  cleanup_handler *cu ;
+  try { cu = _cleanup.remove_first() ;}
+  catch(IPCException& e) { PEGASUS_ASSERT(0); }
+  _pthread_cleanup_pop(&(cu->_cleanup_buffer), execute);
+  delete cu;
+}
+
+
+#endif 
+
+
+#if defined(PEGASUS_PLATFORM_LINUX_IX86_GNU)
+#define PEGASUS_ATOMIC_INT_NATIVE // use native implementation
+
+AtomicInt::AtomicInt(): _rep(0) {}
+
+AtomicInt::AtomicInt( Uint32 initial) : _rep(initial) {}
+
+AtomicInt::~AtomicInt() {}
+
+AtomicInt::AtomicInt(const AtomicInt& original) 
+{
+  _rep = original._rep;
+}
+
+AtomicInt& AtomicInt::operator=( const AtomicInt& original)
+{
+  // don't worry about self-assignment in this implementation
+  if(this != &original)
+    _rep = original._rep;
+  return *this;
+}
+
+
+Uint32 AtomicInt::value(void)
+{
+  return((Uint32)_rep);
+}
+
+void AtomicInt::operator++(void) { _rep++; }
+
+void AtomicInt::operator--(void) { _rep--; }
+
+Uint32 AtomicInt::operator+(const AtomicInt& val) { return((Uint32)(_rep + val._rep));}
+Uint32 AtomicInt::operator+(Uint32 val) { return( (Uint32)(_rep + val)); }
+
+Uint32 AtomicInt::operator-(const AtomicInt& val) { return((Uint32)(_rep + val._rep));}
+Uint32 AtomicInt::operator-(Uint32 val) { return((Uint32)(_rep + val)); }
+
+AtomicInt& AtomicInt::operator+=(const AtomicInt& val) { _rep += val._rep; return *this; }
+AtomicInt& AtomicInt::operator+=(Uint32 val) { _rep += val; return *this; }
+
+AtomicInt& AtomicInt::operator-=(const AtomicInt& val) { _rep -= val._rep; return *this; }
+AtomicInt& AtomicInt::operator-=(Uint32 val) { _rep -= val; return *this; }
+
+
+#endif // linux x86 platform
+
+
+PEGASUS_NAMESPACE_END

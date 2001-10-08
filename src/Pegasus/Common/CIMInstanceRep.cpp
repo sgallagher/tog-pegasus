bf05ff95344e1b282b027fa99f88dd4c4b079f48
chip <chip>
2001-10-08 15:58:47 +0000

    ensure case is preserved in instance names.

--- src/Pegasus/Common/CIMInstanceRep.cpp
+++ src/Pegasus/Common/CIMInstanceRep.cpp
@@ -1,52 +1,44 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%/////////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Mike Brasher (mbrasher@bmc.com)
 //
-//////////////////////////////////////////////////////////////////////////
+// Modified By:
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
-#include "CIMInstanceRep.h"
 #include "CIMInstance.h"
-#include "CIMClassRep.h"
-#include "CIMScope.h"
 #include "DeclContext.h"
-#include "Resolver.h"
+#include "Indentor.h"
 #include "CIMName.h"
-#include "Constants.h"
-#include "StrLit.h"
-
-PEGASUS_USING_STD;
+#include "XmlWriter.h"
 
 PEGASUS_NAMESPACE_BEGIN
 
-CIMInstanceRep::CIMInstanceRep(const CIMObjectPath& reference)
-    : CIMObjectRep(reference)
+CIMInstanceRep::CIMInstanceRep(const String& className)
+    : _className(className), _resolved(false)
 {
-
+    if (!CIMName::legal(className))
+	throw IllegalName();
 }
 
 CIMInstanceRep::~CIMInstanceRep()
@@ -54,214 +46,346 @@ CIMInstanceRep::~CIMInstanceRep()
 
 }
 
+void CIMInstanceRep::addProperty(const CIMProperty& x)
+{
+    if (!x)
+	throw UnitializedHandle();
+
+    // Reject duplicate property names:
+
+    if (findProperty(x.getName()) != PEG_NOT_FOUND)
+	throw AlreadyExists();
+
+    // Note: class origin is resolved later:
+
+    // Append property:
+
+    _properties.append(x);
+}
+
+Uint32 CIMInstanceRep::findProperty(const String& name)
+{
+    for (Uint32 i = 0, n = _properties.size(); i < n; i++)
+    {
+	if (CIMName::equal(_properties[i].getName(), name))
+	    return i;
+    }
+
+    return PEG_NOT_FOUND;
+}
+
+Boolean CIMInstanceRep::existsProperty(const String& name)
+{
+    return (findProperty(name) != PEG_NOT_FOUND) ?
+		    true : false;
+}
+
+CIMProperty CIMInstanceRep::getProperty(Uint32 pos)
+{
+    if (pos >= _properties.size())
+	throw OutOfBounds();
+
+    return _properties[pos];
+}
+
+void CIMInstanceRep::removeProperty(Uint32 pos)
+    {
+	if (pos >= _properties.size())
+	    throw OutOfBounds();
+
+	_properties.remove(pos);
+    }
+
+
+Uint32 CIMInstanceRep::getPropertyCount() const
+{
+    return _properties.size();
+}
+
+
 void CIMInstanceRep::resolve(
     DeclContext* context,
-    const CIMNamespaceName& nameSpace,
-    CIMConstClass& cimClassOut,
-    Boolean propagateQualifiers)
+    const String& nameSpace,
+    CIMConstClass& cimClassOut)
 {
     // ATTN: Verify that references are initialized.
 
+#if 0
+    if (_resolved)
+	throw InstanceAlreadyResolved();
+#endif
+
     if (!context)
-        throw NullPointer();
+	throw NullPointer();
 
     //----------------------------------------------------------------------
     // First obtain the class:
     //----------------------------------------------------------------------
 
     CIMConstClass cimClass =
-        context->lookupClass(nameSpace, _reference.getClassName());
+	context->lookupClass(nameSpace, _className);
 
-    if (cimClass.isUninitialized())
-        throw PEGASUS_CIM_EXCEPTION(CIM_ERR_INVALID_CLASS,
-            _reference.getClassName().getString ());
+    if (!cimClass)
+	throw PEGASUS_CIM_EXCEPTION(CIM_ERR_INVALID_CLASS, _className);
 
     cimClassOut = cimClass;
+
+#if 0
+    if (!cimClass._rep->_resolved)
+	throw ClassNotResolved(_className);
+#endif
 
     //----------------------------------------------------------------------
     // Disallow instantiation of abstract classes.
     //----------------------------------------------------------------------
 
     if (cimClass.isAbstract())
-        throw InstantiatedAbstractClass(_reference.getClassName().getString ());
+	throw InstantiatedAbstractClass();
 
     //----------------------------------------------------------------------
-    // Validate and propagate qualifiers.
+    // Validate the qualifiers of this class:
     //----------------------------------------------------------------------
+
     _qualifiers.resolve(
-        context,
-        nameSpace,
-        (cimClass.isAssociation()) ? CIMScope::ASSOCIATION : CIMScope::CLASS,
-        false,
-        cimClass._rep->_qualifiers,
-        propagateQualifiers);
+	context,
+	nameSpace,
+	CIMScope::CLASS,
+	false,
+	cimClass._rep->_qualifiers);
 
     //----------------------------------------------------------------------
     // First iterate the properties of this instance and verify that
     // each one is defined in the class and then resolve each one.
+    // Also set the class origin.
     //----------------------------------------------------------------------
 
-    CIMName className = cimClass.getClassName();
+    String classOrigin = cimClass.getClassName();
 
     for (Uint32 i = 0, n = _properties.size(); i < n; i++)
     {
-        CIMProperty& property = _properties[i];
+	CIMProperty& property = _properties[i];
 
-        Uint32 index = cimClass.findProperty(property.getName());
+	Uint32 pos = cimClass.findProperty(property.getName());
 
-        if (index == PEG_NOT_FOUND)
-        {
-            //
-            //  Allow addition of Creator property to Indication Subscription,
-            //  Filter and Handler instances
-            //
-// l10n add language property support
-            if (!(((className.equal
-                    (CIMName (PEGASUS_CLASSNAME_INDSUBSCRIPTION))) ||
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_FORMATTEDINDSUBSCRIPTION))) ||
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_INDHANDLER_CIMXML))) ||
-#ifdef  PEGASUS_ENABLE_PROTOCOL_WSMAN
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_INDHANDLER_WSMAN))) ||
-#endif
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_LSTNRDST_CIMXML))) ||
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_INDHANDLER_SNMP))) ||
-#ifdef  PEGASUS_ENABLE_SYSTEM_LOG_HANDLER
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_LSTNRDST_SYSTEM_LOG))) ||
-#endif
-#ifdef  PEGASUS_ENABLE_EMAIL_HANDLER
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_LSTNRDST_EMAIL))) ||
-#endif
-                (className.equal
-                    (CIMName (PEGASUS_CLASSNAME_LSTNRDST_FILE))) ||
+	if (pos == PEG_NOT_FOUND)
+	    throw NoSuchProperty(property.getName());
 
-                (className.equal (CIMName (PEGASUS_CLASSNAME_INDFILTER)))) &&
-                ((property.getName ().equal
-                    (CIMName (PEGASUS_PROPERTYNAME_INDSUB_CREATOR))) ||
-                (property.getName ().equal
-                    (CIMName (PEGASUS_PROPERTYNAME_INDSUB_ACCEPTLANGS))) ||
-                (property.getName ().equal
-                    (CIMName (PEGASUS_PROPERTYNAME_LSTNRDST_CREATIONTIME))) ||
-                (property.getName ().equal
-                    (CIMName (PEGASUS_PROPERTYNAME_INDSUB_CONTENTLANGS))))))
-            {
-                throw NoSuchProperty(property.getName().getString ());
-            }
-        }
-        else
-        {
-            // resolve the property
-            Resolver::resolveProperty (property, context, nameSpace, true,
-                cimClass.getProperty (index), propagateQualifiers);
-        }
+	property.resolve(context, nameSpace, true, cimClass.getProperty(pos));
+        property.setClassOrigin(classOrigin);
     }
 
     //----------------------------------------------------------------------
     // Inject all properties from the class that are not included in the
     // instance. Copy over the class-origin and set the propagated flag
-    // to true. NOTE: The propagated flag indicates that the property
-    // was not part of the property set input with the create and
-    // was inherited from the default in the class (see cimxml spec sect 3.1.5)
+    // to true.
     //----------------------------------------------------------------------
 
     for (Uint32 i = 0, m = 0, n = cimClass.getPropertyCount(); i < n; i++)
     {
-        CIMConstProperty property = cimClass.getProperty(i);
-        const CIMName& name = property.getName();
+	CIMConstProperty property = cimClass.getProperty(i);
+	const String& name = property.getName();
 
-        // See if this instance already contains a property with this name:
+	// See if this instance already contains a property with this name:
 
-        Boolean found = false;
+	Boolean found = false;
 
-        for (Uint32 j = m, s = _properties.size(); j < s; j++)
-        {
-            if (name.equal(_properties[j].getName()))
-            {
-                found = true;
-                break;
-            }
-        }
+	for (Uint32 j = m, n = _properties.size(); j < n; j++)
+	{
+	    if (CIMName::equal(_properties[j].getName(), name))
+	    {
+		found = true;
+		break;
+	    }
+	}
 
-        if (!found)
-        {
-            CIMProperty p;
-            if (propagateQualifiers)
-            {
-                p = property.clone();
-            }
-            else
-            {
-                p = CIMProperty(
-                    property.getName(),
-                    property.getValue(),
-                    property.getArraySize(),
-                    property.getReferenceClassName(),
-                    property.getClassOrigin(),
-                    property.getPropagated());
-            }
-            p.setPropagated(true);
-            _properties.insert(m++, p);
-        }
+	if (!found)
+	{
+	    CIMProperty p = property.clone();
+	    p.setPropagated(true);
+	    _properties.insert(m++, p);
+	}
     }
+
+#if 0
+    _resolved = true;
+#endif
 }
 
-CIMInstanceRep::CIMInstanceRep(const CIMInstanceRep& x) : CIMObjectRep(x)
+CIMInstanceRep::CIMInstanceRep()
 {
+
 }
 
-CIMObjectPath CIMInstanceRep::buildPath(
+CIMInstanceRep::CIMInstanceRep(const CIMInstanceRep& x) :
+    Sharable(),
+    _className(x._className),
+    _resolved(x._resolved)
+{
+    x._qualifiers.cloneTo(_qualifiers);
+
+    _properties.reserve(x._properties.size());
+
+    for (Uint32 i = 0, n = x._properties.size(); i < n; i++)
+	_properties.append(x._properties[i].clone());
+}
+
+CIMInstanceRep& CIMInstanceRep::operator=(const CIMInstanceRep& x)
+{
+    return *this;
+}
+
+Boolean CIMInstanceRep::identical(const CIMInstanceRep* x) const
+{
+    if (_className != x->_className)
+	return false;
+
+    if (!_qualifiers.identical(x->_qualifiers))
+	return false;
+
+    // Compare properties:
+
+    {
+	const Array<CIMProperty>& tmp1 = _properties;
+	const Array<CIMProperty>& tmp2 = x->_properties;
+
+	if (tmp1.size() != tmp2.size())
+	    return false;
+
+	for (Uint32 i = 0, n = tmp1.size(); i < n; i++)
+	{
+	    if (!tmp1[i].identical(tmp2[i]))
+		return false;
+	}
+    }
+
+    if (_resolved != x->_resolved)
+	return false;
+
+    return true;
+}
+
+void CIMInstanceRep::toXml(Array<Sint8>& out) const
+{
+    // Class opening element:
+
+    out << "<INSTANCE ";
+    out << " CLASSNAME=\"" << _className << "\" ";
+    out << ">\n";
+
+    // Qualifiers:
+
+    _qualifiers.toXml(out);
+
+    // Parameters:
+
+    for (Uint32 i = 0, n = _properties.size(); i < n; i++)
+	_properties[i].toXml(out);
+
+    // Class closing element:
+
+    out << "</INSTANCE>\n";
+}
+
+void CIMInstanceRep::print(PEGASUS_STD(ostream) &os) const
+{
+    Array<Sint8> tmp;
+    toXml(tmp);
+    tmp.append('\0');
+    os << tmp.getData() << PEGASUS_STD(endl);
+}
+
+CIMReference CIMInstanceRep::getInstanceName(
     const CIMConstClass& cimClass) const
 {
     //--------------------------------------------------------------------------
     // Get class name:
     //--------------------------------------------------------------------------
 
-    CIMName className = getClassName();
+    String className = getClassName();
 
     //--------------------------------------------------------------------------
     // Get key names:
     //--------------------------------------------------------------------------
 
-    Array<CIMName> keyNames;
+    Array<String> keyNames;
     cimClass.getKeyNames(keyNames);
 
     if (keyNames.size() == 0)
-        return CIMObjectPath("", CIMNamespaceName(), className);
+	return CIMReference();
 
     //--------------------------------------------------------------------------
     // Get type and value for each key (building up key bindings):
     //--------------------------------------------------------------------------
 
-    Array<CIMKeyBinding> keyBindings;
+    KeyBindingArray keyBindings;
 
     for (Uint32 i = 0, n = keyNames.size(); i < n; i++)
     {
-        const CIMName& keyName = keyNames[i];
+	const String& keyName = keyNames[i];
 
-        Uint32 index = findProperty(keyName);
-        if (index == PEG_NOT_FOUND)
-        {
-            throw NoSuchProperty(keyName.getString());
-        }
+	Uint32 pos = findProperty(keyName);
+	PEGASUS_ASSERT(pos != PEG_NOT_FOUND);
 
-        CIMConstProperty tmp = getProperty(index);
-        keyBindings.append(CIMKeyBinding(keyName, tmp.getValue()));
+	CIMConstProperty tmp = getProperty(pos);
+
+	if (CIMName::equal(tmp.getName(), keyName))
+	{
+	    const CIMValue& value = tmp.getValue();
+
+	    // ATTN-A: for now just assert:
+	    if (value.isArray())
+		PEGASUS_ASSERT(false);
+
+	    CIMType type = value.getType();
+	    String valueStr;
+
+	    KeyBinding::Type kbType = KeyBinding::STRING;
+
+	    switch (type)
+	    {
+		case CIMType::BOOLEAN:
+		    kbType = KeyBinding::BOOLEAN;
+		    valueStr = value.toString();
+		    break;
+
+		case CIMType::UINT8:
+		case CIMType::SINT8:
+		case CIMType::UINT16:
+		case CIMType::SINT16:
+		case CIMType::UINT32:
+		case CIMType::SINT32:
+		case CIMType::UINT64:
+		case CIMType::SINT64:
+		case CIMType::CHAR16:
+		    kbType = KeyBinding::NUMERIC;
+		    valueStr = value.toString();
+		    break;
+
+		case CIMType::STRING:
+		case CIMType::DATETIME:
+		    kbType = KeyBinding::STRING;
+		    valueStr = value.toString();
+		    break;
+
+		case CIMType::REFERENCE:
+		{
+		    kbType = KeyBinding::STRING;
+
+		    CIMReference tmpRef = value.toString();
+		    valueStr = tmpRef.toString();
+		    break;
+		}
+
+		case CIMType::REAL32:
+		case CIMType::REAL64:
+		    PEGASUS_ASSERT(false);
+	    }
+
+	    keyBindings.append(KeyBinding(keyName, valueStr, kbType));
+	}
     }
 
-    return CIMObjectPath(String(), CIMNamespaceName(), className, keyBindings);
-}
-void CIMInstanceRep::filter(
-    Boolean,
-    Boolean,
-    const CIMPropertyList&)
-{
-    return;
+    return CIMReference(String(), String(), className, keyBindings);
 }
 
 PEGASUS_NAMESPACE_END

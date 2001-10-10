bb6506052f6bba0f9b08a29d7dd6d8295cb7b102
chip <chip>
2001-10-10 20:20:03 +0000

    update

---
+++ src/Providers/generic/OperatingSystemProvider/OperatingSystem.h
@@ -0,0 +1,87 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_OperatingSystem_h
+#define Pegasus_OperatingSystem_h
+
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/String.h>
+#include <Pegasus/Common/CIMDateTime.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+//
+// OperatingSystem
+//
+class OperatingSystem
+{
+public:
+   enum OSTYPE {
+      Unknown, Other, MACOS, ATTUNIX, DGUX, DECNT, Digital_Unix, OpenVMS, HPUX, AIX, MVS,
+      OS400, OS2, JavaVM, MSDOS, WIN3x, WIN95, WIN98, WINNT, WINCE, NCR3000, NetWare, OSF,
+      DCOS, Reliant_UNIX, SCO_UnixWare, SCO_OpenServer, Sequent, IRIX, Solaris, SunOS, U6000,
+      ASERIES, TandemNSK, TandemNT, BS2000, LINUX, Lynx, XENIX, VM_ESA, Interactive_UNIX,
+      BSDUNIX, FreeBSD, NetBSD, GNU_Hurd, OS9, MACH_Kernel, Inferno, QNX, EPOC, IxWorks,
+      VxWorks, MiNT, BeOS, HP_MPE, NextStep, PalmPilot, Rhapsody, Windows_2000, Dedicated,
+      OS390, VSE, TPF, Windows_Me
+   };
+
+public:
+   OperatingSystem(void);
+   virtual ~OperatingSystem(void);
+
+   String GetCSCreationClassName(void) const;
+   String GetCSName(void) const;
+   String GetCreationClassName(void) const;
+   String GetName(void) const;
+   Uint16 GetOSType(void) const;
+   String GetOtherTypeDescription(void) const;
+   String GetVersion(void) const;
+   CIMDateTime GetLastBootUpTime(void) const;
+   CIMDateTime GetLocalDateTime(void) const;
+   CIMDateTime GetInstallDate(void) const;
+   Sint16 GetCurrentTimeZone(void) const;
+   Uint32 GetNumberOfLicensedUsers(void) const;
+   Uint32 GetNumberOfUsers(void) const;
+   Uint32 GetNumberOfProcesses(void) const;
+   Uint32 GetMaxNumberOfProcesses(void) const;
+   Uint64 GetTotalSwapSpaceSize(void) const;
+   Uint64 GetTotalVirtualMemorySize(void) const;
+   Uint64 GetFreeVirtualMemory(void) const;
+   Uint64 GetFreePhysicalMemory(void) const;
+   Uint64 GetTotalVisibleMemorySize(void) const;
+   Uint64 GetSizeStoredInPagingFiles(void) const;
+   Uint64 GetFreeSpaceInPagingFiles(void) const;
+   Uint64 GetMaxProcessMemorySize(void) const;
+   Boolean GetDistributed(void) const;
+
+};
+
+PEGASUS_NAMESPACE_END
+
+#endif
---
+++ src/Providers/generic/OperatingSystemProvider/OperatingSystemProvider.cpp
@@ -0,0 +1,209 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#include <Pegasus/Common/Config.h>
+
+#include <Pegasus/Provider2/SimpleResponseHandler.cpp>
+
+#include "OperatingSystemProvider.h"
+#include "OperatingSystem.h"
+
+PEGASUS_NAMESPACE_BEGIN
+
+OperatingSystemProvider::OperatingSystemProvider(void)
+{
+}
+
+OperatingSystemProvider::~OperatingSystemProvider(void)	
+{
+}
+
+void OperatingSystemProvider::initialize(CIMOMHandle & cimom)
+{
+	_cimom = cimom;
+}
+
+void OperatingSystemProvider::terminate(void)
+{
+}
+
+void OperatingSystemProvider::getInstance(
+	const OperationContext & context,
+	const CIMReference & instanceReference,
+	const Uint32 flags,
+	const Array<String> & propertyList,
+	ResponseHandler<CIMInstance> & handler)
+{
+	handler.processing();
+
+	Array<CIMReference> cimReferences = _enumerateInstanceNames(context, instanceReference);
+	
+	for(Uint32 i = 0, n = cimReferences.size(); i < n; i++)
+	{
+		if(instanceReference == cimReferences[i])
+		{
+			Array<CIMInstance> cimInstances = _enumerateInstances(context, instanceReference);
+
+			handler.deliver(cimInstances[i]);
+
+			break;
+		}
+	}
+	
+	handler.complete();
+}
+
+void OperatingSystemProvider::enumerateInstances(
+	const OperationContext & context,
+	const CIMReference & classReference,
+	const Uint32 flags,
+	const Array<String> & propertyList,
+	ResponseHandler<CIMInstance> & handler)
+{
+	handler.processing();
+
+	OperatingSystem os;
+
+	CIMInstance cimInstance("Pegasus_OperatingSystem");
+
+	cimInstance.addProperty(CIMProperty("CSCreationClassName", os.GetCSCreationClassName()));
+	cimInstance.addProperty(CIMProperty("CSName", os.GetCSName()));
+	cimInstance.addProperty(CIMProperty("CreationClassName", os.GetCreationClassName()));
+	cimInstance.addProperty(CIMProperty("Name", os.GetName()));
+	cimInstance.addProperty(CIMProperty("OSType", os.GetOSType()));
+	cimInstance.addProperty(CIMProperty("OtherTypeDescription", os.GetOtherTypeDescription()));
+	cimInstance.addProperty(CIMProperty("CurrentTimeZone", os.GetCurrentTimeZone()));
+	cimInstance.addProperty(CIMProperty("LocalDateTime", os.GetLocalDateTime()));
+	//cimInstance.addProperty(CIMProperty("LastBootUpTime", os.GetLastBootUpTime()));
+	cimInstance.addProperty(CIMProperty("NumberOfLicensedUsers", os.GetNumberOfLicensedUsers()));
+	cimInstance.addProperty(CIMProperty("NumberOfUsers", os.GetNumberOfUsers()));
+	cimInstance.addProperty(CIMProperty("NumberOfProcesses", os.GetNumberOfProcesses()));
+	cimInstance.addProperty(CIMProperty("MaxNumberOfProcesses", os.GetMaxNumberOfProcesses()));
+	//cimInstance.addProperty(CIMProperty("InstallDate", os.GetInstallDate()));
+	cimInstance.addProperty(CIMProperty("Version", os.GetVersion()));
+	cimInstance.addProperty(CIMProperty("TotalSwapSpaceSize", os.GetTotalSwapSpaceSize()));
+	cimInstance.addProperty(CIMProperty("TotalVirtualMemorySize", os.GetTotalVirtualMemorySize()));
+	cimInstance.addProperty(CIMProperty("FreeVirtualMemory", os.GetFreeVirtualMemory()));
+	cimInstance.addProperty(CIMProperty("FreePhysicalMemory", os.GetFreePhysicalMemory()));
+	cimInstance.addProperty(CIMProperty("TotalVisibleMemorySize", os.GetTotalVisibleMemorySize()));
+	cimInstance.addProperty(CIMProperty("SizeStoredInPagingFiles", os.GetSizeStoredInPagingFiles()));
+	cimInstance.addProperty(CIMProperty("FreeSpaceInPagingFiles", os.GetFreeSpaceInPagingFiles()));
+	cimInstance.addProperty(CIMProperty("MaxProcessMemorySize", os.GetMaxProcessMemorySize()));
+	cimInstance.addProperty(CIMProperty("Distributed", os.GetDistributed()));
+
+	handler.deliver(cimInstance);
+
+	handler.complete();
+}
+
+void OperatingSystemProvider::enumerateInstanceNames(
+	const OperationContext & context,
+	const CIMReference & classReference,
+	ResponseHandler<CIMReference> & handler)
+{
+	// begin processing the request
+	handler.processing();
+
+	// get class definition from repository
+	CIMClass cimclass = _cimom.getClass(
+		OperationContext(),
+		classReference.getNameSpace(),
+		classReference.getClassName(),
+		false,
+		false,
+		false,
+		Array<String>());
+
+	Array<CIMInstance> cimInstances = _enumerateInstances(context, classReference);
+	
+	// convert instances to references;
+	for(Uint32 i = 0, n = cimInstances.size(); i < n; i++)
+	{
+		CIMReference instanceReference = cimInstances[i].getInstanceName(cimclass);
+
+		// ensure references are fully qualified
+		instanceReference.setHost(classReference.getHost());
+		instanceReference.setNameSpace(classReference.getNameSpace());
+
+		// deliver reference
+		handler.deliver(instanceReference);
+	}
+
+	// complete processing the request
+	handler.complete();
+}
+
+void OperatingSystemProvider::modifyInstance(
+	const OperationContext & context,
+	const CIMReference & instanceReference,
+	const CIMInstance & instanceObject,
+	ResponseHandler<CIMInstance> & handler)
+{
+	throw NotSupported("OperatingSystem::modifyInstance");
+}
+
+void OperatingSystemProvider::createInstance(
+	const OperationContext & context,
+	const CIMReference & instanceReference,
+	const CIMInstance & instanceObject,
+	ResponseHandler<CIMInstance> & handler)
+{
+	throw NotSupported("OperatingSystem::createInstance");
+}
+
+void OperatingSystemProvider::deleteInstance(
+	const OperationContext & context,
+	const CIMReference & instanceReference,
+	ResponseHandler<CIMInstance> & handler)
+{
+	throw NotSupported("OperatingSystem::deleteInstance");
+}
+
+Array<CIMInstance> OperatingSystemProvider::_enumerateInstances(
+	const OperationContext & context,
+	const CIMReference & classReference)
+{
+	SimpleResponseHandler<CIMInstance> handler;
+
+	enumerateInstances(context, classReference, 0xffffffff, Array<String>(), handler);
+
+	return(handler._objects);
+}
+
+Array<CIMReference> OperatingSystemProvider::_enumerateInstanceNames(
+	const OperationContext & context,
+	const CIMReference & classReference)
+{
+	SimpleResponseHandler<CIMReference> handler;
+
+	enumerateInstanceNames(context, classReference, handler);
+
+	return(handler._objects);
+}
+
+PEGASUS_NAMESPACE_END

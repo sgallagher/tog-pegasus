45c7c5f85e8c01b70beb7c35aa0e08a78a548d21
mike <mike>
2001-11-20 18:12:36 +0000

    Fixed error introduced while removing warnings.

--- src/Pegasus/Server/ProviderManager.cpp
+++ src/Pegasus/Server/ProviderManager.cpp
@@ -105,7 +105,8 @@ PEGASUS_THREAD_RETURN PEGASUS_THREAD_CDECL ProviderManager::monitorThread(void *
 
         // start with highest entry to prevent out-of-bounds
         // exception in case a removed entry - Markus
-        for(Uint32 i = _this->_providers.size()-1; i >= 0; i--)
+
+        for (Uint32 i = _this->_providers.size()-1; ; i--)
         {
             // get provider timeout
 
@@ -123,6 +124,9 @@ PEGASUS_THREAD_RETURN PEGASUS_THREAD_CDECL ProviderManager::monitorThread(void *
                 _this->_providers[i].getProvider()->terminate();
                 _this->_providers.remove(i);
             }
+
+	    if (i == 0)
+		break;
         }
     }
     

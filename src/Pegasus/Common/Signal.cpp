466e6cdbc5390850ba61851d2982cf0e7f939767
sage <sage>
2001-12-06 19:43:02 +0000

    Signal Handler: minor fix

--- src/Pegasus/Common/Signal.cpp
+++ src/Pegasus/Common/Signal.cpp
@@ -1,5 +1,32 @@
-#include "Signal.h"
+//%//-*-c++-*-/////////////////////////////////////////////////////////////////
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Markus Mueller (markus_mueller@de.ibm.com
+//
+// Modified By: 
+//
+//%/////////////////////////////////////////////////////////////////////////////
 
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/Signal.h>
 #include <Pegasus/Common/IPC.h>
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/Exception.h>
@@ -14,6 +41,10 @@ void sig_act(int s_n, siginfo_t * s_info, void * sig)
     printf(" in address %p\n", s_info->si_addr);
     printf(" pid %d\n", getpid());
     
+    // in general it is dangerous to call pthread call
+    // from within a signal handler, because
+    // they are not signal safe
+
     pthread_exit(retval);
 }
 

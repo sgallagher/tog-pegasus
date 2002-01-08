563313e9e96872213beea87ce63bc380fb3173cd
chip <chip>
2002-01-08 18:49:50 +0000

    changed includes from Provider2 to Provider

--- src/Pegasus/ExportServer/CIMExportRequestDispatcher.h
+++ src/Pegasus/ExportServer/CIMExportRequestDispatcher.h
@@ -2,20 +2,20 @@
 //
 // Copyright (c) 2000, 2001 BMC Software, Hewlett-Packard Company, IBM,
 // The Open Group, Tivoli Systems
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to 
-// deal in the Software without restriction, including without limitation the 
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
-// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
@@ -35,7 +35,7 @@
 #include <Pegasus/Common/CIMMessage.h>
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/CIMObject.h>
-#include <Pegasus/Provider2/CIMIndicationConsumer.h>
+#include <Pegasus/Provider/CIMIndicationConsumer.h>
 #include <Pegasus/ExportServer/Linkage.h>
 
 #include "ConsumerTable.h"
@@ -48,13 +48,13 @@ class HandlerTable;
 
 /** This class dispatches CIM export requests. For now it simply
 */
-class PEGASUS_EXPORT_SERVER_LINKAGE CIMExportRequestDispatcher 
+class PEGASUS_EXPORT_SERVER_LINKAGE CIMExportRequestDispatcher
     : public MessageQueue
 {
 public:
 
   typedef MessageQueue Base;
-  
+
     CIMExportRequestDispatcher(CIMRepository* repository);
 
     virtual ~CIMExportRequestDispatcher();
--- src/Pegasus/ExportServer/ConsumerTable.h
+++ src/Pegasus/ExportServer/ConsumerTable.h
@@ -4,18 +4,18 @@
 // The Open Group, Tivoli Systems
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to 
-// deal in the Software without restriction, including without limitation the 
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
-// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
@@ -33,7 +33,7 @@
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/Array.h>
 #include <Pegasus/Common/Exception.h>
-#include <Pegasus/Provider2/CIMIndicationConsumer.h>
+#include <Pegasus/Provider/CIMIndicationConsumer.h>
 #include <Pegasus/ExportServer/Linkage.h>
 
 PEGASUS_NAMESPACE_BEGIN
@@ -43,14 +43,14 @@ class CreateIndicationConsumerReturnedNull : public Exception
 public:
 
     CreateIndicationConsumerReturnedNull(
-	const String& libName, 
+	const String& libName,
 	const String& funcName)
 	: Exception(funcName + " returned null in library " + libName) { }
 };
- 
-// The consumer table maintains a list of consumers which have been 
-// dynamically loaded. It maintains a mapping between string 
-// consumer identifiers and consumers. CIMOM Listner will use the 
+
+// The consumer table maintains a list of consumers which have been
+// dynamically loaded. It maintains a mapping between string
+// consumer identifiers and consumers. CIMOM Listner will use the
 // consumer table to find consumer for the purposes of request dispatching.
 
 class PEGASUS_EXPORT_SERVER_LINKAGE ConsumerTable

e7dc7de8ec34def95c39b32d821f99ab1b8beaec
chip <chip>
2002-01-08 18:22:45 +0000

    defined CIMConstObject constructors for CIMConstClass and CIMConstInstance (they were already declared, but not defined).

--- src/Pegasus/Common/CIMObject.cpp
+++ src/Pegasus/Common/CIMObject.cpp
@@ -3,18 +3,18 @@
 // Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to 
-// deal in the Software without restriction, including without limitation the 
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
-// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
-// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
-// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
@@ -87,7 +87,17 @@ CIMConstObject::CIMConstObject(const CIMClass& x)
     Inc(_rep = x._rep);
 }
 
+CIMConstObject::CIMConstObject(const CIMConstClass& x)
+{
+    Inc(_rep = x._rep);
+}
+
 CIMConstObject::CIMConstObject(const CIMInstance& x)
+{
+    Inc(_rep = x._rep);
+}
+
+CIMConstObject::CIMConstObject(const CIMConstInstance& x)
 {
     Inc(_rep = x._rep);
 }
@@ -145,7 +155,7 @@ CIMObjectWithPath::CIMObjectWithPath()
 
 CIMObjectWithPath::CIMObjectWithPath(
     const CIMReference& reference,
-    const CIMObject& object) 
+    const CIMObject& object)
     : _reference(reference), _object(object)
 {
 
@@ -173,7 +183,7 @@ CIMObjectWithPath& CIMObjectWithPath::operator=(const CIMObjectWithPath& x)
 }
 
 void CIMObjectWithPath::set(
-    const CIMReference& reference, 
+    const CIMReference& reference,
     const CIMObject& object)
 {
     _reference = reference;

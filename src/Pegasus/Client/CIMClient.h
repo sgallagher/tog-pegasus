37d3afc79da1fcc22234c2b2ff6a803ce201e0da
karl <karl>
2002-01-30 22:32:47 +0000

    fix up documentaiton

--- src/Pegasus/Client/CIMClient.h
+++ src/Pegasus/Client/CIMClient.h
@@ -61,7 +61,28 @@ public:
 
     enum { DEFAULT_TIMEOUT_MILLISECONDS = 20000 };
 
-    ///
+    /** Constructor for a CIM Client object.
+    @param monitor Defines the monitor object to be used with this
+    CIMClient object.
+    @param httpConnector defines the connector object to use with
+    this CIMClient object. The connector defines TBD.
+    @param timeOutMilliseconds Defines the number of milliseconds
+    of inactivity before the connection will timeout
+    
+    <PRE>
+        Monitor* monitor = new Monitor;
+        HTTPConnector* connector;
+        connector = new HTTPConnector(monitor);
+
+        CIMClient client(monitor, connector, 60 * 1000);
+
+        char * connection = connectionList[i].allocateCString();
+        cout << "connecting to " << connection << endl;
+        client.connect(connection);
+    
+    </PRE>
+    @exception TBD
+    */
     CIMClient(
 	Monitor* monitor,
 	HTTPConnector* httpConnector,
@@ -70,19 +91,22 @@ public:
     ///
     ~CIMClient();
 
-    ///
+    /**  TBD
+    */
     virtual void handleEnqueue();
 
     /** Returns the queue name. */
     virtual const char* getQueueName() const;
 
-    ///
+    /** TBD
+    */
     Uint32 getTimeOut() const
     {
 	return _timeOutMilliseconds;
     }
 
-    ///
+    /** Sets the timeout in milliseconds for the CIMClient.
+    */
     void setTimeOut(Uint32 timeOutMilliseconds)
     {
 	_timeOutMilliseconds = timeOutMilliseconds;
@@ -98,17 +122,94 @@ public:
         @exception CannotCreateSocket
         @exception CannotConnect
         @exception UnexpectedFailure
+        <PRE>
+            TBD
+        </PRE>
     */
     void connect(const String& address);
 
-    //
-    // Connection used by local clients
-    //
+    /** connectLocal - Creates connection to the server for
+        Local clients. The connectLocal differs from the connect function
+        in that the userName is supplied directly with the call.
+        Once the authentication characteristics are set, this
+        function uses connect() to make the connection.
+        @param address - String defining the URL of the server
+        to which the client should connect.
+        @param userName - user name to be used for this connection.
+        @return - No return defined. Failure to connect throws an exception.
+        @SeeAlso connect - The exceptions are defined in connect.
+    */
     void connectLocal(
         const String& address,
         const String& userName = String::EMPTY);
 
-    ///
+    /** The <TT>getClass</TT> method  executes a CIM operation that returns a single CIM Class from the
+	target Namespace where the ClassName input parameter defines the name of
+	the class to be retrieved.
+
+	@param nameSpace The <TT>nameSpace</TT> parameter is a string that
+	defines the target Namespace.
+	See defintion of \URL[Namespace]{DefinitionofTerms.html#NAMESPACE}.
+
+	@param className The <TT>className</TT> input parameter defines the name
+	of the Class to be retrieved.
+
+	@param localOnly If the <TT>localOnly</TT> input parameter is true, this
+	specifies that only CIM Elements (properties, methods and qualifiers)
+	overriden within the definition of the Class are returned.  If false,
+	all elements are returned.  This parameter therefore effects a CIM
+	Server-side mechanism to filter certain elements of the returned object
+	based on whether or not they have been propagated from the parent Class
+	(as defined by the PROPAGATED attribute).
+
+	@param includeQualifiers If the <TT>includeQualifiers</TT> input
+	parameter is true, this specifies that all Qualifiers for that Class
+	(including Qualifiers on the Class and on any returned Properties,
+	Methods or CIMMethod Parameters) MUST be included as <QUALIFIER>
+	elements in the response.  If false no <QUALIFIER> elements are
+	present in the returned Class.
+
+	@param includeClassOrigin If the <TT>includeClassOrigin</TT> input
+	parameter is true, this specifies that the CLASSORIGIN attribute MUST be
+	present on all appropriate elements in the returned Class. If false, no
+	CLASSORIGIN attributes are present in the returned Class.
+
+	@param propertyList If the <TT>propertyList</TT> input parameter is not
+	NULL, the members of the array define one or more CIMProperty names. The
+	returned Class MUST NOT include elements for any Properties missing from
+	this list. Note that if LocalOnly is specified as true this acts as an
+	additional filter on the set of Properties returned (for example, if
+	CIMProperty A is included in the PropertyList but LocalOnly is set to
+	true and A is not local to the requested Class, then it will not be
+	included in the response). If the PropertyList input parameter is an
+	empty array this signifies that no Properties are included in the
+	response. If the PropertyList input parameter is NULL this specifies
+	that all Properties (subject to the conditions expressed by the other
+	parameters) are included in the response.
+
+	If the <TT>propertyList</TT> contains duplicate elements, the Server
+	MUST ignore the duplicates but otherwise process the request normally.
+	If the PropertyList contains elements which are invalid CIMProperty
+	names for the target Class, the Server MUST ignore such entries but
+	otherwise process the request normally.
+
+	@return If successful, the return value is a single CIM Class.
+
+	If unsuccessful, one of the following status codes MUST be returned by
+	this method, where the first applicable error in the list (starting with
+	the first element of the list, and working down) is the error returned.
+	Any additional method-specific interpretation of the error in is given
+	in parentheses.
+	<UL>
+		<LI>CIM_ERR_ACCESS_DENIED
+		<LI>CIM_ERR_INVALID_NAMESPACE
+		<LI>CIM_ERR_INVALID_PARAMETER (including missing,
+		duplicate,unrecognized or otherwise incorrect parameters)
+		<LI>CIM_ERR_NOT_FOUND (the request CIM Class does not exist in
+		the specified namespace)
+		<LI>CIM_ERR_FAILED (some other unspecified error occurred)</LI>
+	</UL>
+    */
     virtual CIMClass getClass(
 	const String& nameSpace,
 	const String& className,
@@ -117,7 +218,8 @@ public:
 	Boolean includeClassOrigin = false,
 	const CIMPropertyList& propertyList = CIMPropertyList());
 
-    ///
+    /** ATTN TBD
+    */
     virtual CIMInstance getInstance(
 	const String& nameSpace,
 	const CIMReference& instanceName,
@@ -126,17 +228,20 @@ public:
 	Boolean includeClassOrigin = false,
 	const CIMPropertyList& propertyList = CIMPropertyList());
 
-    ///
+    /** ATTN TBD
+    */
     virtual void deleteClass(
 	const String& nameSpace,
 	const String& className);
 
-    ///
+    /** ATTN TBD
+    */ 
     virtual void deleteInstance(
 	const String& nameSpace,
 	const CIMReference& instanceName);
 
-    ///
+    /** ATTN TBD
+    */
     virtual void createClass(
 	const String& nameSpace,
 	const CIMClass& newClass);

30e8780b435e773895311c5842908702b550d6bd
sage <sage>
2002-01-31 11:30:06 +0000

    zOS threading support

---
+++ src/Pegasus/Common/IPCzOS.h
@@ -0,0 +1,222 @@
+//%////////////-*-c++-*-///////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Markus Mueller (sedgewick_de@yahoo.de)
+//
+// Modified By: Mike Day (mdday@us.ibm.com)
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef IPC_UNIX_include
+#define IPC_UNIX_include
+
+//#include <sched.h>
+//#include <semaphore.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <time.h>
+//#include <sys/timex.h>
+#include <unistd.h>
+
+#define SEM_VALUE_MAX 0xffffffff
+
+#undef PEGASUS_NEED_CRITICAL_TYPE
+
+PEGASUS_NAMESPACE_BEGIN
+
+#ifdef PEGASUS_NEED_CRITICAL_TYPE
+typedef pthread_spinlock_t PEGASUS_CRIT_TYPE;
+#endif
+
+//typedef sem_t PEGASUS_SEMAPHORE_TYPE;
+//typedef pthread_t PEGASUS_THREAD_TYPE;
+typedef PEGASUS_UINT64 PEGASUS_THREAD_TYPE;
+typedef pthread_mutex_t PEGASUS_MUTEX_TYPE;
+
+typedef struct {
+    Uint32 waiters;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    PEGASUS_THREAD_TYPE owner;
+} PEGASUS_SEM_HANDLE ;
+
+typedef struct {
+    pthread_mutex_t mut;
+    pthread_mutexattr_t mutatt;
+    PEGASUS_THREAD_TYPE owner;
+} PEGASUS_MUTEX_HANDLE ;
+
+struct _pthread_cleanup_buffer {
+    void (*__routine) (void *);             /* Function to call.  */
+    void *__arg;                            /* Its argument.  */
+    int __canceltype;                       /* Saved cancellation type. */
+    struct _pthread_cleanup_buffer *__prev;  /* Chaining of cleanup functions.*/
+};
+typedef struct _pthread_cleanup_buffer  PEGASUS_CLEANUP_HANDLE;
+
+typedef void *PEGASUS_THREAD_RETURN;
+
+#define PEGASUS_THREAD_CDECL
+
+typedef struct {
+    PEGASUS_THREAD_TYPE thid;
+    pthread_attr_t thatt;
+} PEGASUS_THREAD_HANDLE ;
+
+//-----------------------------------------------------------------
+/// Conditionals to support native or generic Conditional Semaphore
+//-----------------------------------------------------------------
+
+#define PEGASUS_CONDITIONAL_NATIVE = 1
+
+typedef pthread_cond_t PEGASUS_COND_TYPE;
+
+typedef struct {
+    pthread_cond_t cond;
+    PEGASUS_THREAD_TYPE owner;
+} PEGASUS_COND_HANDLE;
+
+//-----------------------------------------------------------------
+/// Conditionals to support native or generic atomic variables
+//-----------------------------------------------------------------
+
+
+//-----------------------------------------------------------------
+/// Conditionals to support native or generic read/write semaphores
+//-----------------------------------------------------------------
+
+
+//#if defined(PEGASUS_PLATFORM_LINUX_IX86_GNU) 
+// #define PEGASUS_READWRITE_NATIVE = 1
+
+// typedef struct {
+//     pthread_rwlock_t rwlock;
+//     pthread_t owner;
+// } PEGASUS_RWLOCK_HANDLE;
+
+//#endif // linux platform read/write type
+
+
+
+inline void pegasus_yield(void)
+{
+      pthread_yield(NULL);
+}
+
+// pthreads cancellation calls 
+inline void disable_cancel(void)
+{
+   pthread_setintr(PTHREAD_INTR_DISABLE);
+}
+
+inline void enable_cancel(void)
+{
+   pthread_setintr(PTHREAD_INTR_ENABLE);
+}
+
+inline void pegasus_sleep(int msec)
+{
+#if 0
+   struct timespec wait;
+   wait.tv_sec = msec / 1000;
+   msec -= wait.tv_sec * 1000;
+   wait.tv_nsec =  (msec & 1000) * 1000000;
+   nanosleep(&wait, NULL);
+#endif
+   usleep(1000*msec);
+}
+
+#ifdef PEGASUS_NEED_CRITICAL_TYPE
+
+inline void init_crit(PEGASUS_CRIT_TYPE *crit)
+{
+   pthread_spin_init(crit, 0);
+}
+
+inline void enter_crit(PEGASUS_CRIT_TYPE *crit)
+{
+   pthread_spin_lock(crit);
+}
+
+inline void try_crit(PEGASUS_CRIT_TYPE *crit)
+{
+   pthread_spin_trylock(crit);
+}
+
+inline void exit_crit(PEGASUS_CRIT_TYPE *crit)
+{
+   pthread_spin_unlock(crit);
+}
+
+inline void destroy_crit(PEGASUS_CRIT_TYPE *crit)
+{
+   pthread_spin_destroy(crit);
+}
+
+#endif /* PEGASUS_NEED_CRITICAL_TYPE */
+
+//-----------------------------------------------------------------
+/// accurate version of gettimeofday for unix systems
+//  posix glibc implementation does not return microseconds.
+//  mdday Wed Aug  1 16:05:26 2001
+//-----------------------------------------------------------------
+#if 0
+inline static int pegasus_gettimeofday(struct timeval *tv)
+{
+   struct ntptimeval ntp;
+   int err;
+   if(tv == NULL)
+      return(EINVAL);
+   err = ntp_gettime(&ntp);
+   tv->tv_sec = ntp.time.tv_sec;
+   tv->tv_usec = ntp.time.tv_usec;
+   return(err);
+}
+#endif
+inline static int pegasus_gettimeofday(struct timeval *tv)
+{
+   return gettimeofday(tv,NULL);
+}
+   
+inline void exit_thread(PEGASUS_THREAD_RETURN rc)
+{
+  pthread_exit(rc);
+}
+
+inline PEGASUS_THREAD_TYPE pegasus_thread_self(void) 
+{ 
+   pthread_t pt = pthread_self();
+   return (* ((PEGASUS_THREAD_TYPE *) &pt));
+}
+
+inline void destroy_thread(PEGASUS_THREAD_TYPE th, PEGASUS_THREAD_RETURN rc)
+{
+   pthread_cancel(*(pthread_t *) &th);
+
+}
+
+
+PEGASUS_NAMESPACE_END
+
+#endif // IPCzOSInclude

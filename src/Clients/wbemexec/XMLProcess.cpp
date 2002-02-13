0ba6b404f7cd3defb1d08dfe7bf535e30bb3719e
sage <sage>
2002-02-13 18:44:34 +0000

    missing Config.h

--- src/Clients/wbemexec/XMLProcess.cpp
+++ src/Clients/wbemexec/XMLProcess.cpp
@@ -28,6 +28,7 @@
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
+#include <Pegasus/Common/Config.h>
 #include <Pegasus/Client/CIMClient.h>
 #include <Pegasus/Common/TimeValue.h>
 #include <Pegasus/Common/XmlReader.h>

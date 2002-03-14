224636c0c3fe9b6c0bdb93db11a87a35ea7ec417
kumpf <kumpf>
2002-03-14 05:34:16 +0000

    Fix repository specification in test program.

--- src/Pegasus/Server/ProviderRegistrationManager/tests/createInstances/createInstances.cpp
+++ src/Pegasus/Server/ProviderRegistrationManager/tests/createInstances/createInstances.cpp
@@ -154,7 +154,7 @@ int main(int argc, char** argv)
 {
 
 
-    CIMRepository r("/PegasusRun/repository") ;
+    CIMRepository r("../repository") ;
 
     ProviderRegistrationManager prmanager(&r);
 

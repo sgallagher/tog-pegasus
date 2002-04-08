fa02aaa5697f9a278ae0028499e8b85cf7960444
kumpf <kumpf>
2002-04-08 18:30:52 +0000

    HP-Nag: Modified AuthorizationHandler tests and added new cimauth command tests.

--- src/Pegasus/Security/UserManager/tests/AuthorizationHandler/AuthorizationHandler.cpp
+++ src/Pegasus/Security/UserManager/tests/AuthorizationHandler/AuthorizationHandler.cpp
@@ -37,10 +37,11 @@
 #include <Pegasus/Common/FileSystem.h>
 #include <Pegasus/Common/HashTable.h>
 #include <Pegasus/Repository/CIMRepository.h>
+#include <Pegasus/Security/UserManager/UserManager.h>
 #include <Pegasus/Security/UserManager/AuthorizationHandler.h>
 
 // Uncomment this if you want detailed messages to be printed.
-//#define VERBOSE 1
+//#define DEBUG 1
 
 PEGASUS_USING_PEGASUS;
 
@@ -52,54 +53,29 @@ static const String GOOD_NAMESPACE       = "root/cimv2";
 static const String BAD_NAMESPACE        = "root/cimvx99";
 
 
-void GetEnvironmentVariables(String& pegasusHome)
-{
-    // Get environment variables:
-
-    const char* tmp = getenv("PEGASUS_HOME");
-
-    if (!tmp)
-    {
-        cerr << " PEGASUS_HOME environment variable undefined" << endl;
-        exit(1);
-    }
-
-    pegasusHome = tmp;
-
-    FileSystem::translateSlashes(pegasusHome);
-}
-
 //
 // main
 //
 int main()
 {
-    String pegasusHome = String::EMPTY;
     String nameSpace = String::EMPTY;
-
-    static const char REPOSITORY[] = "/repository";
 
     String testUser = String::EMPTY;
 
     Boolean authorized;
 
-
     testUser.assign(System::getCurrentLoginName());
 
-    GetEnvironmentVariables(pegasusHome);
+    // Create a test repository
+    String repositoryPath = "./repository";
 
-    assert(FileSystem::isDirectory(pegasusHome));
+    PEGASUS_ASSERT(FileSystem::isDirectory(repositoryPath));
 
-    pegasusHome.append(REPOSITORY);
+    CIMRepository* repository = new CIMRepository(repositoryPath);
 
-    assert(FileSystem::isDirectory(pegasusHome));
-        
+    // -- Create a UserManager object:
 
-    // Create a repository
-    CIMRepository* repository = new CIMRepository(pegasusHome);
-
-    // Create auth handler
-    AuthorizationHandler* authHandler = new AuthorizationHandler(repository);
+    UserManager*  userManager = UserManager::getInstance(repository);
 
     //
     // Test authorization
@@ -107,51 +83,50 @@ int main()
     try
     {
         nameSpace.assign(BAD_NAMESPACE);
-        assert(!authHandler->verifyNamespace(nameSpace));
+        assert(!userManager->verifyNamespace(nameSpace));
 
         nameSpace.assign(GOOD_NAMESPACE);
-        assert(authHandler->verifyNamespace(nameSpace));
+        assert(!userManager->verifyNamespace(nameSpace));
 
-        authHandler->setAuthorization(testUser, nameSpace, "rw");
-        authHandler->setAuthorization("root", nameSpace, "w");
+        userManager->setAuthorization(testUser, nameSpace, "rw");
+        userManager->setAuthorization("root", nameSpace, "w");
 
-        String temp = authHandler->getAuthorization(testUser, nameSpace);
+        String temp = userManager->getAuthorization(testUser, nameSpace);
 
         if (testUser != "root")
            assert(String::equal(temp, "rw") || String::equal(temp, "wr"));
 
-        temp = authHandler->getAuthorization("root", nameSpace);
+        temp = userManager->getAuthorization("root", nameSpace);
         assert(String::equal(temp, "w"));
 
-        authHandler->removeAuthorization("root", nameSpace);
+        userManager->removeAuthorization("root", nameSpace);
         temp.clear();
         try
         {
-            temp = authHandler->getAuthorization("root", nameSpace);
+            temp = userManager->getAuthorization("root", nameSpace);
             assert(temp.size() == 0);
         }catch(Exception& e) { }
 
-        authHandler->setAuthorization("root", nameSpace, "w");
+        userManager->setAuthorization("root", nameSpace, "w");
 
         if (testUser != "root") 
-           assert(authHandler->verifyAuthorization(testUser, nameSpace, 
+           assert(userManager->verifyAuthorization(testUser, nameSpace, 
                    "GetInstance"));
-        assert(!authHandler->verifyAuthorization("root", nameSpace, "GetInstance"));
+        assert(!userManager->verifyAuthorization("root", nameSpace, "GetInstance"));
 
-        authHandler->setAuthorization("root", nameSpace, "r");
+        userManager->setAuthorization("root", nameSpace, "r");
 
-        assert(!authHandler->verifyAuthorization("root", nameSpace, "SetProperty"));
+        assert(!userManager->verifyAuthorization("root", nameSpace, "SetProperty"));
 
-        authHandler->removeAuthorization("root", nameSpace);
+        userManager->removeAuthorization("root", nameSpace);
         if (testUser != "root")
-           authHandler->removeAuthorization(testUser, nameSpace);
+           userManager->removeAuthorization(testUser, nameSpace);
     }
     catch(Exception& e)
     {
         cout << "Exception: " << e.getMessage() << endl;
         assert(0);
     }
-
 
     cout << "+++++ passed all tests" << endl;
 

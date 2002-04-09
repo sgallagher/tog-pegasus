d4da09f9b28d482f3fbfb8de9913d499cfca5ade
chip <chip>
2002-04-09 19:21:52 +0000

    fix provider name

--- src/Providers/sample/IndicationProvider/IndicationProviderMain.cpp
+++ src/Providers/sample/IndicationProvider/IndicationProviderMain.cpp
@@ -35,7 +35,7 @@ PEGASUS_NAMESPACE_BEGIN
 
 extern "C" PEGASUS_EXPORT CIMBaseProvider * PegasusCreateProvider(const String & providerName)
 {
-    if(String::equalNoCase(className, "sample_helloworldindication"))
+    if(String::equalNoCase(providerName, "SampleIndicationProvider"))
     {
 	return(new IndicationProvider());
     }

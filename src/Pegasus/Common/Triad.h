e5543113c9501eeffed2bf8e95ab1c3a67db2499
sage <sage>
2002-05-23 17:01:55 +0000

    added for ProviderAdapter

---
+++ src/Pegasus/Common/Triad.h
@@ -0,0 +1,68 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Markus Mueller (sedgewick_de@yahoo.de)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_Triad_h
+#define Pegasus_Triad_h
+
+#include <Pegasus/Common/Config.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+template<class T, class U, class V>
+struct Triad
+{
+    T first;
+    U second;
+    V third;
+
+    Triad() : first(), second(), third()
+    { 
+    }
+
+    Triad(const Triad<T,U,V>& x) : first(x.first), second(x.second), 
+                                   third(x.third)
+    { 
+    }
+
+    Triad(const T& first_, const U& second_, const V& third_) :
+                      first(first_), second(second_), third(third_)
+    {
+    }
+
+    Triad<T,U,V>& operator=(const Triad<T,U,V>& x)
+    {
+	first = x.first;
+	second = x.second;
+	third = x.third;
+	return *this;
+    }
+};
+
+PEGASUS_NAMESPACE_END
+
+#endif /* Pegasus_Triad_h */

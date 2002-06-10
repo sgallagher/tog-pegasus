95cc36d7ca83d7e7497035c2f1931171ced2a4b7
karl <karl>
2002-06-10 10:47:24 +0000

    Import the cli command line files

---
+++ src/Clients/CLITestClients/CLIClientLib/CLIClientLib.cpp
@@ -0,0 +1,587 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author:  Karl Schopmeyer (k.schopmeyer@opengroup.org) 
+//          Mary Hinton (m.hinton@verizon.net)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+#include <Pegasus/Common/Config.h>
+#include <cassert>
+#include <Pegasus/Common/FileSystem.h>
+#include <Pegasus/Common/XmlWriter.h>
+#include <Pegasus/Common/MofWriter.h>
+#include "CLIClientLib.h"
+
+PEGASUS_USING_STD;
+PEGASUS_NAMESPACE_BEGIN
+
+const String DEFAULT_NAMESPACE = "root/cimv2";
+
+static const char * usage = "blah blah";
+static const char * extra = "more blah";
+
+int OutputFormatInstance(OutputType format, CIMInstance& instance)
+{
+    if (format == OUTPUT_XML)
+        XmlWriter::printInstanceElement(instance, cout);
+    else if (format == OUTPUT_MOF)
+    {
+        Array<Sint8> x;
+        MofWriter::appendInstanceElement(x, instance);
+
+        x.append('\0');
+
+        mofFormat(cout, x.getData(), 4);
+    }   
+    return 0;
+}
+
+int OutputFormatClass(OutputType format, CIMClass& myClass)
+{
+    if (format == OUTPUT_XML)
+        XmlWriter::printClassElement(myClass, cout);
+    else if (format == OUTPUT_MOF)
+    {
+        Array<Sint8> x;
+        MofWriter::appendClassElement(x, myClass);
+
+        x.append('\0');
+
+        mofFormat(cout, x.getData(), 4);
+    }   
+    return 0;
+}
+
+int OutputFormatQualifierDecl(OutputType format, CIMQualifierDecl& myQualifierDecl)
+{
+    if (format == OUTPUT_XML)
+        XmlWriter::printQualifierDeclElement(myQualifierDecl, cout);
+    else if (format == OUTPUT_MOF)
+    {
+        Array<Sint8> x;
+        MofWriter::appendQualifierDeclElement(x, myQualifierDecl);
+
+        x.append('\0');
+
+        mofFormat(cout, x.getData(), 4);
+    }   
+    return 0;
+}
+
+int OutputFormatCIMValue(OutputType format, CIMValue& myValue)
+{
+    if (format == OUTPUT_XML)
+        XmlWriter::printValueElement(myValue, cout);
+    else if (format == OUTPUT_MOF)
+    {
+        Array<Sint8> x;
+        MofWriter::appendValueElement(x, myValue);
+
+        x.append('\0');
+
+        mofFormat(cout, x.getData(), 4);
+    }   
+    return 0;
+}
+
+
+
+int enumerateInstanceNames(CIMClient& client, Options& opts)
+{
+    Array<CIMObjectPath> instanceNames = 
+    client.enumerateInstanceNames(opts.nameSpace, opts.className);
+
+    //simply output the list one per line for the moment.
+    for (Uint32 i = 0; i < instanceNames.size(); i++)
+                cout << instanceNames[i] << endl;
+    return 0;
+}        
+
+int enumerateInstances(CIMClient& client, Options& opts)
+{
+    Array<CIMInstance> instances; 
+    instances = client.enumerateInstances(opts.nameSpace,
+                                                   opts.className,
+                                                   opts.deepInheritance,
+                                                   opts.localOnly,
+                                                   opts.includeQualifiers,
+                                                   opts.includeClassOrigin);
+    Uint32 cnt = 0;
+    opts.outputFormat.toLower();
+
+    for( ; cnt < NUM_OUTPUTS; cnt++ ) 
+    {
+        if (opts.outputFormat == OutputTable[cnt].OutputName)
+                break;
+    }
+    // Output the returned instances
+    for (Uint32 i = 0; i < instances.size(); i++)
+    {
+        CIMInstance instance = instances[i];
+        // Check Output Format to print results
+        OutputFormatInstance(OutputTable[cnt].OutputType, instance);
+    }
+    return 0;
+}
+
+int deleteInstance(CIMClient& client, Options& opts)
+{
+    client.deleteInstance(opts.nameSpace, opts.className);
+    return 0;  
+}
+
+int getInstance(CIMClient& client, Options& opts)
+{
+    CIMInstance cimInstance = client.getInstance(opts.nameSpace,
+                                                 opts.cimObjectPath);
+
+    Uint32 cnt = 0;
+    for( ; cnt < NUM_OUTPUTS; cnt++ ) 
+    {
+        if (opts.outputFormat == OutputTable[cnt].OutputName)
+                break;
+    }
+    // Check Output Format to print results
+    OutputFormatInstance(OutputTable[cnt].OutputType, cimInstance);
+    return 0;
+}
+
+int enumerateClassNames(CIMClient& client, Options& opts)
+{
+    Array<String> classNames; 
+    
+    classNames = client.enumerateClassNames(opts.nameSpace,
+                                        opts.className,
+                                        opts.deepInheritance);
+    
+    //simply output the list one per line for the moment.
+    for (Uint32 i = 0; i < classNames.size(); i++)
+                cout << classNames[i] << endl;
+    
+    return 0;
+}
+
+int enumerateClasses(CIMClient& client, Options& opts)
+{
+    Array<CIMClass> classes; 
+    
+    classes = client.enumerateClasses(opts.nameSpace,
+                                        opts.className,
+                                        opts.deepInheritance,
+                                        opts.localOnly,
+                                        opts.includeQualifiers,
+                                        opts.includeClassOrigin);
+    Uint32 cnt = 0;
+    opts.outputFormat.toLower();
+
+    for( ; cnt < NUM_OUTPUTS; cnt++ ) 
+    {
+        if (opts.outputFormat == OutputTable[cnt].OutputName)
+                break;
+    }
+    // Output the returned instances
+    for (Uint32 i = 0; i < classes.size(); i++)
+    {
+        CIMClass myClass = classes[i];
+    
+        OutputFormatClass(OutputTable[cnt].OutputType, myClass);
+    }
+    return 0;
+}
+
+int deleteClass(CIMClient& client, Options& opts)
+{
+    client.deleteClass(opts.nameSpace, opts.className);
+    return 0;  
+}
+
+int getClass(CIMClient& client, Options& opts)
+{
+    CIMClass cimClass = client.getClass(opts.nameSpace,
+                                        opts.className,
+                                        opts.localOnly,
+                                        opts.includeQualifiers,
+                                        opts.includeClassOrigin,
+                                        opts.propertyList);
+    Uint32 cnt = 0;
+    opts.outputFormat.toLower();
+
+    for( ; cnt < NUM_OUTPUTS; cnt++ ) 
+    {
+        if (opts.outputFormat == OutputTable[cnt].OutputName)
+                break;
+    }
+    
+    OutputFormatClass(OutputTable[cnt].OutputType, cimClass);
+    return 0;
+}
+
+int getProperty(CIMClient& client, Options& opts)
+{
+    CIMValue cimValue;
+    cimValue = client.getProperty( opts.nameSpace,
+                                   opts.instanceName,
+                                   opts.propertyName);
+                                   
+    // display returned property
+    return 0;
+}
+
+
+int setProperty(CIMClient& client, Options& opts)
+{
+    
+    client.setProperty( opts.nameSpace,
+                                   opts.instanceName,
+                                   opts.propertyName,
+                                   opts.newValue);
+                                   
+
+    return 0;
+}
+
+int getQualifier(CIMClient& client, Options& opts)
+{
+    CIMQualifierDecl cimQualifierDecl;
+    cimQualifierDecl = client.getQualifier( opts.nameSpace,
+                                   opts.qualifierName);
+                                   
+    // display new qualifier
+    
+    Uint32 cnt = 0;
+    opts.outputFormat.toLower();
+
+    for( ; cnt < NUM_OUTPUTS; cnt++ ) 
+    {
+        if (opts.outputFormat == OutputTable[cnt].OutputName)
+                break;
+    }
+    
+    OutputFormatQualifierDecl(OutputTable[cnt].OutputType, cimQualifierDecl);
+    
+    return 0;
+}
+int setQualifier(CIMClient& client, Options& opts)
+{
+    client.setQualifier( opts.nameSpace,
+                         opts.qualifierDeclaration);
+    return 0;
+}
+int deleteQualifier(CIMClient& client, Options& opts)
+{
+    client.deleteQualifier( opts.nameSpace,
+                            opts.qualifierName);
+                                   
+    return 0;
+}
+int enumerateQualifiers(CIMClient& client, Options& opts)
+{
+    Array<CIMQualifierDecl> qualifierDecls;
+    qualifierDecls = client.enumerateQualifiers( opts.nameSpace);
+    
+    Uint32 cnt = 0;
+    opts.outputFormat.toLower();
+    
+    for( ; cnt < NUM_OUTPUTS; cnt++ ) 
+    {
+        if (opts.outputFormat == OutputTable[cnt].OutputName)
+                break;
+    }
+    // Output the returned instances
+    for (Uint32 i = 0; i < qualifierDecls.size(); i++)
+    {
+        CIMQualifierDecl myQualifierDecl = qualifierDecls[i];
+    
+        OutputFormatQualifierDecl(OutputTable[cnt].OutputType, myQualifierDecl);
+    }
+                                   
+    return 0;
+}
+
+
+
+
+void GetOptions(
+    OptionManager& om,
+    int& argc,
+    char** argv,
+    const String& testHome)
+{
+    static const char* outputFormats[] = { "xml", "mof", "txt"};
+    static const Uint32 NUM_OUTPUTFORMATS = sizeof(outputFormats) /
+                                            sizeof(outputFormats[0]);
+
+    static OptionRow optionsTable[] =
+        //optionname defaultvalue rqd  type domain domainsize clname hlpmsg
+    {
+        {"User", "unknown", false, Option::STRING, 0, 0, "u",
+                                        "Defines User Name for authentication" },
+        
+        {"Password", "unknown", false, Option::STRING, 0, 0, "u",
+                                        "Defines password for authentication" },
+        
+        {"cimCmd", "unknown", false, Option::STRING, 0, 0, "-c",
+                                        "specifies CIM Command to use" },
+        
+        {"className", "", false, Option::STRING, 0, 0, "-s",
+                                        "ClassName to use" },
+
+        {"cimObjectPath", "", false, Option::STRING, 0, 0, "-p",
+                                        "CIMObjectPath to use" },
+        
+        {"location", "localhost:5988", false, Option::STRING, 0, 0, "-l",
+                                        "specifies system and port" },
+        
+        {"namespace", "root/cimv2", false, Option::STRING, 0, 0, "-n",
+                                        "specifies namespace to use for operation" },
+        
+        {"outputformats", "mof", false, Option::STRING, 0,
+                                         NUM_OUTPUTFORMATS, "-o",
+                                        "Output in xml, mof, txt"},
+        {"deepinheritance", "false", false, Option::BOOLEAN, 0, 0, "d",
+                                        "If set does deep enum "},
+
+        {"version", "false", false, Option::BOOLEAN, 0, 0, "v",
+                                        "Displays software Version "},
+
+        {"help", "false", false, Option::BOOLEAN, 0, 0, "h",
+                            "Prints help message with command line options "},
+
+        {"debug", "false", false, Option::BOOLEAN, 0, 0, "d", 
+                     "Not Used "},
+    };
+    const Uint32 NUM_OPTIONS = sizeof(optionsTable) / sizeof(optionsTable[0]);
+
+    om.registerOptions(optionsTable, NUM_OPTIONS);
+
+    //We want to make this code common to all of the commands
+
+    String configFile = "/CLTest.conf";
+
+    cout << "Config file from " << configFile << endl;
+
+    if (FileSystem::exists(configFile))
+             om.mergeFile(configFile);
+
+    om.mergeCommandLine(argc, argv);
+
+    om.checkRequiredOptions();
+
+}
+
+// ATTN: KS Think I stuck these together.
+void printHelp(char* name, OptionManager& om)
+{
+    String header = "Usage ";
+    header.append(name);
+
+    //om.printOptionsHelpTxt(header, trailer);
+}
+
+/* PrintHelp - This is temporary until we expand the options manager to allow
+   options help to be defined with the OptionRow entries and presented from
+   those entries.
+*/
+void printHelpMsg(const char* pgmName, const char* usage, const char* extraHelp, 
+                OptionManager& om)
+{
+    cout << endl << pgmName << endl;
+    cout << "Usage: " << pgmName << endl << usage << endl;
+    cout << endl;
+    // ATTN: KS om.printHelp(const char* pgmName, OptionManager om);
+    cout << extraHelp << endl;
+
+}
+
+
+
+int CheckCommonOptionValues(OptionManager& om, char** argv, Options& opts) 
+{
+    // Check to see if user asked for help (-h otpion):
+    if (om.valueEquals("verbose", "true"))
+    {
+                printHelpMsg(argv[0], usage, extra, om);
+                exit(0);
+    }
+
+    // Establish the namespace from the input parameters
+    //String nameSpace;
+    if(om.lookupValue("namespace", opts.nameSpace))
+    {
+       cout << "Namespace = " << opts.nameSpace << endl;
+
+    }
+
+    if(om.lookupValue("cimCmd", opts.cimCmd))
+    {
+       cout << "CIM command = " << opts.cimCmd << endl;
+
+    }
+    if(om.lookupValue("className", opts.className))
+    {
+       cout << "Class Name = " << opts.className << endl;
+
+    }
+
+   if(om.lookupValue("outputformats", opts.outputFormat))
+    {
+       cout << "Output Format = " << opts.outputFormat << endl;
+
+    }
+
+    if(om.lookupValue("cimObjectPath", opts.cimObjectPath))
+    {
+       cout << "CIM ObjectPath = " << opts.cimObjectPath << endl;
+
+    }
+
+    Boolean verboseTest = (om.valueEquals("verbose", "true")) ? true :false;
+    /*
+    Boolean activeTest = false;
+    if (om->valueEquals("active", "true"))
+                 activeTest = true;
+    */
+    // Now develop the target instname from the arglist
+    // For this one, at least one arguement is required
+    // Not sure if this is true.
+    // Think we should be able to get by with no argument.
+    if (om.valueEquals("help", "true"))
+    {
+        String header = "Usage ";
+        String trailer = "";
+        om.printOptionsHelpTxt(header, trailer);
+        exit(0);
+    }
+
+    //String location =   "localhost:5988";
+    if(om.lookupValue("location", opts.location))
+    {
+       cout << "Location = " << opts.location << endl;
+
+    }
+    return 0;
+}
+
+
+//------------------------------------------------------------------------------
+//
+// _indent()
+//
+//------------------------------------------------------------------------------
+
+static void _indent(PEGASUS_STD(ostream)& os, Uint32 level, Uint32 indentSize)
+{
+    Uint32 n = level * indentSize;
+    if (n > 50)
+    {
+    cout << "Jumped Ship " << level << " size " << indentSize << endl;
+    exit(1);
+    }
+
+    for (Uint32 i = 0; i < n; i++)
+        os << ' ';
+}
+void mofFormat(
+    PEGASUS_STD(ostream)& os, 
+    const char* text, 
+    Uint32 indentSize)
+{
+    char* tmp = strcpy(new char[strlen(text) + 1], text);
+    Uint32 count = 0;
+    Uint32 indent = 0;
+    Boolean quoteState = false;
+    Boolean qualifierState = false;
+    char c;
+    char prevchar;
+    while (c = *tmp++)
+    {
+        count++;
+        // This is too simplistic and must move to a token based mini parser
+        // but will do for now. One problem is tokens longer than 12 characters that
+        // overrun the max line length.
+        switch (c)
+        {
+            case '\n':
+                os << Sint8(c);
+                prevchar = c;
+                count = 0 + (indent * indentSize);
+                _indent(os, indent, indentSize);   
+                break;
+
+            case '\"':   // quote 
+                os << Sint8(c);
+                prevchar = c;
+                quoteState = !quoteState;
+                break;
+
+            case ' ':
+                os << Sint8(c);
+                prevchar = c;
+                if (count > 66)
+                {
+                    if (quoteState)
+                    {   
+                        os << "\"\n";
+                        _indent(os, indent + 1, indentSize);   
+                        os <<"\"";
+                    }
+                    else
+                    {
+                        os <<"\n";
+                        _indent(os, indent + 1,  indentSize);   
+                    }
+                    count = 0 + ((indent + 1) * indentSize);
+                }
+                break;
+            case '[':
+                if (prevchar == '\n')
+                {
+                    indent++;
+                    _indent(os, indent,  indentSize);
+                    qualifierState = true;
+                }
+                os << Sint8(c);
+                prevchar = c;
+                break;
+
+            case ']':
+                if (qualifierState)
+                {
+                    if (indent > 0)
+                        indent--;
+                    qualifierState = false;
+                }
+                os << Sint8(c);
+                prevchar = c;
+                break;
+
+            default:
+                os << Sint8(c);
+                prevchar = c;
+        }
+
+    }
+}
+
+PEGASUS_NAMESPACE_END
---
+++ src/Clients/CLITestClients/CLIClientLib/CLIClientLib.h
@@ -0,0 +1,201 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to 
+// deal in the Software without restriction, including without limitation the 
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN 
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author:  Karl Schopmeyer (k.schopmeyer@opengroup.org) 
+//          Mary Hinton (m.hinton@verizon.net)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef _CLI_CLIENTLIB_H
+#define _CLI_CLIENTLIB_H
+
+#include <Pegasus/Common/Config.h>
+#include <cassert>
+#include <Pegasus/Client/CIMClient.h>
+#include <Pegasus/Common/OptionManager.h>
+#include <Clients/CLITestClients/CLIClientLib/Linkage.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+typedef int     CommandID;
+typedef int     OutputType;
+const OutputType    OUTPUT_XML                  = 1;
+const OutputType    OUTPUT_MOF                  = 2;
+const OutputType    OUTPUT_TEXT                 = 3;
+const OutputType    OUTPUT_FILE                 = 4;
+
+const CommandID ID_EnumerateInstanceNames       = 1;
+const CommandID ID_EnumerateInstances           = 2;
+const CommandID ID_GetClass                     = 3;
+const CommandID ID_GetInstance                  = 4;
+const CommandID ID_CreateInstance               = 5;
+const CommandID ID_DeleteInstance               = 6;
+const CommandID ID_CreateClass                  = 7;
+const CommandID ID_DeleteClass                  = 8;
+const CommandID ID_EnumerateClasses             = 9;
+const CommandID ID_EnumerateClassNames          = 10;
+const CommandID ID_GetProperty                  = 11;
+const CommandID ID_SetProperty                  = 12;
+const CommandID ID_GetQualifier                 = 13;
+const CommandID ID_SetQualifier                 = 14;
+const CommandID ID_EnumerateQualifiers          = 15;
+const CommandID ID_DeleteQualifier              = 16;
+const CommandID ID_Associators                  = 17;
+const CommandID ID_AssociatorNames              = 18;
+const CommandID ID_References                   = 19;
+const CommandID ID_ReferenceNames               = 20;
+const CommandID ID_InvokeMethod                 = 21;
+const CommandID ID_ExecQuery                    = 22;
+
+const CommandID ID_ModifyClass                  = 23;
+const CommandID ID_ModifyInstance               = 24;
+
+const CommandID ID_Unknown                      = 25;
+
+struct  OUTPUT_STRUCT
+{
+    OutputType OutputType;
+    char* OutputName;
+};
+
+OUTPUT_STRUCT OutputTable[] =
+{
+    // Output Type      OutputName              
+    {   OUTPUT_XML,     "xml"   },
+    {   OUTPUT_MOF,     "mof"   },
+    {   OUTPUT_TEXT,    "txt"   }
+};
+const Uint32 NUM_OUTPUTS = sizeof(OutputTable) / sizeof(OutputTable[0]);
+    
+struct  CMD_STRUCT
+{
+    CommandID ID_Command;
+    char* CommandName;
+};
+
+// ******** CIM Client Commands
+CMD_STRUCT CommandTable[] =
+{
+        // Command ID                CommandName                
+    {   ID_EnumerateInstanceNames,  "enumerateinstancenames"    },
+    {   ID_EnumerateInstances,      "enumerateinstances"        },
+    {   ID_EnumerateClassNames,     "enumerateclassnames"       },
+    {   ID_EnumerateClasses,        "enumerateclasses"          },
+    {   ID_GetClass,                "getclass"                  },
+    {   ID_GetInstance,             "getinstance"               },
+    {   ID_CreateInstance,          "createinstance"            },
+    {   ID_DeleteInstance,          "deleteinstance"            },
+    {   ID_CreateClass   ,          "createclass"               },
+    {   ID_DeleteInstance,          "deleteclass"               },
+    {   ID_ModifyInstance,          "modifyclass"               },
+    {   ID_DeleteClass,             "deleteinstance"            },
+    {   ID_GetProperty,             "getproperty"               },
+    {   ID_SetProperty,             "setproperty"               },
+    {   ID_GetQualifier,            "getqualifier"              },
+    {   ID_SetQualifier,            "setqualifier"              },
+    {   ID_EnumerateQualifiers,     "enumeratequalifiers"       },
+    {   ID_InvokeMethod,            "InvokeMethod"              },
+    {   ID_Associators,             "associators"               },
+    {   ID_AssociatorNames,         "associatornames"           },
+    {   ID_References,              "references"                },
+    {   ID_ReferenceNames,          "referencenames"            },
+    {   ID_InvokeMethod,            "InvokeMethod"              },
+    {   ID_ExecQuery,               "execquery"                 },
+    
+    {   ID_Unknown,                 "unknown"                   }
+//    { 0,                           NULL,                      }
+};
+
+const Uint32 NUM_COMMANDS = sizeof(CommandTable) / sizeof(CommandTable[0]);
+
+// ************* Options Functions
+
+struct  OPTION_STRUCT
+{
+    String location;
+    String nameSpace;
+    String className;
+    String cimCmd;
+    String outputFormat;
+    Boolean deepInheritance;
+    Boolean localOnly;
+    Boolean includeQualifiers;
+    Boolean includeClassOrigin;
+    CIMObjectPath instanceName;
+    CIMValue newValue;
+    String cimObjectPath;
+    String qualifierName;
+    CIMQualifierDecl qualifierDeclaration;
+    String user;
+    String password;
+    String propertyName;
+    CIMPropertyList propertyList;
+};
+
+typedef struct OPTION_STRUCT Options;
+
+void PEGASUS_CLI_LINKAGE printHelpMsg(const char* pgmName, const char* usage, const char* extraHelp, 
+                OptionManager& om);
+
+void PEGASUS_CLI_LINKAGE printHelp(char* name, OptionManager& om);
+
+void  PEGASUS_CLI_LINKAGE GetOptions(OptionManager& om, int& argc, char** argv,  const String& testHome);
+
+int PEGASUS_CLI_LINKAGE CheckCommonOptionValues(OptionManager& om, char** argv, Options& opts);
+
+void PEGASUS_CLI_LINKAGE mofFormat(PEGASUS_STD(ostream)& os, const char* text, Uint32 indentSize);
+
+// ************* CIMClient Functions
+int PEGASUS_CLI_LINKAGE enumerateClassNames(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE enumerateClasses(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE deleteClass(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE getClass(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE deleteInstance(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE enumerateInstanceNames(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE enumerateInstances(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE getInstance(CIMClient& client, Options& opts);
+                                           
+int PEGASUS_CLI_LINKAGE getProperty(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE setProperty(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE setQualifier(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE getQualifier(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE deleteQualifier(CIMClient& client, Options& opts);
+
+int PEGASUS_CLI_LINKAGE enumerateQualifiers(CIMClient& client, Options& opts);
+
+
+PEGASUS_NAMESPACE_END
+
+#endif

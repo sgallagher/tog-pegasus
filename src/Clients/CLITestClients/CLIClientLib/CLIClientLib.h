24be40e1412fa31164bcd8888405a807824c0cc9
karl <karl>
2002-06-10 22:31:11 +0000

    fix to OutputType defintion

--- src/Clients/CLITestClients/CLIClientLib/CLIClientLib.h
+++ src/Clients/CLITestClients/CLIClientLib/CLIClientLib.h
@@ -14,13 +14,13 @@
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
 // LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR 
 // PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN 
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
 // ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 //==============================================================================
 //
-// Author:  Karl Schopmeyer (k.schopmeyer@opengroup.org) 
+// Author:  Karl Schopmeyer (k.schopmeyer@opengroup.org)
 //          Mary Hinton (m.hinton@verizon.net)
 //
 // Modified By:
@@ -75,7 +75,8 @@ const CommandID ID_Unknown                      = 25;
 
 struct  OUTPUT_STRUCT
 {
-    OutputType OutputType;
+    // KS the old form OutputType OutputType causes redefinition error in Linux
+    int OutputType;
     const char* OutputName;
 };
 

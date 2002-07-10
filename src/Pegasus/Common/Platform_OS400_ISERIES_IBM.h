b0f9e4c17604dfb54311128ad47b56eea5aaee9b
chuck <chuck>
2002-07-10 21:12:06 +0000

    Initial OS400 version

---
+++ src/Pegasus/Common/Platform_OS400_ISERIES_IBM.h
@@ -0,0 +1,78 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001 The Open group, BMC Software, Tivoli Systems, IBM
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Chuck Carmack (carmack@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_Platform_OS400_ISERIES_IBM_h
+#define Pegasus_Platform_OS400_ISERIES_IBM_h
+
+//#define _XOPEN_SOURCE_EXTENDED 1
+//#define _OPEN_SYS 1
+#define _MSE_PROTOS
+#define _OPEN_SOURCE 3
+
+#define _MULTI_THREADED  
+
+// chuck
+//#pragma convert(819)
+
+#include <Pegasus/Common/ConfigUnix.h>
+#include <ifs.cleinc>
+  
+#define PEGASUS_OS_OS400
+
+#define PEGASUS_ARCHITECTURE_ISERIES
+
+#define PEGASUS_COMPILER_IBM
+
+#define PEGASUS_UINT64 unsigned long int
+
+#define PEGASUS_SINT64 long int
+
+#define PEGASUS_LLONG_MAX 9223372036854775807LL
+#define PEGASUS_LLONG_MIN (-PEGASUS_LLONG_MAX - 1LL)
+
+#define PEGASUS_ULLONG_MAX 18446744073709551615ULL
+
+#define PEGASUS_HAVE_NAMESPACES
+
+#define PEGASUS_HAVE_EXPLICIT
+
+#define PEGASUS_HAVE_MUTABLE
+
+#define PEGASUS_HAVE_FOR_SCOPE
+
+#define PEGASUS_HAVE_TEMPLATE_SPECIALIZATION
+
+#define PEGASUS_HAVE_BOOLEAN
+
+// #define PEGASUS_EXPLICIT_INSTANTIATION
+
+#define PEGASUS_HAS_EBCDIC
+
+#define PEGASUS_STATIC_CDECL __cdecl
+
+#endif /* Pegasus_Platform_ZOS_ZSERIES_IBM_h */

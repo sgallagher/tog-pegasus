9c5a442ca47a25f780b3dc787b0bafe87347c8ea
kumpf <kumpf>
2002-09-20 22:09:13 +0000

    HP-RK Remove unused private constructor.

--- src/Pegasus/Common/CIMValue.h
+++ src/Pegasus/Common/CIMValue.h
@@ -412,10 +412,6 @@ public:
 
 private:
 
-    // ATTN-RK-P2-20020719: For debugging only; remove before API release.
-    // Use CIMValue(const String&) instead.
-    PEGASUS_EXPLICIT CIMValue(const char* x) {}
-
     CIMValueRep* _rep;
 
     friend class CIMMethodRep;

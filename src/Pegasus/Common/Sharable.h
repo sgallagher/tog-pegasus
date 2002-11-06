a35fde5338d52c679a3a179f0e70ebb924d40032
mday <mday>
2002-11-06 22:03:42 +0000

    implemented roger kumpf fix to sharable

--- src/Pegasus/Common/Sharable.h
+++ src/Pegasus/Common/Sharable.h
@@ -1,31 +1,29 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%/////////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software, Hewlett-Packard Company, IBM,
+// The Open Group, Tivoli Systems
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Mike Brasher (mbrasher@bmc.com)
 //
-//////////////////////////////////////////////////////////////////////////
+// Modified By: Roger Kumpf, Hewlett-Packard Company (roger_kumpf@hp.com)
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -34,14 +32,14 @@
 
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/Linkage.h>
-#include <Pegasus/Common/PegasusAssert.h>
-#include <Pegasus/Common/AtomicInt.h>
+#include <Pegasus/Common/IPC.h>
+#include <assert.h>
 
 PEGASUS_NAMESPACE_BEGIN
 
-/**
-    The Sharable class implements a simple reference counting scheme.
-    The static Inc() method increments the reference count member.
+/** 
+    The Sharable class implements a simple reference counting scheme. 
+    The static Inc() method increments the reference count member. 
     The static Dec() method decrements the reference count and deletes
     the object when the reference count becomes zero.
 
@@ -55,32 +53,36 @@ public:
     Sharable() : _ref(1) { }
 
     virtual ~Sharable();
+    Uint32 getRef() const { return _ref.value(); }
 
-    friend void Inc(Sharable* sharable);
+    friend void Inc(const Sharable* sharable);
 
-    friend void Dec(Sharable* sharable);
+    friend void Dec(const Sharable* sharable);
 
 private:
-    Sharable(const Sharable &s);
-    Sharable& operator=(const Sharable &s);
-
-    AtomicInt _ref;
+        AtomicInt _ref;
 };
 
-inline void Inc(Sharable* x)
+inline void Inc(const Sharable* x)
+
 {
-    if (x)
+  if (x)
     {
-        // A sharable object should never be incremented from zero.
-        PEGASUS_DEBUG_ASSERT(x->_ref.get());
-        x->_ref++;
+      // A sharable object should never be incremented from zero.
+      // If so, there is a double delete being cause by impropoer use
+      // of sharable assignment or copy constructors somewhere
+      // << Wed Nov  6 12:46:52 2002 mdd >>
+      assert(((Sharable*)x)->_ref.value());
+      ((Sharable*)x)->_ref++;
     }
+  
 }
 
-inline void Dec(Sharable* x)
+
+inline void Dec(const Sharable* x)
 {
-  if (x && x->_ref.decAndTestIfZero())
-    delete x;
+  if (x && ((Sharable*)x)->_ref.DecAndTestIfZero())
+    delete (Sharable*)x;
 }
 
 PEGASUS_NAMESPACE_END

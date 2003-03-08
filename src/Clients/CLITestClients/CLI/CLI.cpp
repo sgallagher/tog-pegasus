570071ba16324a799396718545df86ede2748650
karl <karl>
2003-03-08 16:45:10 +0000

    fix compile error

--- src/Clients/CLITestClients/CLI/CLI.cpp
+++ src/Clients/CLITestClients/CLI/CLI.cpp
@@ -105,7 +105,7 @@ int main(int argc, char** argv)
     opts.includeClassOrigin = false;
     opts.resultClassName = String::EMPTY;
     opts.role = String::EMPTY;
-    opts.resultRole = String::EMPTY
+    opts.resultRole = String::EMPTY;
     opts.propertyListText = String::EMPTY;
     opts.propertyList.clear(); 
     opts.propertyName = String::EMPTY;

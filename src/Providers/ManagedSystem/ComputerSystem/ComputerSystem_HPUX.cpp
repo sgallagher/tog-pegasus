fcbb12d35e4f2e428f03ff58226dce69e51e9bb7
kumpf <kumpf>
2003-03-12 22:36:39 +0000

    HP-[BJH] HP-UX updates to compile with -AA option (Bugzilla 202)

--- src/Providers/ManagedSystem/ComputerSystem/ComputerSystem_HPUX.cpp
+++ src/Providers/ManagedSystem/ComputerSystem/ComputerSystem_HPUX.cpp
@@ -43,7 +43,7 @@
 #include <time.h>        // localtime()
 #include <sys/utsname.h> // uname()
 #include <sys/param.h>   // MAXHOSTNAMELEN
-#include <fstream.h>
+#include <fstream>
 
 #include <Pegasus/Common/InternalException.h>
 #include "ComputerSystemProvider.h"

45d44fcc6ad309a6e88e2121b97c92976f68f653
kumpf <kumpf>
2003-07-16 17:43:03 +0000

    HP-JY Add Default Instance Provider.

---
+++ src/Providers/TestProviders/DefaultInstanceProvider/DefaultInstanceProvider.h
@@ -0,0 +1,113 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software, Hewlett-Packard Company, IBM,
+// The Open Group, Tivoli Systems
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Jenny Yu, Hewlett-Packard Company (jenny_yu@hp.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_DefaultInstanceProvider_h
+#define Pegasus_DefaultInstanceProvider_h
+
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Client/CIMClient.h>
+#include <Pegasus/Provider/CIMInstanceProvider.h>
+#include <Pegasus/Repository/CIMRepository.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+class DefaultInstanceProvider : public CIMInstanceProvider
+{
+public:
+	DefaultInstanceProvider(void);
+	~DefaultInstanceProvider(void);
+
+	// CIMProvider interface
+	virtual void initialize(CIMOMHandle & cimom);
+	virtual void terminate(void);
+
+	// CIMInstanceProvider interface
+	void getInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const Boolean includeQualifiers,
+		const Boolean includeClassOrigin,
+		const CIMPropertyList & propertyList,
+		InstanceResponseHandler & handler);
+
+	void enumerateInstances(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const Boolean includeQualifiers,
+		const Boolean includeClassOrigin,
+		const CIMPropertyList & propertyList,
+		InstanceResponseHandler & handler);
+
+	void enumerateInstanceNames(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		ObjectPathResponseHandler & handler);
+
+	void modifyInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const CIMInstance & obj,
+		const Boolean includeQualifiers,
+		const CIMPropertyList & propertyList,
+		ResponseHandler & handler);
+
+	void createInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const CIMInstance & obj,
+		ObjectPathResponseHandler & handler);
+
+	void deleteInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		ResponseHandler & handler);
+
+protected:
+	Array<CIMObjectPath> _instanceNames;
+	Array<CIMInstance>   _instances;
+        CIMRepository *      _repository;
+
+private:
+        Boolean _nameSpaceExists(const CIMNamespaceName& nameSpaceName) const;
+
+        void _copyNameSpace(const String & nameSpace, 
+                            const String & className);
+
+        void _copyClass(const String & nameSpace,
+                        const String & className);
+
+        void _copySuperClasses(CIMClient & client, 
+                           const String & nameSpace, 
+                           const CIMClass & cimClass, 
+                           Array<CIMClass> & superClasses);
+};
+
+#endif
+
+PEGASUS_NAMESPACE_END

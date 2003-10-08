2d2e0ad79b126f8a587bfa9bd5287efbef281474
chip <chip>
2003-10-08 16:30:46 +0000

    new initialization object for provider registration

---
+++ src/Pegasus/ProviderManager2/ProviderName.cpp
@@ -0,0 +1,170 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000 - 2003 BMC Software, Hewlett-Packard Company, IBM,
+// The Open Group, Tivoli Systems
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#include "ProviderName.h"
+
+#include <Pegasus/Common/CIMValue.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+ProviderName::ProviderName(void) : _capabilities(0)
+{
+}
+
+/*
+ProviderName::ProviderName(const String & providerName)
+    : _capabilities(0)
+{
+    // NOTE: format ::= <physical_name>::<logical_name>::<object_name>::<capabilities>
+
+    String s = providerName;
+
+    Uint32 beg = 0;
+    Uint32 end = 0;
+
+    // get physical name
+    beg = 0;
+    end = s.find("::");
+
+    String temp = s.subString(beg, end - beg);
+    _physicalName = temp;
+
+    s.remove(beg, end + 2); // skip past the "::"
+    beg = end;
+
+    // get logical name
+    beg = 0;
+    end = s.find("::");
+
+    temp = s.subString(beg, end - beg);
+    _logicalName = temp;
+
+    s.remove(beg, end + 2); // skip past the "::"
+    beg = end;
+
+    // get object name
+    beg = 0;
+    end = s.find("::");
+
+    temp = s.subString(beg, end - beg);
+    _objectName = temp;
+
+    //s.remove(beg, end + 2); // skip past the "::"
+    //beg = end;
+
+    _objectName = temp;
+
+    // get capabilities mask
+    beg = 0;
+    end = s.find("::");
+
+    _physicalName = String(s.subString(beg, end - beg));
+}
+*/
+
+ProviderName::ProviderName(
+    const String & objectName,
+    const String & logicalName,
+    const String & physicalName,
+    const String & interfaceName,
+    const Uint32 capabilities)
+    : _capabilities(0)
+{
+    // ATTN: validate arguments ???
+    _objectName = objectName;
+
+    _logicalName = logicalName;
+
+    _physicalName = physicalName;
+
+    _interfaceName = interfaceName;
+}
+
+ProviderName::~ProviderName(void)
+{
+}
+
+/*
+String ProviderName::toString(void) const
+{
+    String s;
+
+    s.append(_physicalName);
+    s.append("::");
+    s.append(_logicalName);
+    s.append("::");
+    s.append(_objectName);
+    //s.append("::");
+    //s.append(CIMValue(_capabilities).toString());
+
+    return(s);
+}
+*/
+
+String ProviderName::getPhysicalName(void) const
+{
+    return(_physicalName);
+}
+
+void ProviderName::setPhysicalName(const String & physicalName)
+{
+    _physicalName = physicalName;
+}
+
+String ProviderName::getLogicalName(void) const
+{
+    return(_logicalName);
+}
+
+void ProviderName::setLogicalName(const String & logicalName)
+{
+    _logicalName = logicalName;
+}
+
+String ProviderName::getObjectName(void) const
+{
+    return(_objectName);
+}
+
+void ProviderName::setObjectName(const String & logicalName)
+{
+    _logicalName = logicalName;
+}
+
+Uint32 ProviderName::getCapabilitiesMask(void) const
+{
+    return(_capabilities);
+}
+
+void ProviderName::setCapabilitiesMask(const Uint32 capabilities)
+{
+    _capabilities = capabilities;
+}
+
+PEGASUS_NAMESPACE_END
---
+++ src/Pegasus/ProviderManager2/ProviderType.h
@@ -0,0 +1,107 @@
+//%/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000 - 2003 BMC Software, Hewlett-Packard Company, IBM,
+// The Open Group, Tivoli Systems
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_InternalProviderName_h
+#define Pegasus_InternalProviderName_h
+
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/String.h>
+
+#include <Pegasus/Server/Linkage.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+class PEGASUS_SERVER_LINKAGE InternalProviderName
+{
+public:
+    InternalProviderName(void);
+    //InternalProviderName(const String & s);
+    /*
+    physicalName - the fully qualified name to the physical component that is responsible for providing
+        for the object named in objectName.
+
+    logicalName - the logical name associated with the physical component. It is possible for one physical
+        component to have one or more logical components.
+
+    objectName - the relative or fully qualified CIM object name of interest.
+    */
+    InternalProviderName(
+        const String & objectName,
+        const String & logicalName,
+        const String & physicalName,
+        const String & interfaceName,
+        const Uint32 capabilities);
+
+    ~InternalProviderName(void);
+
+    //String toString(void) const;
+
+    String getObjectName(void) const;
+    void setObjectName(const String & objectName);
+
+    String getLogicalName(void) const;
+    void setLogicalName(const String & logicalName);
+
+    String getPhysicalName(void) const;
+    void setPhysicalName(const String & physicalName);
+
+    String getInterfaceName(void) const;
+    void setInterfaceName(const String & interfaceName);
+
+    Uint32 getCapabilitiesMask(void) const;
+    void setCapabilitiesMask(const Uint32 capabilities);
+
+private:
+    String _physicalName;
+    String _interfaceName;
+
+    String _logicalName;
+    String _objectName;
+
+    Uint32 _capabilities;
+
+};
+
+class PEGASUS_SERVER_LINKAGE ProviderType
+{
+public:
+    // these constants have the same value as those in DynamicRoutingTable.
+    static const Uint32 INSTANCE;
+    static const Uint32 CLASS;
+    static const Uint32 METHOD;
+    static const Uint32 ASSOCIATION;
+    static const Uint32 QUERY;
+    static const Uint32 INDICATION;
+    static const Uint32 CONSUMER;
+
+};
+
+PEGASUS_NAMESPACE_END
+
+#endif

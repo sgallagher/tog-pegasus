037e40710d8b6026d89849ea849036b010053e05
s.soni <s.soni>
2004-02-17 15:21:54 +0000

    Test provider for pegasus as explained in PEP#121

---
+++ test/TestProviders/XMLParserGen/ParserTestProvider.h
@@ -0,0 +1,118 @@
+//%2003////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002  BMC Software, Hewlett-Packard Development
+// Company, L. P., IBM Corp., The Open Group, Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L. P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: <Subodh Soni> (<ssubodh@in.ibm.com>)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_ParserTestProvider_h
+#define Pegasus_ParserTestProvider_h
+
+#include <iostream>
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/String.h>
+#include <Pegasus/Provider/CIMInstanceProvider.h>
+
+using namespace std;
+
+#define DEBUG(X)  cout << "ParserTestProvider:" <<  X << endl
+// This is not reqd. if IBMKR provider is not being used.
+#define DIRECTORY "/home/subodh/ProjectDir/BadFileNames"
+// Till here
+//
+#define CLASS_NAME "SampleXML_ParserClass"
+#define CLASS_1	"Sample_PClassOne"
+#define CLASS_2	"Sample_PClassTwo"
+#define CLASS_3	"Sample_PClassThree"
+
+#define BADSTR_1 "\0\0\0\'\'\'\'\'0000<<<<<<<>"
+#define BADSTR_2 "\0\0\0\'\'\'\'\'0000        "
+#define BADSTR_3 "     \0\0\0\'\'\'\'\'0000\0\0\0\0\0"
+
+PEGASUS_NAMESPACE_BEGIN
+
+class ParserTestProvider :
+	public CIMInstanceProvider
+{
+public:
+	ParserTestProvider(void);
+	virtual ~ParserTestProvider(void);
+    	virtual Boolean tryTerminate(void);
+
+	// CIMProvider interface
+	virtual void initialize(CIMOMHandle & cimom);
+	virtual void terminate(void);
+
+	// CIMInstanceProvider interface
+	virtual void getInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const Boolean includeQualifiers,
+		const Boolean includeClassOrigin,
+		const CIMPropertyList & propertyList,
+		InstanceResponseHandler & handler);
+
+	virtual void enumerateInstances(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const Boolean includeQualifiers,
+		const Boolean includeClassOrigin,
+		const CIMPropertyList & propertyList,
+		InstanceResponseHandler & handler);
+
+	virtual void enumerateInstanceNames(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		ObjectPathResponseHandler & handler);
+
+	virtual void modifyInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const CIMInstance & obj,
+		const Boolean includeQualifiers,
+		const CIMPropertyList & propertyList,
+		ResponseHandler & handler);
+
+	virtual void createInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		const CIMInstance & obj,
+		ObjectPathResponseHandler & handler);
+
+	virtual void deleteInstance(
+		const OperationContext & context,
+		const CIMObjectPath & ref,
+		ResponseHandler & handler);
+
+protected:
+    Array<CIMInstance> _instances;
+    Array<CIMObjectPath> _instanceNames;
+};
+
+PEGASUS_NAMESPACE_END
+
+#endif /*Pegasus_ParserTestProvider_h */

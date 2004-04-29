6551b781738328c51582323cf7d5770438ae6e46
marek <marek>
2004-04-29 08:39:20 +0000

    BUG#: 1415 TITLE: Casting error in CIMClientRep.cpp
    DESCRIPTION: I removed the call to inet_addr for those platforms requiring a (char *) as first parameter for gethostbyaddr() as those expect a string of type "x.x.x.x" (i.e. hostname) as input parameter. Moved the libraries required for call gethostbyaddr() out of the SLP only condition part.

--- src/Pegasus/Client/CIMClientRep.cpp
+++ src/Pegasus/Client/CIMClientRep.cpp
@@ -1439,23 +1439,25 @@ Uint32 CIMClientRep::_acquireIP(const char* hostname)
 		// given hostname starts with an numeric character
 #ifdef PEGASUS_OS_OS400
 		Uint32 tmp_addr = inet_addr(ebcdicHost);
-#else
-#ifdef PEGASUS_OS_ZOS
+#elif defined(PEGASUS_OS_ZOS)
 		Uint32 tmp_addr = inet_addr_ebcdic((char *)hostname);
+#elif defined(PEGASUS_PLATFORM_WIN32_IX86_MSVC) || defined(PEGASUS_PLATFORM_SOLARIS_SPARC_CC) || defined(PEGASUS_OS_HPUX)
+		// resolve hostaddr to a real host entry
+		entry = gethostbyaddr((char *) hostname, sizeof(hostname), AF_INET);
 #else
-		Uint32 tmp_addr = inet_addr((char *)hostname);
+		Uint32 tmp_addr = inet_addr((char *) hostname);
 #endif
-#endif
+		
+#if !defined(PEGASUS_PLATFORM_WIN32_IX86_MSVC) && !defined(PEGASUS_PLATFORM_SOLARIS_SPARC_CC) && !defined(PEGASUS_OS_HPUX)
 		// 0xFFFFFFF is same as -1 in an unsigned int32
 		if (tmp_addr == 0xFFFFFFFF)
 		{
 			// error, given ip does not follow format requirements
 			return 0xFFFFFFFF;
 		}
-
 		// resolve hostaddr to a real host entry
-
 		entry = gethostbyaddr(&tmp_addr,sizeof(tmp_addr),AF_INET);
+#endif
 
 		if (entry == 0)
 		{

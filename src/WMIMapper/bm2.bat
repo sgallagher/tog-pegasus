05073e6a8706235e01862fc4d30416995fd509ed
kumpf <kumpf>
2004-05-04 23:17:58 +0000

    BUG#: 1437 TITLE: [HP-TM] WMIMapper Obsolete/Unused Files
    DESCRIPTION: Several files in the WMIMapper source tree are no longer being used/maintained (e.g., Visual C++ project files, and old build scripts that have been replaced).  These files have been removed from the WMI Mapper source tree.

--- src/WMIMapper/bm2.bat
+++
@@ -1,2 +0,0 @@
-rem This file should be run from the Pegasus directory.
-make -f src/WMIMapper/buildmapper mapper >a%1.txt 2>&1
--- src/WMIMapper/m.bat
+++
@@ -1,2 +0,0 @@
-rem This file should be run from the Pegasus directory.
-make rebuild >a%1.txt 2>&1

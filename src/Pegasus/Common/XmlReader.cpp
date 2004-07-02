4c69f2ee2e2cdc7aa6cbdfdb2d7bc5f2617208e7
a.dunfey <a.dunfey>
2004-07-02 14:40:08 +0000

    BUG#: 1589 TITLE: XML parse error with empty IPARAMVALUE element
    DESCRIPTION:
    
    Merged fix for bugzilla 1578 in the Trunk to the 2.3.2/2.3.3 branch

--- src/Pegasus/Common/XmlReader.cpp
+++ src/Pegasus/Common/XmlReader.cpp
@@ -3944,12 +3944,15 @@ Boolean XmlReader::getIMethodResponseStartTag(
 
 Boolean XmlReader::getIParamValueTag(
     XmlParser& parser, 
-    const char*& name)
+    const char*& name,
+    Boolean& isEmptyTag)
 {
     XmlEntry entry;
 
-    if (!testStartTag(parser, entry, "IPARAMVALUE"))
+    if (!testStartTagOrEmptyTag(parser, entry, "IPARAMVALUE"))
 	return false;
+
+    isEmptyTag = (entry.type == XmlEntry::EMPTY_TAG);
 
     // Get IPARAMVALUE.NAME attribute:
 
@@ -3967,6 +3970,26 @@ Boolean XmlReader::getIParamValueTag(
     }
 
     return true;
+}
+
+//------------------------------------------------------------------------------
+//
+// rejectNullIParamValue()
+//
+//------------------------------------------------------------------------------
+
+void XmlReader::rejectNullIParamValue(
+    XmlParser& parser, 
+    Boolean isEmptyTag,
+    const char* paramName)
+{
+    if (isEmptyTag)
+    {
+        MessageLoaderParms mlParms("Common.XmlReader.INVALID_NULL_IPARAMVALUE",
+            "A null value is not valid for IPARAMVALUE \"$0\".",
+            paramName);
+        throw XmlValidationError(parser.getLine(), mlParms);
+    }
 }
 
 //------------------------------------------------------------------------------
--- src/Pegasus/Common/XmlReader.h
+++ src/Pegasus/Common/XmlReader.h
@@ -362,7 +362,13 @@ public:
 
     static Boolean getIParamValueTag(
 	XmlParser& parser, 
-	const char*& name);
+	const char*& name,
+	Boolean& isEmptyTag);
+
+    static void rejectNullIParamValue(
+        XmlParser& parser,
+        Boolean isEmptyTag,
+        const char* paramName);
 
     static Boolean getPropertyValue(
 	XmlParser& parser, 

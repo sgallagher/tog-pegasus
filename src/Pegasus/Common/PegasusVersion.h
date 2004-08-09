a16cc2a72b49f32c5da418815d57cb27c842e184
dj.gorey <dj.gorey>
2004-08-09 18:25:31 +0000

    BUG#:1785 TITLE:Remove "Experimental" status from PegasusVersion.h
    DESCRIPTION: removed

--- src/Pegasus/Common/PegasusVersion.h
+++ src/Pegasus/Common/PegasusVersion.h
@@ -39,7 +39,7 @@ PEGASUS_NAMESPACE_BEGIN
 
 const char PEGASUS_NAME[] = "CIM Server ";
 const char PEGASUS_VERSION[] = "2.4";
-const char PEGASUS_STATUS[] = "Experimental" ;
+const char PEGASUS_STATUS[] = "" ;
 
 #if defined(PLATFORM_VERSION_SUPPORTED)
 #  if defined(PEGASUS_PLATFORM_HPUX_ACC)

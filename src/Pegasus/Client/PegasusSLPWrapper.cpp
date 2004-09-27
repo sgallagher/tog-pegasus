b3362cf3c9a939fdbb9fea11320275e9a8094723
david.dillard <david.dillard>
2004-09-27 21:38:52 +0000

    BUG#: 1927 TITLE: Implements SLP attrreq
    DESCRIPTION: Implementation by Stuart Naisbitt of VERITAS.  I'm checking in for him.  I made one minor functional change to what he provided me (added an error check after calling strdup()).

--- src/Pegasus/Client/PegasusSLPWrapper.cpp
+++ src/Pegasus/Client/PegasusSLPWrapper.cpp
@@ -25,7 +25,8 @@
 //
 // Author: Tony Fiorentino (fiorentino_tony@emc.com)
 //
-// Modified By:
+// Modified By: Stuart Naisbitt (stuartn@veritas.com)
+//              David Dillard (david.dillard@veritas.com)
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -60,6 +61,111 @@ CIMServerDiscoveryRep::~CIMServerDiscoveryRep()
 {
 }
 
+/*
+ * PegasusSLP supports return of attributes piggybacked on the srvReply,
+ * other implementations may not
+ */
+static BOOL
+_lookup_attrs(const String &save_url, Array<Attribute>& attribs,const SLPClientOptions* options)
+{
+    struct slp_client *client = NULL;
+    lslpMsg responses, *attrReplyEntry;
+    
+    int8 *scopes;
+    int8 *spi;
+    int8 *iface;
+    int8 *addr;
+    int8 *type;
+    int8 *predicate;
+    int16 port;
+    BOOL dir_agent;
+    
+    int8* tags = (int8*)NULL;
+    int16 converge=(int16)0;
+    if(0==save_url.size()){
+        /* Pilot error */
+        return FALSE;
+    }
+
+    char* url=strdup((const char*)save_url.getCString());
+    if (url == NULL)
+    {
+        return FALSE;
+    }
+
+    if((SLPClientOptions*)NULL == options){
+        addr = NULL;
+        iface = NULL;
+        port = 427;
+        scopes = "DEFAULT";
+        spi="DSA";
+        type = "service:wbem";
+        predicate = NULL;
+        dir_agent = FALSE;
+    }else{
+        scopes = (char*)options->scopes;
+        spi = (char*)options->spi;
+        iface = options->local_interface;
+        addr = options->target_address;
+        type = options->service_type;
+        predicate = options->predicate;
+        port = options->target_port;
+        dir_agent = options->use_directory_agent==true?1:0;
+    }
+    /* largely cut-and-paste from slp_attrreq.cpp
+     * with gratuitous reformatting
+     */
+    if(NULL != (client = create_slp_client(
+                    addr,
+                    iface,
+                    port,
+                    spi,
+                    scopes,
+                    FALSE, 
+                    dir_agent))) {
+        if(addr != NULL && inet_addr(addr) == inet_addr("127.0.0.1")) {
+            client->local_attr_req(client, url, scopes, tags);
+        } else if(converge) {
+            client->_convergence = converge ;
+            client->converge_attr_req(client, url, scopes, tags);
+        } else {
+            SOCKADDR_IN address;
+            address.sin_port = htons(port);
+            address.sin_family = AF_INET;
+            if(addr != NULL) {
+                address.sin_addr.s_addr = inet_addr(addr);
+                client->unicast_attr_req(client, url, scopes, tags, &address);
+            }else {
+                address.sin_addr.s_addr = _LSLP_MCAST;
+                client->converge_attr_req(client, url, scopes, tags);
+            }
+        } /* end of request  */
+
+        responses.isHead = TRUE;
+        responses.next = responses.prev = &responses;
+
+        client->get_response(client, &responses);
+        while( ! _LSLP_IS_EMPTY(&responses) ) {
+            attrReplyEntry = responses.next;
+            if(attrReplyEntry->type == attrRep) {
+                if( attrReplyEntry->msg.attrRep.attrListLen > 0) {
+                    String attrString = attrReplyEntry->msg.attrRep.attrList;
+                    _extractAttributes(attrString,attribs);
+                }
+                
+            }/* if we got an attr rply */ 
+            _LSLP_UNLINK(attrReplyEntry);
+            lslpDestroySLPMsg(attrReplyEntry, LSLP_DESTRUCTOR_DYNAMIC);
+        } /* while traversing response list */ 
+
+        destroy_slp_client(client);
+
+     } /* client successfully created */ 
+
+    free(url);
+    return TRUE;
+}/*static BOOL _lookup_attrs()*/
+
 Array<CIMServerDescription>
 CIMServerDiscoveryRep::lookup(const Array<Attribute> & criteria, const SLPClientOptions* options)
 {
@@ -76,37 +182,37 @@ CIMServerDiscoveryRep::lookup(const Array<Attribute> & criteria, const SLPClient
   BOOL dir_agent; // = FALSE;
  
   if((SLPClientOptions*)NULL == options){
-	  addr = NULL;
-	  iface = NULL;
-	  port = 427;
-	  scopes = "DEFAULT";
-	  spi="DSA";
-	  type = "service:wbem";
-	  predicate = NULL;
-	  dir_agent = FALSE;
+      addr = NULL;
+      iface = NULL;
+      port = 427;
+      scopes = "DEFAULT";
+      spi="DSA";
+      type = "service:wbem";
+      predicate = NULL;
+      dir_agent = FALSE;
   }else{
-	  scopes = (char*)options->scopes;
-	  spi = (char*)options->spi;
-	  iface = options->local_interface;
-	  addr = options->target_address;
-	  type = options->service_type;
-	  predicate = options->predicate;
-	  port = options->target_port;
-	  dir_agent = options->use_directory_agent==true?1:0;
-	  options->print();
-	  
+      scopes = (char*)options->scopes;
+      spi = (char*)options->spi;
+      iface = options->local_interface;
+      addr = options->target_address;
+      type = options->service_type;
+      predicate = options->predicate;
+      port = options->target_port;
+      dir_agent = options->use_directory_agent==true?1:0;
+      options->print();
+      
   }
   Array<CIMServerDescription> connections;
   
   if (NULL != (client = create_slp_client(
-				  addr, // target_addr
-				  iface, // local_interface
-				  port, // target_port
-				  spi, // spi
-				  scopes, // scopes
-				  FALSE, // should_listen
-				  dir_agent // use_das
-				  )))
+                  addr, // target_addr
+                  iface, // local_interface
+                  port, // target_port
+                  spi, // spi
+                  scopes, // scopes
+                  FALSE, // should_listen
+                  dir_agent // use_das
+                  )))
     {
       if (addr != NULL && inet_addr(addr) == inet_addr("127.0.0.1"))
         {
@@ -119,9 +225,9 @@ CIMServerDiscoveryRep::lookup(const Array<Attribute> & criteria, const SLPClient
           address.sin_family = AF_INET;
           if (addr != NULL){
             address.sin_addr.s_addr = inet_addr(addr);
-	  }else{
+      }else{
             address.sin_addr.s_addr = _LSLP_MCAST;
-	  }
+      }
           client->unicast_srv_req(client, type, predicate, scopes, &address);
         }
 
@@ -130,7 +236,7 @@ CIMServerDiscoveryRep::lookup(const Array<Attribute> & criteria, const SLPClient
       
       client->get_response(client, &responses);
       while(!_LSLP_IS_EMPTY(&responses))
-        {
+      {
           srvReplyEntry = responses.next;
           if (srvReplyEntry->type == srvRply) 
             {
@@ -149,8 +255,11 @@ CIMServerDiscoveryRep::lookup(const Array<Attribute> & criteria, const SLPClient
                               CIMServerDescription connection(url_list->url);
 
                               Array<Attribute> attributes;
-	
+
                               /* check for attributes */
+                              /* PegasusSLP reurns attributes with srvReply
+                               * per RFC 3059. Other implementations do not.
+                               */
                               if (NULL != url_list->attrs && !_LSLP_IS_HEAD(url_list->attrs->next))
                                 {
                                   lslpAtomList *attrs = url_list->attrs->next;
@@ -159,26 +268,42 @@ CIMServerDiscoveryRep::lookup(const Array<Attribute> & criteria, const SLPClient
                                       _extractAttributes(String(attrs->str), attributes);
                                       attrs = attrs->next;
                                     }
-                                }
-
-                              /* add to connections array */
-                              connection.setAttributes(attributes);
-                              connections.append(connection);
-                            }
-
-                          url_list = url_list->next;
-                        }
-                    }
-                }
-            }
+                                }/*if attrs*/
+                  /* add to connections array */
+                  /* VRTS - interop. Add unconditionally.
+                   * Not initialised in contsructor */
+                  connection.setAttributes(attributes);
+                  connections.append(connection);
+                            }/*if url*/
+              url_list = url_list->next;
+                        }/*while we have urls*/
+                    }/*if urlList*/
+                }/*if srvReply*/
+            }/*if type==srvReply*/
           _LSLP_UNLINK(srvReplyEntry);
           lslpDestroySLPMsg(srvReplyEntry, LSLP_DESTRUCTOR_DYNAMIC);
-        }
+        }/*while*/
       destroy_slp_client(client);
+    }
+
+    /* If no attributes were present, then the attributes array
+     * has zero size. Rescan the connections and create a fresh slp_client to 
+     * get the attributes, if none present
+     */
+    if(0==connections.size()){
+      return connections;
+    }
+    for(int i=0; i< connections.size();i++){
+        Array<Attribute> attrs = connections[i].getAttributes();
+        if(0==attrs.size()){
+            String url=connections[i].getUrl();
+            if(TRUE==_lookup_attrs(url,attrs,options)){
+                connections[i].setAttributes(attrs);
+            }
+        }
     }
 
   return connections;
 }
 
 PEGASUS_NAMESPACE_END
-

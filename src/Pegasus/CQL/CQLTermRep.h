009e91b44e11e50633362c792caa71e85385ce52
david <david>
2004-11-12 17:18:01 +0000

    PEP#: 193 TITLE: CQL Updates
    DESCRIPTION: CQL Updates

--- src/Pegasus/CQL/CQLTermRep.h
+++ src/Pegasus/CQL/CQLTermRep.h
@@ -45,155 +45,43 @@ PEGASUS_NAMESPACE_BEGIN
 class PEGASUS_CQL_LINKAGE CQLFactory;
 class PEGASUS_CQL_LINKAGE QueryContext;
 
-/**
-    The CQLTerm class encapsulates a generic CQL term to make it 
-    easier to break into pieces (factors) and process the term.  
-    A CQL term is made up of factors and operators. 
-    There must be exactly one more factor than there are operators.
-*/
 class PEGASUS_CQL_LINKAGE CQLTermRep
 {
  public:
   
-  /** 
-      Contructs CQLTermRep default object.
-      
-      @param  - None.
-      @return - None.
-      @throw  - None.
-      @experimental
-  */
   CQLTermRep();
 
-  /** 
-      Contructs CQLTermRep from a CQLFactor object.
-      
-      @param  - theFactor is a CQLFactor object.
-      @return - None.
-      @throw  - None.
-      @experimental
-  */
   CQLTermRep(const CQLFactor & theFactor);
 
-  /** 
-      Contructs CQLTermRep from a CQLTermRep object. (copy-constructor)
-      
-      @param  - rep is a CQLTermRep object.
-      @return - None.
-      @throw  - None.
-      @experimental
-  */
   CQLTermRep(const CQLTermRep& rep);
 
-  /** 
-      Destroyes CQLTermRep object.
-     
-      @param  - None.
-      @return - None.
-      @throw  - None.
-      @experimental
-  */
   ~CQLTermRep();
 
-  /** the resolveValue method evaluates the CQL Term and returns the value.
-      Any property that need to be resolved into a value is taken from the
-      CIMInstance.
-      
-      @param  - CI - The CIMInstance used for the evaluate.
-      @param  - QueryCtx - Reference to the Querycontext object.
-      @return - CQLValue - The value of the object being resolved.
-      @throw  - CQLRuntimeException 
-      @experimental
-  */
   CQLValue resolveValue(const CIMInstance& CI, const QueryContext& QueryCtx);
 
-  /** The function is used by Bison.
-      It is invoked 0 or more times for the CQLTerm, and
-      when invoked will always pass in an integer that is the Factor operation
-      type and a CQLFactor object.
-     
-      @param  - inFactorOpType - next operation to be performed.
-      @param  - inFactor - next CQLFactor to be operated on.
-      @return - None.
-      @throw  - None.
-      @experimental
-  */
-    void appendOperation(FactorOpType inFactorOpType, CQLFactor inFactor);
+  void appendOperation(FactorOpType inFactorOpType, CQLFactor inFactor);
 
-  /** 
-      Converts the contents of the object to a String.
-      
-      @param  - None.
-      @return - A string version of the object.
-      @throw  - None.
-      @experimental
-  */
    String toString()const;
 
-  /** 
-      Returns true if the object is simple.  Simple is defined as only having
-      a one CQLFactor object. The CQLFactor object could be complex.
-      
-      @param  - None.
-      @return - true or false
-      @throw  - None.
-      @experimental
-  */
    Boolean isSimple()const;
 
-  /** 
-      Returns true if the value within the object is Simple.  In this case
-      the object only has one CQLFactor object.  This CQLFactor object in 
-      not a complex object.
-      
-      @param  - None.
-      @return - true or false
-      @throw  - None.
-      @experimental
-  */
    Boolean isSimpleValue()const;
 
-  /** 
-      Will return an array of CQLFactors.
-      
-      @param  - None.
-      @return - An array of CQLFactor
-      @throw  - None.
-      @experimental
-  */
    Array<CQLFactor> getFactors()const;
 
-  /** 
-      Will return an array of Operations.
-      
-      @param  - None.
-      @return - an array of FactorOpType
-      @throw  - None.
-      @experimental
-  */
    Array<FactorOpType> getOperators()const;
 
-  /** 
-      Calling applyContext function for every internal object.  This
-      will fully qualify the Chained Identifiers within all the CQLValue objects.
-      
-      @param  - inContext - Query Context used to access the repository.
-      @param  - inCid - Chained Identifier used for standalone symbolic constants.
-      @return - None.
-      @throw  - None.
-      @experimental
-  */
    void applyContext(QueryContext& inContext,
                      CQLChainedIdentifier& inCid); 
-
+   
    friend class CQLFactory;
-
-  private:
-    
-    Array<FactorOpType> _FactorOperators;
-
-    Array<CQLFactor> _Factors;
-
+   
+ private:
+   
+   Array<FactorOpType> _FactorOperators;
+   
+   Array<CQLFactor> _Factors;
+   
 };
 
 PEGASUS_NAMESPACE_END

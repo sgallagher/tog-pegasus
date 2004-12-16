7f46280e71310636a70fe930c72366cb23d49296
a.dunfey <a.dunfey>
2004-12-16 20:58:59 +0000

    BUG#: 2293 TITLE: Added missing ProviderIdContainer, LocaleContainer assignment operators
    DESCRIPTION:
    
    Put fix from 2.5 back into 2.4.1

--- src/Pegasus/Common/OperationContextInternal.cpp
+++ src/Pegasus/Common/OperationContextInternal.cpp
@@ -1,41 +1,37 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2004////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Chip Vincent (cvincent@us.ibm.com)
 //
-//////////////////////////////////////////////////////////////////////////
+// Modified By:
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
 #include "OperationContextInternal.h"
-
-#if defined(PEGASUS_PLATFORM_LINUX_GENERIC_GNU) || \
-    defined(PEGASUS_PLATFORM_DARWIN_PPC_GNU) || \
-    defined(PEGASUS_PLATFORM_DARWIN_IX86_GNU)
-# define PEGASUS_INCLUDE_SUPERCLASS_INITIALIZER
-#endif
 
 PEGASUS_NAMESPACE_BEGIN
 
@@ -45,11 +41,11 @@ PEGASUS_NAMESPACE_BEGIN
 
 const String LocaleContainer::NAME = "LocaleContainer";
 
-LocaleContainer::LocaleContainer(const OperationContext::Container& container)
+LocaleContainer::LocaleContainer(const OperationContext::Container & container)
 {
-    const LocaleContainer* p = dynamic_cast<const LocaleContainer*>(&container);
+    const LocaleContainer * p = dynamic_cast<const LocaleContainer *>(&container);
 
-    if (p == 0)
+    if(p == 0)
     {
         throw DynamicCastFailedException();
     }
@@ -57,45 +53,44 @@ LocaleContainer::LocaleContainer(const OperationContext::Container& container)
     *this = *p;
 }
 
-LocaleContainer::LocaleContainer(const String& languageId)
+LocaleContainer::LocaleContainer(const String & languageId)
 {
     _languageId = languageId;
 }
 
-LocaleContainer::~LocaleContainer()
+LocaleContainer::~LocaleContainer(void)
 {
 }
 
-LocaleContainer& LocaleContainer::operator=(const LocaleContainer&container)
+LocaleContainer &LocaleContainer::operator=(const LocaleContainer &container)
 {
-    if (this == &container)
-    {
-        return *this;
-    }
+	if (this == &container)
+	{
+		return (*this);
+	}
 
-    _languageId = container._languageId;
-
-    return *this;
+	_languageId = container._languageId;
+	return (*this);
 }
 
-String LocaleContainer::getName() const
+String LocaleContainer::getName(void) const
 {
-    return NAME;
+    return(NAME);
 }
 
-OperationContext::Container* LocaleContainer::clone() const
+OperationContext::Container * LocaleContainer::clone(void) const
 {
-    return new LocaleContainer(*this);
+    return(new LocaleContainer(*this));
 }
 
-void LocaleContainer::destroy()
+void LocaleContainer::destroy(void)
 {
     delete this;
 }
 
-String LocaleContainer::getLanguageId() const
+String LocaleContainer::getLanguageId(void) const
 {
-    return _languageId;
+    return(_languageId);
 }
 
 //
@@ -104,13 +99,11 @@ String LocaleContainer::getLanguageId() const
 
 const String ProviderIdContainer::NAME = "ProviderIdContainer";
 
-ProviderIdContainer::ProviderIdContainer(
-    const OperationContext::Container& container)
+ProviderIdContainer::ProviderIdContainer(const OperationContext::Container & container)
 {
-    const ProviderIdContainer* p =
-        dynamic_cast<const ProviderIdContainer*>(&container);
+    const ProviderIdContainer * p = dynamic_cast<const ProviderIdContainer *>(&container);
 
-    if (p == 0)
+    if(p == 0)
     {
         throw DynamicCastFailedException();
     }
@@ -118,286 +111,68 @@ ProviderIdContainer::ProviderIdContainer(
     *this = *p;
 }
 
-ProviderIdContainer::ProviderIdContainer(
-    const CIMInstance& module,
-    const CIMInstance& provider,
-    Boolean isRemoteNameSpace,
-    const String& remoteInfo)
-    : _module(module),
-    _provider(provider),
-    _isRemoteNameSpace(isRemoteNameSpace),
-    _remoteInfo(remoteInfo)
+ProviderIdContainer::ProviderIdContainer(const CIMInstance & module, const CIMInstance & provider,
+        Boolean remoteNameSpace, String remoteInfo)
+{
+    _module = module;
+    _provider = provider;
+    _remoteNameSpace=remoteNameSpace;
+    _remoteInfo=remoteInfo;
+}
+
+ProviderIdContainer::~ProviderIdContainer(void)
 {
 }
 
-ProviderIdContainer::~ProviderIdContainer()
-{
-}
-
-ProviderIdContainer& ProviderIdContainer::operator=(
-    const ProviderIdContainer& container)
+ProviderIdContainer & ProviderIdContainer::operator=(
+    const ProviderIdContainer & container)
 {
     if (this == &container)
     {
-        return *this;
+        return (*this);
     }
 
     _module = container._module;
     _provider = container._provider;
-    _isRemoteNameSpace = container._isRemoteNameSpace;
+    _remoteNameSpace = container._remoteNameSpace;
     _remoteInfo = container._remoteInfo;
-    _provMgrPath = container._provMgrPath;
 
-    return *this;
+    return (*this);
 }
 
-String ProviderIdContainer::getName() const
+String ProviderIdContainer::getName(void) const
 {
-    return NAME;
+    return(NAME);
 }
 
-OperationContext::Container* ProviderIdContainer::clone() const
+OperationContext::Container * ProviderIdContainer::clone(void) const
 {
-    return new ProviderIdContainer(*this);
+    return(new ProviderIdContainer(*this));
 }
 
-void ProviderIdContainer::destroy()
-{
-    delete this;
-}
-
-CIMInstance ProviderIdContainer::getModule() const
-{
-    return _module;
-}
-
-CIMInstance ProviderIdContainer::getProvider() const
-{
-    return _provider;
-}
-
-Boolean ProviderIdContainer::isRemoteNameSpace() const
-{
-    return _isRemoteNameSpace;
-}
-
-String ProviderIdContainer::getRemoteInfo() const
-{
-    return _remoteInfo;
-}
-
-String ProviderIdContainer::getProvMgrPath() const
-{
-    return _provMgrPath;
-}
-
-void ProviderIdContainer::setProvMgrPath(const String &path)
-{
-    _provMgrPath = path;
-}
-
-//
-// CachedClassDefinitionContainer
-//
-
-const String CachedClassDefinitionContainer::NAME =
-    "CachedClassDefinitionContainer";
-
-CachedClassDefinitionContainer::CachedClassDefinitionContainer(
-    const OperationContext::Container& container)
-{
-    const CachedClassDefinitionContainer* p =
-        dynamic_cast<const CachedClassDefinitionContainer*>(&container);
-
-    if (p == 0)
-    {
-        throw DynamicCastFailedException();
-    }
-
-    *this = *p;
-}
-
-
-CachedClassDefinitionContainer::CachedClassDefinitionContainer(
-    const CIMConstClass& cimClass)
-    : _cimClass(cimClass)
-{
-}
-
-CachedClassDefinitionContainer::~CachedClassDefinitionContainer()
-{
-}
-
-CachedClassDefinitionContainer& CachedClassDefinitionContainer::operator=(
-    const CachedClassDefinitionContainer& container)
-{
-    if (this == &container)
-    {
-        return *this;
-    }
-
-    _cimClass = container._cimClass;
-
-    return *this;
-}
-
-String CachedClassDefinitionContainer::getName() const
-{
-    return NAME;
-}
-
-OperationContext::Container* CachedClassDefinitionContainer::clone() const
-{
-    return new CachedClassDefinitionContainer(*this);
-}
-
-void CachedClassDefinitionContainer::destroy()
+void ProviderIdContainer::destroy(void)
 {
     delete this;
 }
 
-CIMConstClass CachedClassDefinitionContainer::getClass() const
+CIMInstance ProviderIdContainer::getModule(void) const
 {
-    return _cimClass;
+    return(_module);
 }
 
-//
-// NormalizerContextContainer
-//
-
-const String NormalizerContextContainer::NAME = "NormalizerContextContainer";
-
-NormalizerContextContainer::NormalizerContextContainer(
-    const OperationContext::Container& container)
+CIMInstance ProviderIdContainer::getProvider(void) const
 {
-    const NormalizerContextContainer* p =
-        dynamic_cast<const NormalizerContextContainer*>(&container);
-
-    if (p == 0)
-    {
-        throw DynamicCastFailedException();
-    }
-
-    *this = *p;
+    return(_provider);
 }
 
-
-NormalizerContextContainer::NormalizerContextContainer(
-    AutoPtr<NormalizerContext>& context) : normalizerContext(context.get())
+Boolean ProviderIdContainer::isRemoteNameSpace(void) const
 {
-  context.release();
+    return(_remoteNameSpace);
 }
 
-NormalizerContextContainer::NormalizerContextContainer(
-    const NormalizerContextContainer& container)
-#ifdef PEGASUS_INCLUDE_SUPERCLASS_INITIALIZER
-    : OperationContext::Container()
-#endif
+const String & ProviderIdContainer::getRemoteInfo(void) const
 {
-    if (this != &container)
-    {
-        normalizerContext.reset(container.normalizerContext->clone());
-    }
+    return(_remoteInfo);
 }
-
-NormalizerContextContainer::~NormalizerContextContainer()
-{
-}
-
-NormalizerContextContainer& NormalizerContextContainer::operator=(
-    const NormalizerContextContainer& container)
-{
-    if (this == &container)
-    {
-        return *this;
-    }
-
-    normalizerContext.reset(container.normalizerContext->clone());
-
-    return *this;
-}
-
-String NormalizerContextContainer::getName() const
-{
-    return NAME;
-}
-
-OperationContext::Container* NormalizerContextContainer::clone() const
-{
-    return new NormalizerContextContainer(*this);
-}
-
-void NormalizerContextContainer::destroy()
-{
-    delete this;
-}
-
-NormalizerContext* NormalizerContextContainer::getContext() const
-{
-    return normalizerContext.get();
-}
-
-//
-// UserRoleContainer
-//
-
-const String UserRoleContainer::NAME = "UserRoleContainer";
-
-UserRoleContainer::UserRoleContainer(
-    const OperationContext::Container& container)
-{
-    const UserRoleContainer* p =
-        dynamic_cast<const UserRoleContainer*>(&container);
-
-    if (p == 0)
-    {
-        throw DynamicCastFailedException();
-    }
-
-    *this = *p;
-}
-
-UserRoleContainer::UserRoleContainer(const String& userRole)
-{
-    _userRole = userRole;
-}
-
-UserRoleContainer::~UserRoleContainer()
-{
-}
-
-UserRoleContainer& UserRoleContainer::operator=(
-    const UserRoleContainer&container)
-{
-    if (this == &container)
-    {
-        return *this;
-    }
-
-    _userRole = container._userRole;
-
-    return *this;
-}
-
-String UserRoleContainer::getName() const
-{
-    return NAME;
-}
-
-OperationContext::Container* UserRoleContainer::clone() const
-{
-    return new UserRoleContainer(*this);
-}
-
-void UserRoleContainer::destroy()
-{
-    delete this;
-}
-
-String UserRoleContainer::getUserRole() const
-{
-    return _userRole;
-}
-
 
 PEGASUS_NAMESPACE_END

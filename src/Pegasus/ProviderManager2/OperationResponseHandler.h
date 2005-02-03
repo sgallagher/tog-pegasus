7c031b2da77bec063ac816443fe53ab1ba614940
chip <chip>
2005-02-03 04:17:46 +0000

    PEP#: 211 TITLE: ObjectNormalization enhancements
    DESCRIPTION: Update GetInstance, EnumerateInstances, and
    EnumerateInstanceNames reponse handlers to normalize objects
    as they are delivered, if enabled. Some code is commented out
    until this function is complete.

--- src/Pegasus/ProviderManager2/OperationResponseHandler.h
+++ src/Pegasus/ProviderManager2/OperationResponseHandler.h
@@ -52,6 +52,10 @@
 #include <Pegasus/Common/CIMIndication.h>
 #include <Pegasus/Common/CIMValue.h>
 
+#include <Pegasus/Common/OperationContext.h>
+#include <Pegasus/Common/OperationContextInternal.h>
+
+#include <Pegasus/Common/ObjectNormalizer.h>
 #include <Pegasus/Common/ResponseHandler.h>
 #include <Pegasus/Common/Logger.h>
 #include <Pegasus/Common/XmlWriter.h>
@@ -102,7 +106,6 @@ public:
     }
 
 protected:
-
     // the default for all derived handlers. Some handlers may not apply
     // async behavior because their callers cannot handle partial responses.
     virtual Boolean isAsync(void) const
@@ -162,7 +165,41 @@ public:
         CIMGetInstanceResponseMessage * response)
     : OperationResponseHandler(request, response)
     {
+        #ifdef PEGASUS_ENABLE_OBJECT_NORMALIZATION
+        // Attempt to get the cached class definition used to validate results of this
+        // operation. If it does not exist, then this feature is disabled for this
+        // operation.
+        try
+        {
+            CachedClassDefinitionContainer container =
+                request->operationContext.get(CachedClassDefinitionContainer::NAME);
+
+            CIMClass cimClass = container.getClass();
+
+            _normalizer =
+                ObjectNormalizer(
+                    cimClass,
+                    request->localOnly,
+                    request->includeQualifiers,
+                    request->includeClassOrigin);
+        }
+        catch(Exception &)
+        {
+            // Do nothing. Container is missing, which implies normalization is disabled
+            // for this operation.
+        }
+        #endif
     }
+
+    /*
+    #ifdef PEGASUS_ENABLE_OBJECT_NORMALIZATION
+    virtual void deliver(const CIMInstance & cimInstance)
+    {
+        // Normalize (if seeded, otherwise do nothing) and pass to subclass method for delivery.
+        SimpleInstanceResponseHandler::deliver(_normalizer.processInstance(cimInstance));
+    }
+    #endif
+    */
 
     virtual String getClass(void) const
     {
@@ -189,6 +226,10 @@ public:
             setStatus(CIM_ERR_NOT_FOUND);
         }
     }
+
+private:
+    ObjectNormalizer _normalizer;
+
 };
 
 class EnumerateInstancesResponseHandler : public OperationResponseHandler, public SimpleInstanceResponseHandler
@@ -199,7 +240,41 @@ public:
         CIMEnumerateInstancesResponseMessage * response)
     : OperationResponseHandler(request, response)
     {
+        #ifdef PEGASUS_ENABLE_OBJECT_NORMALIZATION
+        // Attempt to get the cached class definition used to validate results of this
+        // operation. If it does not exist, then this feature is disabled for this
+        // operation.
+        try
+        {
+            CachedClassDefinitionContainer container =
+                request->operationContext.get(CachedClassDefinitionContainer::NAME);
+
+            CIMClass cimClass = container.getClass();
+
+            _normalizer =
+                ObjectNormalizer(
+                    cimClass,
+                    request->localOnly,
+                    request->includeQualifiers,
+                    request->includeClassOrigin);
+        }
+        catch(Exception &)
+        {
+            // Do nothing. Container is missing, which implies normalization is disabled
+            // for this operation.
+        }
+        #endif
     }
+
+    /*
+    #ifdef PEGASUS_ENABLE_OBJECT_NORMALIZATION
+    virtual void deliver(const CIMInstance & cimInstance)
+    {
+        // Normalize (if seeded, otherwise do nothing) and pass to subclass method for delivery.
+        SimpleInstanceResponseHandler::deliver(_normalizer.processInstance(cimInstance));
+    }
+    #endif
+    */
 
     virtual String getClass(void) const
     {
@@ -213,6 +288,10 @@ public:
 
         msg.cimNamedInstances = getObjects();
     }
+
+private:
+    ObjectNormalizer _normalizer;
+
 };
 
 class EnumerateInstanceNamesResponseHandler : public OperationResponseHandler, public SimpleObjectPathResponseHandler
@@ -223,7 +302,41 @@ public:
         CIMEnumerateInstanceNamesResponseMessage * response)
     : OperationResponseHandler(request, response)
     {
+        #ifdef PEGASUS_ENABLE_OBJECT_NORMALIZATION
+        // Attempt to get the cached class definition used to validate results of this
+        // operation. If it does not exist, then this feature is disabled for this
+        // operation.
+        try
+        {
+            CachedClassDefinitionContainer container =
+                request->operationContext.get(CachedClassDefinitionContainer::NAME);
+
+            CIMClass cimClass = container.getClass();
+
+            _normalizer =
+                ObjectNormalizer(
+                    cimClass,
+                    false,
+                    false,
+                    false);
+        }
+        catch(Exception &)
+        {
+            // Do nothing. Container is missing, which implies normalization is disabled
+            // for this operation.
+        }
+        #endif
     }
+
+    /*
+    #ifdef PEGASUS_ENABLE_OBJECT_NORMALIZATION
+    virtual void deliver(const CIMObjectPath & cimObjectPath)
+    {
+        // Normalize (if seeded, otherwise do nothing) and pass to subclass method for delivery.
+        SimpleObjectPathResponseHandler::deliver(_normalizer.processInstanceObjectPath(cimObjectPath));
+    }
+    #endif
+    */
 
     virtual String getClass(void) const
     {
@@ -237,6 +350,10 @@ public:
 
         msg.instanceNames = getObjects();
     }
+
+private:
+    ObjectNormalizer _normalizer;
+
 };
 
 class CreateInstanceResponseHandler : public OperationResponseHandler, public SimpleObjectPathResponseHandler

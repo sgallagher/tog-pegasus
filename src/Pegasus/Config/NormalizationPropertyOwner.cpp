0f84610a01b26b0bf65b1ca37652472e3d4ab9bf
chip <chip>
2005-02-03 00:45:25 +0000

    PEP#: 211 TITLE: ObjectNormalization enhancements
    DESCRIPTION: Add NormalizationPropertyOwner to pegconfig. This just checks
    in the files, but does not build or link them. That will be done within the next day
    once I've done some more testing.

---
+++ src/Pegasus/Config/NormalizationPropertyOwner.cpp
@@ -0,0 +1,213 @@
+//%2004////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author:  Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#include "NormalizationPropertyOwner.h"
+
+PEGASUS_NAMESPACE_BEGIN
+
+static struct ConfigPropertyRow properties[] =
+{
+    { "enableNormalization", "false", 0, 0, 0, 1 },
+    { "excludeModulesFromNormalization", "", 0, 0, 0, 1 }
+};
+
+const Uint32 NUM_PROPERTIES = sizeof(properties) / sizeof(properties[0]);
+
+NormalizationPropertyOwner::NormalizationPropertyOwner(void)
+{
+    _providerObjectNormalizationEnabled.reset(new ConfigProperty());
+    _providerObjectNormalizationModuleExclusions.reset(new ConfigProperty());
+}
+
+void NormalizationPropertyOwner::initialize(void)
+{
+    for(Uint8 i = 0; i < NUM_PROPERTIES; i++)
+    {
+        if(String::equalNoCase(properties[i].propertyName, "enableNormalization"))
+        {
+            _providerObjectNormalizationEnabled->propertyName = properties[i].propertyName;
+            _providerObjectNormalizationEnabled->defaultValue = properties[i].defaultValue;
+            _providerObjectNormalizationEnabled->currentValue = properties[i].defaultValue;
+            _providerObjectNormalizationEnabled->plannedValue = properties[i].defaultValue;
+            _providerObjectNormalizationEnabled->dynamic = properties[i].dynamic;
+            _providerObjectNormalizationEnabled->domain = properties[i].domain;
+            _providerObjectNormalizationEnabled->domainSize = properties[i].domainSize;
+            _providerObjectNormalizationEnabled->externallyVisible = properties[i].externallyVisible;
+        }
+        else if(String::equalNoCase(properties[i].propertyName, "excludeModulesFromNormalization"))
+        {
+            _providerObjectNormalizationModuleExclusions->propertyName = properties[i].propertyName;
+            _providerObjectNormalizationModuleExclusions->defaultValue = properties[i].defaultValue;
+            _providerObjectNormalizationModuleExclusions->currentValue = properties[i].defaultValue;
+            _providerObjectNormalizationModuleExclusions->plannedValue = properties[i].defaultValue;
+            _providerObjectNormalizationModuleExclusions->dynamic = properties[i].dynamic;
+            _providerObjectNormalizationModuleExclusions->domain = properties[i].domain;
+            _providerObjectNormalizationModuleExclusions->domainSize = properties[i].domainSize;
+            _providerObjectNormalizationModuleExclusions->externallyVisible = properties[i].externallyVisible;
+        }
+    }
+}
+
+void NormalizationPropertyOwner::getPropertyInfo(const String & name, Array<String> & propertyInfo)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    propertyInfo.clear();
+
+    propertyInfo.append(configProperty->propertyName);
+    propertyInfo.append(configProperty->defaultValue);
+    propertyInfo.append(configProperty->currentValue);
+    propertyInfo.append(configProperty->plannedValue);
+
+    if(configProperty->dynamic)
+    {
+        propertyInfo.append(STRING_TRUE);
+    }
+    else
+    {
+        propertyInfo.append(STRING_FALSE);
+    }
+
+    if(configProperty->externallyVisible)
+    {
+        propertyInfo.append(STRING_TRUE);
+    }
+    else
+    {
+        propertyInfo.append(STRING_FALSE);
+    }
+}
+
+const String NormalizationPropertyOwner::getDefaultValue(const String & name)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    return(configProperty->defaultValue);
+}
+
+const String NormalizationPropertyOwner::getCurrentValue(const String & name)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    return(configProperty->currentValue);
+}
+
+const String NormalizationPropertyOwner::getPlannedValue(const String & name)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    return(configProperty->plannedValue);
+}
+
+void NormalizationPropertyOwner::initCurrentValue(const String & name, const String & value)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    configProperty->currentValue = value;
+}
+
+void NormalizationPropertyOwner::initPlannedValue(const String & name, const String & value)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    configProperty->plannedValue = value;
+}
+
+void NormalizationPropertyOwner::updateCurrentValue(const String & name, const String & value)
+{
+    // make sure the property is dynamic before updating the value.
+    if(!isDynamic(name))
+    {
+        throw NonDynamicConfigProperty(name);
+    }
+
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    configProperty->currentValue = value;
+}
+
+void NormalizationPropertyOwner::updatePlannedValue(const String & name, const String & value)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    configProperty->plannedValue = value;
+}
+
+Boolean NormalizationPropertyOwner::isValid(const String & name, const String & value)
+{
+    if(String::equalNoCase(name, "enableNormalization"))
+    {
+        // valid values are "true" and "false"
+        if(String::equal(value, "true") || String::equal(value, "false"))
+        {
+            return(true);
+        }
+    }
+    else if(String::equalNoCase(name, "excludeModulesFromNormalization"))
+    {
+        // valid values must be in the form "n.n.n"
+
+        // TODO: validate value
+
+        return(true);
+    }
+
+    return(false);
+}
+
+Boolean NormalizationPropertyOwner::isDynamic(const String & name)
+{
+    struct ConfigProperty * configProperty = _lookupConfigProperty(name);
+
+    return(configProperty->dynamic);
+}
+
+struct ConfigProperty * NormalizationPropertyOwner::_lookupConfigProperty(const String & name)
+{
+    if(String::equalNoCase(name, _providerObjectNormalizationEnabled->propertyName))
+    {
+        return(_providerObjectNormalizationEnabled.get());
+    }
+    else if(String::equalNoCase(name, _providerObjectNormalizationModuleExclusions->propertyName))
+    {
+        return(_providerObjectNormalizationModuleExclusions.get());
+    }
+    else
+    {
+        throw UnrecognizedConfigProperty(name);
+    }
+
+    return(0);
+}
+
+PEGASUS_NAMESPACE_END
---
+++ src/Pegasus/Config/NormalizationPropertyOwner.h
@@ -0,0 +1,174 @@
+//%2004////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#ifndef Pegasus_NormalizationPropertyOwner_h
+#define Pegasus_NormalizationPropertyOwner_h
+
+#include <Pegasus/Config/ConfigPropertyOwner.h>
+
+#include <Pegasus/Common/AutoPtr.h>
+
+PEGASUS_NAMESPACE_BEGIN
+
+/**
+    This is NormalizationPropertyOwner class that extends ConfigPropertyOwner
+    class.
+*/
+class PEGASUS_CONFIG_LINKAGE NormalizationPropertyOwner : public ConfigPropertyOwner
+{
+public:
+    NormalizationPropertyOwner(void);
+
+    /**
+    Initialize the config properties.
+
+    This method is expected to be called only once at the start of the
+    CIMOM. It initializes the properties with the default values.
+    */
+    void initialize(void);
+
+    /**
+    Get information about the specified property.
+
+    @param propertyName   The name of the property.
+    @param propertyInfo   List to store the property info.
+    @exception UnrecognizedConfigProperty  if the property is not defined.
+    */
+    void getPropertyInfo(const String & name, Array<String> & propertyInfo);
+
+    /**
+    Get default value of the specified property.
+
+    @param  name         The name of the property.
+    @return string containing the default value of the property specified.
+    @exception UnrecognizedConfigProperty  if the property is not defined.
+    */
+    const String getDefaultValue(const String & name);
+
+    /**
+    Get current value of the specified property.
+
+    @param  name         The name of the property.
+    @return string containing the currnet value of the property specified.
+    @exception UnrecognizedConfigProperty  if the property is not defined.
+    */
+    const String getCurrentValue(const String & name);
+
+    /**
+    Get planned value of the specified property.
+
+    @param  name         The name of the property.
+    @return string containing the planned value of the property specified.
+    @exception UnrecognizedConfigProperty  if the property is not defined.
+    */
+    const String getPlannedValue(const String & name);
+
+    /**
+    Init current value of the specified property to the specified value.
+    This method is expected to be called only once at the start of the
+    CIMOM. The property value will be initialized irrespective of whether
+    the property is dynamic or not.
+
+    @param  name         The name of the property.
+    @param  value        The current value of the property.
+    @exception     UnrecognizedConfigProperty  if the property is not defined.
+    @exception     InvalidPropertyValue  if the property value is not valid.
+    */
+    void initCurrentValue(const String & name, const String & value);
+
+    /**
+    Init planned value of the specified property to the specified value.
+    This method is expected to be called only once at the start of the
+    CIMOM. The property value will be initialized irrespective of whether
+    the property is dynamic or not.
+
+    @param  name         The name of the property.
+    @param  value        The planned value of the property.
+    @exception     UnrecognizedConfigProperty  if the property is not defined.
+    @exception     InvalidPropertyValue  if the property value is not valid.
+    */
+    void initPlannedValue(const String & name, const String & value);
+
+    /**
+    Update current value of the specified property to the specified value.
+    The property value will be updated only if the property is dynamically
+    updatable.
+
+    @param  name         The name of the property.
+    @param  value        The current value of the property.
+    @exception     NonDynamicConfigProperty  if the property is not dynamic.
+    @exception     InvalidPropertyValue  if the property value is not valid.
+    @exception     UnrecognizedConfigProperty  if the property is not defined.
+    */
+    void updateCurrentValue(const String & name, const String & value);
+
+    /**
+    Update planned value of the specified property to the specified value.
+
+    @param  name         The name of the property.
+    @param  value        The planned value of the property.
+    @exception     InvalidPropertyValue  if the property value is not valid.
+    @exception     UnrecognizedConfigProperty  if the property is not defined.
+    */
+    void updatePlannedValue(const String & name, const String & value);
+
+    /**
+    Checks to see if the given value is valid or not.
+
+    @param  name         The name of the property.
+    @param  value        The value of the property to be validated.
+    @return Boolean      True if the specified value for the property is valid.
+    @exception UnrecognizedConfigProperty  if the property is not defined.
+    */
+    Boolean isValid(const String & name, const String & value);
+
+    /**
+    Checks to see if the specified property is dynamic or not.
+
+    @param  name         The name of the property.
+    @return Boolean      True if the specified property is dynamic.
+    @exception UnrecognizedConfigProperty  if the property is not defined.
+    */
+    Boolean isDynamic(const String & name);
+
+private:
+    struct ConfigProperty * _lookupConfigProperty(const String & name);
+
+    AutoPtr<struct ConfigProperty> _providerObjectNormalizationEnabled;
+    AutoPtr<struct ConfigProperty> _providerObjectNormalizationModuleExclusions;
+
+};
+
+PEGASUS_NAMESPACE_END
+
+#endif

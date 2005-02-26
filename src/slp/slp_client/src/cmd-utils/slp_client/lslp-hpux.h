3e059f014a13c28382d17c9cd6189572de99f56d
david.dillard <david.dillard>
2005-02-26 06:11:40 +0000

    BUG#: 2052 TITLE: File needed for HP-UX.
    DESCRIPTION: HP-UX specific file needed for SLP.

---
+++ src/slp/slp_client/src/cmd-utils/slp_client/lslp-hpux.h
@@ -0,0 +1,89 @@
+//%2005////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+/*****************************************************************************
+ *  Description:   portability routines for nucleus RTOS
+ *
+ *  Originated: December 17, 2001 
+ *	Original Author: Mike Day md@soft-hackle.net
+ *                                mdday@us.ibm.com 
+ *  $Header: /cvs/MSB/pegasus/src/slp/slp_client/src/cmd-utils/slp_client/lslp-hpux.h,v 1.1 2005/02/26 06:11:40 david.dillard Exp $ 	                                                            
+ *               					                    
+ *  Copyright (c) 2001 - 2003  IBM                                          
+ *  Copyright (c) 2000 - 2003 Michael Day                                    
+ *                                                                           
+ *  Permission is hereby granted, free of charge, to any person obtaining a  
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation 
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ *  and/or sell copies of the Software, and to permit persons to whom the     
+ *  Software is furnished to do so, subject to the following conditions:       
+ * 
+ *  The above copyright notice and this permission notice shall be included in 
+ *  all copies or substantial portions of the Software.
+ * 
+ * 
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ *  DEALINGS IN THE SOFTWARE.
+ *
+ *****************************************************************************/
+
+#ifndef _LSLP_HPUX_INCL_
+#define LSLP_HPUX_INCL_
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+/*** unix include files ***/
+#include <netdb.h>
+
+/*** typedefs ***/
+typedef size_t socklen_t;
+
+/*** defines ***/
+#define _LSLP_SETSOCKOPT(a, b, c, d, e) \
+           setsockopt((int)(a), (int)(b), (int)(c), (const char *)(d), (socklen_t)(e))
+#define _LSLP_SENDTO(a, b, c, d, e, f) \
+           sendto((int)(a), (const char *)(b), (size_t)(c), (int)(d), \
+                  (const struct sockaddr *)(e), (socklen_t)(f))
+
+#define _LSLP_RECV_FROM(a, b, c, d, e, f) \
+           recvfrom((int)(a), (char *)(b), (size_t)(c), (int)(d), \
+                    (struct sockaddr *)(e), (int *)(f))
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif /* unix include */

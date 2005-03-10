101346842ccc07fd6dbe59c5d3463e76849e1c85
kumpf <kumpf>
2005-03-10 04:01:17 +0000

    BUG#: 3003 TITLE: CIMDateTime::getCurrentDateTime returns wrong UTC sign on Linux DESCRIPTION: Fixed the Linux UTC offset.  Attempted to fix the HP-UX UTC offset.  Consolidated the getCurrentDateTime code to reduce platform-specific logic.

--- src/Pegasus/Common/CIMDateTimeUnix.cpp
+++ src/Pegasus/Common/CIMDateTimeUnix.cpp
@@ -29,120 +29,71 @@
 //
 // Author: Sushma Fernandes, Hewlett Packard Company (sushma_fernandes@hp.com)
 //
-//%////////////////////////////////////////////////////////////////////////////
-
-
-///////////////////////////////////////////////////////////////////////////////
-// 
-// DateTime 
+// Modified By: Roger Kumpf, Hewlett Packard Company (roger_kumpf@hp.com)
 //
-///////////////////////////////////////////////////////////////////////////////
+//%////////////////////////////////////////////////////////////////////////////
 
 #include <Pegasus/Common/CIMDateTime.h>
 
 #include <sys/time.h>
 #include <cstring>
 
-typedef struct Timestamp {
-    char year[4];
-    char month[2];
-    char day[2];
-    char hour[2];
-    char minutes[2];
-    char seconds[2];
-    char dot;
-    char microSeconds[6];
-    char plusOrMinus;
-    char utcOffset[3];
-    char padding[3];
-} Timestamp_t;
-
 PEGASUS_USING_STD;
 
 PEGASUS_NAMESPACE_BEGIN
 
-//
-// Returns the local time.
-// Note: Original code was taken from OperatingSystem::getLocalDateTime ()
-//
 CIMDateTime CIMDateTime::getCurrentDateTime()
 {
-    CIMDateTime 	currentDateTime;
-    int         	year;
-    Timestamp_t  	dateTime;
-    char   		mTmpString[80];
-    time_t 		mSysTime;
-    struct 		timeval   tv;
-    struct 		timezone  tz;
-    struct tm* 		tmval;
-    struct tm		local_tm;
-#if defined (PEGASUS_PLATFORM_SOLARIS_SPARC_CC) || \
-    defined(PEGASUS_OS_LINUX)
-    time_t		utc_offset;
-#endif
+    char dateTimeBuffer[26];
+    time_t systemTime;
+    struct timeval tv;
+    struct timezone tz;
+    struct tm* tmval;
+    struct tm tmvalBuffer;
+    int tzMinutesEast;
+
     // Get the system date and time
-    mSysTime = time(NULL);
+    systemTime = time(NULL);
 
     // Get the localtime
-#if defined PEGASUS_PLATFORM_SOLARIS_SPARC_CC
-    tmval = localtime_r(&mSysTime, &local_tm);
+    tmval = localtime_r(&systemTime, &tmvalBuffer);
     PEGASUS_ASSERT(tmval != 0);
-    gettimeofday(&tv,NULL);
-    utc_offset = (tmval->tm_isdst > 0 && daylight) ? altzone : timezone ;
-    utc_offset /= 60;	// CIM only uses minutes, not seconds.
-#elif defined(PEGASUS_OS_LINUX)
-    tmval = localtime_r(&mSysTime, &local_tm);
-    PEGASUS_ASSERT(tmval != 0);
-    gettimeofday(&tv,NULL);
-    utc_offset = local_tm.tm_gmtoff/60;
-#else
-    tmval = localtime_r(&mSysTime, &local_tm);
-    PEGASUS_ASSERT(tmval != 0);
-    gettimeofday(&tv,&tz);
-#endif
 
-    // Initialize the year 
-    year = 1900;
+    // Get the microseconds value and the UTC offset
+#if defined(PEGASUS_PLATFORM_SOLARIS_SPARC_CC)
+    gettimeofday(&tv, NULL);
+    tzMinutesEast =
+        - (int)((tmval->tm_isdst > 0 && daylight) ? altzone : timezone) / 60;
+#elif defined(PEGASUS_OS_HPUX)
+    gettimeofday(&tv, NULL);
+    tzMinutesEast = - (int) timezone / 60;
+    if ((tmval->tm_isdst > 0) && daylight)
+    {
+        // ATTN: It is unclear how to determine the DST offset.  Assume 1 hour.
+        tzMinutesEast += 60;
+    }
+#elif defined(PEGASUS_OS_LINUX)
+    gettimeofday(&tv, NULL);
+    tzMinutesEast = (int) tmval->tm_gmtoff/60;
+#else
+    gettimeofday(&tv, &tz);
+    tzMinutesEast = -tz.tz_minuteswest;
+#endif
 
     // Format the date
-    sprintf((char *)&dateTime,"%04d%02d%02d%02d%02d%02d.%06ld+%03d",
-                    year + tmval->tm_year,
-                    tmval->tm_mon + 1,
-                    tmval->tm_mday,
-                    tmval->tm_hour,
-                    tmval->tm_min,
-                    tmval->tm_sec,
-                    tv.tv_usec,
-#if defined(PEGASUS_PLATFORM_SOLARIS_SPARC_CC) || \
-    defined(PEGASUS_OS_LINUX)
-		    abs((int)utc_offset));
+    sprintf(
+        dateTimeBuffer,
+        "%04d%02d%02d%02d%02d%02d.%06ld%+04d",
+        1900 + tmval->tm_year,
+        tmval->tm_mon + 1,
+        tmval->tm_mday,
+        tmval->tm_hour,
+        tmval->tm_min,
+        tmval->tm_sec,
+        tv.tv_usec,
+        tzMinutesEast);
 
-    // Set UTC sign
-    if(utc_offset > 0)
-    {
-	dateTime.plusOrMinus = '-';
-    }
-#else
-                    abs(tz.tz_minuteswest)); // take care of the sign later on
-
-    // Set the UTC Sign
-    if (tz.tz_minuteswest > 0)
-    {
-        dateTime.plusOrMinus = '-';
-    }
-    else
-    {
-        dateTime.plusOrMinus = '+';
-    }
-#endif
-    currentDateTime.clear();
-    strcpy(mTmpString, (char *)&dateTime);
-    currentDateTime.set(mTmpString);
-
-    return currentDateTime;
+    return CIMDateTime(dateTimeBuffer);
 }
 
 PEGASUS_NAMESPACE_END
-
-
-

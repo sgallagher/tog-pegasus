3f7d4c3be876a92caa5b3f22dcbdba4f31450e8e
david.dillard <david.dillard>
2005-03-11 22:01:51 +0000

    BUG#: 3000 TITLE: Fixed constant truncation warning.
    DESCRIPTION: The code had used both 255 or 0xFF, which are both integers, and therefore can be truncated when converted to signed characters.  I changed the code to use '\xFF' which is a signed character value already, no conversion necessary.

--- src/Pegasus/Common/tests/Base64/Base64.cpp
+++ src/Pegasus/Common/tests/Base64/Base64.cpp
@@ -15,7 +15,7 @@
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
@@ -72,7 +72,7 @@ int main(int argc, char** argv)
             if (out[i] != 0)
             {
                 if(verbose)
-					cout <<"Decode error" << endl;   
+                    cout <<"Decode error" << endl;
             }
     }
     // Test a simple decode to determine if decode correct
@@ -94,7 +94,7 @@ int main(int argc, char** argv)
             if (out[i] != 0xFF) //char 255
             {
                 if(verbose)
-                 cout << "Decode error " << static_cast<int>(out[i]) << endl;   
+                 cout << "Decode error " << static_cast<int>(out[i]) << endl;
             }
     }
 
@@ -118,33 +118,33 @@ int main(int argc, char** argv)
         assert(out.size() == 4);
         for (Uint32 i = 0; i <out.size(); i++ )
         {
-            assert(out[i] == 'A');    
+            assert(out[i] == 'A');
         }
     }
 
-	if(verbose)
-		cout << "Code and decode tests. Test 1 - all characters in buffer" 
-			 << endl;
+    if(verbose)
+        cout << "Code and decode tests. Test 1 - all characters in buffer"
+                << endl;
     {
         // Repeat test for buffer size from 0 to 1000 characters long
         for (Uint32 i = 0; i < maxLength; i++)
         {
             //build an array i characters long with integers 0 - 63
-                                                    
+
             Array<char> in;
             Array<char> out;
-        
+
             Uint8 k = 0;
-        
+
             // Build the array from assending set of characters
-            in.append(k++);   
+            in.append(k++);
             Array<char> temp = Base64::encode(in);
             out = Base64::decode(temp);
-    
+
             assert(in == out);
-    
+
             /* Detailed error testing if we need to turn it on
-            
+
             if (in.size() != out.size())
             {
                 cout << "size error" << endl;
@@ -153,21 +153,21 @@ int main(int argc, char** argv)
                 if (in[l] != out[l])
                     cout << "Error in something" << endl;
             if (!(in == out))
-               cout << "Problem with Base64 Equal test" << endl; 
+               cout << "Problem with Base64 Equal test" << endl;
             */
         }
     }
 
     // Test with buffer of all zeros
-	if(verbose)
-		cout << "Test 2 -Test all zero buffer" << endl;
+    if(verbose)
+        cout << "Test 2 -Test all zero buffer" << endl;
     {
         Array<char> in;
         Array<char> out;
-        
+
         for (Uint32 i = 0; i < maxLength; i++)
         {
-            in.append(0);   
+            in.append(0);
             Array<char> temp = Base64::encode(in);
             out = Base64::decode(temp);
             assert(in == out);
@@ -176,15 +176,15 @@ int main(int argc, char** argv)
 
 
     // Test with buffer of char 0xFF (255)
-	if(verbose)
-		cout << "Test 3 -- Test all 1s in buffer" << endl;
+    if(verbose)
+        cout << "Test 3 -- Test all 1s in buffer" << endl;
     {
         Array<char> in;
         Array<char> out;
-        
+
         for (Uint32 i = 0; i < maxLength; i++)
         {
-            in.append(0xFF); //char 255   
+            in.append('\xFF');
             Array<char> temp = Base64::encode(in);
             out = Base64::decode(temp);
 
@@ -196,10 +196,10 @@ int main(int argc, char** argv)
     {
         Array<char> in;
         Array<char> out;
-        
+
         for (Uint32 i = 0; i < maxLength; i++)
         {
-            in.append(0xFF);   //char 255
+            in.append('\xFF');
             out = Base64::decode(Base64::encode(in));
 
             assert(in == out);

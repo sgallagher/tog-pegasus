616df6c8339dfc1417c5af1247af48828bf77e57
chip <chip>
2005-03-24 04:44:13 +0000

    BUG#: 3045 TITLE: CIM object class names can be changed after initialization.
    DESCRIPTION: Fixed CIMObject::setPath() to throw TypeMismatchException if
    the class name in an object name is different than the current class name, which
    can be changed during copy or assignment. Minor cleanup.

--- src/Pegasus/Common/CIMObjectRep.cpp
+++ src/Pegasus/Common/CIMObjectRep.cpp
@@ -15,7 +15,7 @@
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
@@ -29,45 +29,66 @@
 //
 // Author: Mike Brasher (mbrasher@bmc.com)
 //
-// Modified By: Carol Ann Krug Graves, Hewlett-Packard Company
-//                  (carolann_graves@hp.com)
+// Modified By:
+//      Carol Ann Krug Graves, Hewlett-Packard Company (carolann_graves@hp.com)
+//      Chip Vincent (cvincent@us.ibm.com)
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
 #include "CIMObjectRep.h"
-#include "CIMName.h"
-#include <Pegasus/Common/MessageLoader.h> //l10n
 
-PEGASUS_USING_STD;
+#include <Pegasus/Common/CIMName.h>
+#include <Pegasus/Common/MessageLoader.h> //l10n
 
 PEGASUS_NAMESPACE_BEGIN
 
+PEGASUS_USING_STD;
+
+CIMObjectRep::CIMObjectRep()
+{
+}
+
+CIMObjectRep::CIMObjectRep(const CIMObjectRep& x)
+    : _reference(x._reference),
+    _resolved(x._resolved)
+{
+    x._qualifiers.cloneTo(_qualifiers);
+
+    _properties.reserveCapacity(x._properties.size());
+
+    for (Uint32 i = 0, n = x._properties.size(); i < n; i++)
+    {
+        _properties.append(x._properties[i].clone());
+    }
+}
+
 CIMObjectRep::CIMObjectRep(const CIMObjectPath& reference)
-    : _reference(reference), _resolved(false)
+    : _reference(reference),
+    _resolved(false)
 {
 }
 
 CIMObjectRep::~CIMObjectRep()
 {
-
 }
 
 void CIMObjectRep::addProperty(const CIMProperty& x)
 {
     if (x.isUninitialized())
-	throw UninitializedObjectException();
+    {
+        throw UninitializedObjectException();
+    }
 
     // Reject duplicate property names:
 
-    if (findProperty(x.getName()) != PEG_NOT_FOUND){
-    	//l10n
-        //throw AlreadyExistsException
-            //("property \"" + x.getName().getString () + "\"");
-        MessageLoaderParms parms("Common.CIMObjectRep.PROPERTY",
-        						 "property \"$0\"",
-        						 x.getName().getString());
+    if (findProperty(x.getName()) != PEG_NOT_FOUND)
+    {
+        MessageLoaderParms parms(
+            "Common.CIMObjectRep.PROPERTY",
+            "property \"$0\"",
+            x.getName().getString());
+
         throw AlreadyExistsException(parms);
-            
     }
 
     // Append property:
@@ -79,86 +100,90 @@ Uint32 CIMObjectRep::findProperty(const CIMName& name) const
 {
     for (Uint32 i = 0, n = _properties.size(); i < n; i++)
     {
-	if (name.equal(_properties[i].getName()))
-	    return i;
+        if (name.equal(_properties[i].getName()))
+        {
+            return(i);
+        }
     }
 
-    return PEG_NOT_FOUND;
+    return(PEG_NOT_FOUND);
 }
 
 CIMProperty CIMObjectRep::getProperty(Uint32 index)
 {
     if (index >= _properties.size())
-	throw IndexOutOfBoundsException();
+    {
+        throw IndexOutOfBoundsException();
+    }
 
-    return _properties[index];
+    return(_properties[index]);
 }
 
 void CIMObjectRep::removeProperty(Uint32 index)
-    {
+{
 	if (index >= _properties.size())
-	    throw IndexOutOfBoundsException();
-
-	_properties.remove(index);
+    {
+        throw IndexOutOfBoundsException();
     }
+
+    _properties.remove(index);
+}
 
 
 Uint32 CIMObjectRep::getPropertyCount() const
 {
-    return _properties.size();
+    return(_properties.size());
 }
 
-
-CIMObjectRep::CIMObjectRep()
-{
-
-}
-
-CIMObjectRep::CIMObjectRep(const CIMObjectRep& x) : 
-    Sharable(),
-    _reference(x._reference),
-    _resolved(x._resolved)
-{
-    x._qualifiers.cloneTo(_qualifiers);
-
-    _properties.reserveCapacity(x._properties.size());
-
-    for (Uint32 i = 0, n = x._properties.size(); i < n; i++)
-	_properties.append(x._properties[i].clone());
-}
 
 Boolean CIMObjectRep::identical(const CIMObjectRep* x) const
 {
     if (!_reference.identical(x->_reference))
-	return false;
+    {
+        return(false);
+    }
 
     if (!_qualifiers.identical(x->_qualifiers))
-	return false;
+    {
+        return(false);
+    }
 
     // Compare properties:
 
     {
-	const Array<CIMProperty>& tmp1 = _properties;
-	const Array<CIMProperty>& tmp2 = x->_properties;
+        const Array<CIMProperty>& tmp1 = _properties;
+        const Array<CIMProperty>& tmp2 = x->_properties;
 
-	if (tmp1.size() != tmp2.size())
-	    return false;
+        if (tmp1.size() != tmp2.size())
+        {
+            return(false);
+        }
 
-	for (Uint32 i = 0, n = tmp1.size(); i < n; i++)
-	{
-	    if (!tmp1[i].identical(tmp2[i]))
-		return false;
-	}
+        for (Uint32 i = 0, n = tmp1.size(); i < n; i++)
+        {
+            if (!tmp1[i].identical(tmp2[i]))
+            {
+                return(false);
+            }
+        }
     }
 
     if (_resolved != x->_resolved)
-	return false;
+    {
+        return(false);
+    }
 
-    return true;
+    return(true);
 }
 
 void CIMObjectRep::setPath (const CIMObjectPath& path)
 {
+    // prevent changing the class name (type) in when updating the object path
+    if(!_reference.getClassName().equal(path.getClassName()))
+    {
+        throw TypeMismatchException();
+    }
+
     _reference = path;
 }
 

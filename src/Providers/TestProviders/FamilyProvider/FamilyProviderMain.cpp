f8c0f618fcbef751c6e7e039e6f661ce2aea55ec
jim.wunderlich <jim.wunderlich>
2005-04-25 17:18:50 +0000

    BUG#: 1343
    TITLE: Unsupported interfaces used in SampleFamilyProvider - move to TestProviders
    
    DESCRIPTION: Moved to TestProviders - renamed to FamilyProvider

---
+++ src/Providers/TestProviders/FamilyProvider/FamilyProviderMain.cpp
@@ -0,0 +1,67 @@
+//%2005////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Karl Schopmeyer (k.schopmeyer@opengroup.org)
+//
+// Modified By:
+//
+//%/////////////////////////////////////////////////////////////////////////////
+// This is a sample provider that provides the instance and association operations
+// associated with the TST_Family MOF for the dynamic versions of the classes.
+// It provides the TST_PersonDynamic instances and the
+// TST_LineageDynamic and TST_LabeledLineageDynamic Associations
+// When started it builds the classes into an internal table.
+// ISSUE: How do I create a multiple provider (instance and association)
+
+
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/String.h>
+
+#include "FamilyProvider.h"
+
+PEGASUS_USING_PEGASUS;
+
+// This is the dynamic entry point into this dynamic module. The name of
+// this provider is "FamilyProvider" which is appened to
+// "PegasusCreateProvider_" to form a symbol name. This function is called
+// by the ProviderModule to load this provider.
+//
+// NOTE: The name of the provider must be correct to be loadable.
+
+extern "C" PEGASUS_EXPORT CIMProvider * PegasusCreateProvider(const String & name)
+{
+	if(String::equalNoCase(name, "FamilyProvider") ||
+		String::equalNoCase(name, "FamilyProvider (PROVIDER)"))
+	{
+        return(new FamilyProvider());
+	}
+
+	return(0);
+}
+

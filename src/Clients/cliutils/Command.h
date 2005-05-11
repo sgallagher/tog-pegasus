21a64126c9c58d3e87619c61b31b4da1789a93c5
david.dillard <david.dillard>
2005-05-11 14:24:11 +0000

    BUG#: 2182 TITLE: Removed non-empty exception specification.
    DESCRIPTION:

--- src/Clients/cliutils/Command.h
+++ src/Clients/cliutils/Command.h
@@ -15,7 +15,7 @@
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
@@ -27,10 +27,11 @@
 //
 //==============================================================================
 //
-// Author: Carol Ann Krug Graves, Hewlett-Packard Company 
-//         (carolann_graves@hp.com)
+// Author: Carol Ann Krug Graves, Hewlett-Packard Company
+//              (carolann_graves@hp.com)
 //
-// Modified By:
+// Modified By: David Dillard, VERITAS Software Corp.
+//                  (david.dillard@veritas.com)
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -47,56 +48,55 @@
 PEGASUS_NAMESPACE_BEGIN
 
 /**
-  
+
     Command is the abstract base class for WBEM and CIM commands.
-  
+
     @author  Hewlett-Packard Company
-  
+
  */
-class PEGASUS_CLIUTILS_LINKAGE Command 
+class PEGASUS_CLIUTILS_LINKAGE Command
 {
 public:
 
     virtual ~Command();
 
     /**
-      
+
         Gets command usage string.  Usage string may be retrieved and displayed
         when a CommandFormatException is caught.
-      
+
         @return  String containing the command usage message
-      
+
      */
     String& getUsage ();
 
     /**
-      
+
         Parses the command line, validates the options, and sets instance
         variables based on the options and arguments.
-      
+
         @param   argc  the number of command line arguments
         @param   argv  the string vector of command line arguments
-      
+
         @exception     CommandFormatException    Indicates the command line was
                                                  improperly formed
-      
+
      */
-    virtual void setCommand (Uint32 argc, char *argv []) 
-        throw (CommandFormatException) = 0;
+    virtual void setCommand (Uint32 argc, char *argv []) = 0;
 
     /**
-      
+
         Executes the command and writes the results to the output streams.
-      
+
         @param  outPrintWriter  the ostream to which output should be
                                 written
-      
+
         @param  errPrintWriter  the ostream to which error output
                                 should be written
-      
+
         @return  0              if the command is successful
                  1              if an error occurs in executing the command
-      
+
      */
     virtual Uint32 execute (PEGASUS_STD(ostream)& outPrintWriter,
                             PEGASUS_STD(ostream)& errPrintWriter) = 0;
@@ -105,26 +105,26 @@ public:
     // Return codes
     //
     /**
-      
+
         Return value from the execute method indicating the command executed
         successfully.
-      
+
     */
     static const Uint32    RC_SUCCESS;
 
     /**
-      
+
         Return value from the execute method indicating an error occurred in
         executing the command.
-      
+
     */
     static const Uint32    RC_ERROR;
-    
+
     //l10n
-	String localizeMessage(	const char *resource_path, 
-						   	const char * key, 
+	String localizeMessage(	const char *resource_path,
+						   	const char * key,
 						   	const char * default_msg,
-							Formatter::Arg arg0 = Formatter::Arg(), 
+							Formatter::Arg arg0 = Formatter::Arg(),
 							Formatter::Arg arg1 = Formatter::Arg(),
 							Formatter::Arg arg2 = Formatter::Arg(),
 							Formatter::Arg arg3 = Formatter::Arg(),
@@ -137,23 +137,23 @@ public:
 
 protected:
     /**
-      
+
         Sets command usage string.  Usage string should be set by Command
         subclass constructor.
-      
+
         @param    usageMessage    the command usage message
-      
+
      */
     void setUsage (String& usageMessage);
 
 private:
 
     /**
-      
+
         The usage string for this command.  The value of the string should be
-        set in the command subclass constructor.  The string may be retrieved 
+        set in the command subclass constructor.  The string may be retrieved
         and displayed when a CommandFormatException is caught.
-      
+
      */
     String _usage;
 };

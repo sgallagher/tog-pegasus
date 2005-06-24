d2ac3523b41d72998e9e26196ed488f90406e61d
konrad.r <konrad.r>
2005-06-24 19:57:38 +0000

    BUG#: 3717 TITLE: CMPIProvider::initialize does not check return codes or the return function table to make sure it is initialized
    DESCRIPTION:

---
+++ src/Providers/TestProviders/CMPI/TestFail_1/tests/TestCMPI_Fail_1.cpp
@@ -0,0 +1,135 @@
+//%2005////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+// Author: Konrad Rzeszutek <konradr@us.ibm.com>
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/Constants.h>
+#include <Pegasus/Common/CIMType.h>
+#include <Pegasus/Common/CIMName.h>
+#include <Pegasus/Common/CIMInstance.h>
+#include <Pegasus/Common/CIMProperty.h>
+#include <Pegasus/Common/CIMValue.h>
+#include <Pegasus/Common/System.h>
+#include <Pegasus/Common/InternalException.h>
+#include <Pegasus/Client/CIMClient.h>
+
+PEGASUS_USING_PEGASUS;
+PEGASUS_USING_STD;
+
+
+const CIMNamespaceName PROVIDERNAMESPACE =
+CIMNamespaceName ("test/TestProvider");
+const CIMName CLASSNAME = CIMName ("TestCMPI_Fail_1");
+const String ERROR = "CIM_ERR_FAILED: A general error occurred that is not covered by a more specific error code: \"ProviderInitFailure: Error initializing TestCMPIFail_1Provider the following API(s): TestCMPIFail_1Provider_Create_InstanceMI, TestCMPIFail_1Provider_Create_AssociationMI, TestCMPIFail_1Provider_Create_MethodMI, TestCMPIFail_1Provider_Create_PropertyMI, TestCMPIFail_1Provider_Create_IndicationMI\"";
+
+Boolean verbose;
+
+void
+_usage ()
+{
+  cerr << "Usage: TestCMPIMethod " << "{test}" << endl;
+}
+
+void
+test01 (CIMClient & client)
+{
+
+  try { 
+  	Array<CIMObjectPath> array = client.enumerateInstanceNames (PROVIDERNAMESPACE,
+					   CLASSNAME);
+  } catch (const CIMException &e) 
+  {
+	if (e.getMessage()!=ERROR)
+	{
+		throw e;
+	}
+  }
+}
+
+
+void
+_test (CIMClient & client)
+{
+  try
+  {
+
+    test01 (client);
+  }
+  catch (Exception & e)
+  {
+    cerr << "test failed: " << e.getMessage () << endl;
+    exit (-1);
+  }
+
+  cout << "+++++ test completed successfully" << endl;
+}
+
+
+int
+main (int argc, char **argv)
+{
+  verbose = (getenv ("PEGASUS_TEST_VERBOSE")) ? true : false;
+  CIMClient client;
+  try
+  {
+    client.connectLocal ();
+  }
+  catch (Exception & e)
+  {
+    cerr << e.getMessage () << endl;
+    return -1;
+  }
+
+  if (argc != 2)
+    {
+      _usage ();
+      return 1;
+    }
+
+  else
+    {
+      const char *opt = argv[1];
+
+      if (String::equalNoCase (opt, "test"))
+	{
+	  _test (client);
+	}
+      else
+	{
+	  cerr << "Invalid option: " << opt << endl;
+	  _usage ();
+	  return -1;
+	}
+    }
+
+  return 0;
+}

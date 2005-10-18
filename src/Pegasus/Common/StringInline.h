9afd57b3ef597a30f832d15e410b39fc8b9089bd
mike <mike>
2005-10-18 19:39:43 +0000

    BUG#: 4265 TITLE: HP compiler error
    DESCRIPTION: Fixed compiler error on HP with ICU enabled.

--- src/Pegasus/Common/StringInline.h
+++ src/Pegasus/Common/StringInline.h
@@ -146,14 +146,7 @@ PEGASUS_STRING_INLINE String& String::append(const Char16& c)
 PEGASUS_STRING_INLINE Boolean String::equalNoCase(
     const String& s1, const String& s2)
 {
-#ifdef PEGASUS_HAS_ICU
-    return equalNoCase_aux(s1, s2);
-#else
-    if (s1._rep->size == s2._rep->size)
-        return StringEqualNoCase(s1, s2);
-
-    return false;
-#endif
+    return s1._rep->size == s2._rep->size && StringEqualNoCase(s1, s2);
 }
 
 #ifdef PEGASUS_USE_EXPERIMENTAL_INTERFACES

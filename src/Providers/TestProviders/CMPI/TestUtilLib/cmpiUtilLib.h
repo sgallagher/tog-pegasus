c98ec2c3f442d8459177faf1509b8aea97d01e75
a.dunfey <a.dunfey>
2005-10-21 21:17:42 +0000

    BUG#: 4224 TITLE: Fix on Windows for new CMPI test cases
    DESCRIPTION:
    
    Submitting changes proposed by IBM and approved by EMC and HP.

--- src/Providers/TestProviders/CMPI/TestUtilLib/cmpiUtilLib.h
+++ src/Providers/TestProviders/CMPI/TestUtilLib/cmpiUtilLib.h
@@ -2,42 +2,42 @@
 #include <Pegasus/Provider/CMPI/cmpift.h>
 
 
-void
+CMPI_EXTERN_C void
 PROV_LOG (const char *fmt, ...);
 
-void
+CMPI_EXTERN_C void
 PROV_LOG_CLOSE ();
 
-void
+CMPI_EXTERN_C void
 PROV_LOG_OPEN (const char *file, const char *location);
 	
-const char *
+CMPI_EXTERN_C const char *
 strCMPIValueState (CMPIValueState state);
 
-const char *
+CMPI_EXTERN_C const char *
 strCMPIPredOp (CMPIPredOp op);
 	
 
-const char *
+CMPI_EXTERN_C const char *
 strCMPIStatus (CMPIStatus stat);
 
-int _CMSameType( CMPIData value1, CMPIData value2 );
-int _CMSameValue( CMPIData value1, CMPIData value2 );
-int _CMSameObject( const CMPIObjectPath * object1, const CMPIObjectPath * object2 );
-int _CMSameInstance( const CMPIInstance * instance1, const CMPIInstance * instance2 );
-const char * _CMPIrcName ( CMPIrc rc );
-const char * _CMPITypeName (CMPIType type);
-char * _CMPIValueToString (CMPIData data);
+CMPI_EXTERN_C int _CMSameType( CMPIData value1, CMPIData value2 );
+CMPI_EXTERN_C int _CMSameValue( CMPIData value1, CMPIData value2 );
+CMPI_EXTERN_C int _CMSameObject( const CMPIObjectPath * object1, const CMPIObjectPath * object2 );
+CMPI_EXTERN_C int _CMSameInstance( const CMPIInstance * instance1, const CMPIInstance * instance2 );
+CMPI_EXTERN_C const char * _CMPIrcName ( CMPIrc rc );
+CMPI_EXTERN_C const char * _CMPITypeName (CMPIType type);
+CMPI_EXTERN_C char * _CMPIValueToString (CMPIData data);
 
-void check_CMPIStatus (CMPIStatus rc);
+CMPI_EXTERN_C void check_CMPIStatus (CMPIStatus rc);
 //int _setProperty (const CMPIBroker *b,CMPIInstance * ci, const char *c,const char *p);
 //
-CMPIBoolean
+CMPI_EXTERN_C CMPIBoolean
 evalute_selectcond (const CMPISelectCond * cond, CMPIAccessor *accessor, void *parm);
 
-CMPIBoolean
+CMPI_EXTERN_C CMPIBoolean
 evaluate(const CMPISelectExp *se, const CMPIInstance *inst,CMPIAccessor *inst_accessor, void *parm );
 
-CMPIData
+CMPI_EXTERN_C CMPIData
 instance_accessor (const char *name, void *param);
 

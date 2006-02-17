2292e09cd1f6ebfc583cfd762b976480f8c9a95a
kumpf <kumpf>
2006-02-17 19:30:37 +0000

    BUG#: 4796 TITLE: Complete and correct the documentation for the CIM interfaces DESCRIPTION: Updated the doc++ to more completely and correctly document the interfaces.

--- src/Pegasus/Common/CIMParamValue.h
+++ src/Pegasus/Common/CIMParamValue.h
@@ -49,54 +49,134 @@ class CIMParamValueRep;
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-///
+/**
+    The CIMParamValue class represents an extrinsic method parameter value,
+    as defined in the DMTF Specification for CIM Operations over HTTP.
+
+    <p>The CIMParamValue class uses a shared representation model, such that
+    multiple CIMParamValue objects may refer to the same data copy.  Assignment
+    and copy operators create new references to the same data, not distinct
+    copies.  An update to a CIMParamValue object affects all the CIMParamValue
+    objects that refer to the same data copy.  The data remains valid until
+    all the CIMParamValue objects that refer to it are destructed.  A separate
+    copy of the data may be created using the clone method.
+*/
 class PEGASUS_COMMON_LINKAGE CIMParamValue
 {
 public:
 
-    ///
+    /**
+        Constructs an uninitialized CIMParamValue object.  A method
+        invocation on an uninitialized object will result in the throwing
+        of an UninitializedObjectException.  An uninitialized object may
+        be converted into an initialized object only by using the assignment
+        operator with an initialized object.
+    */
     CIMParamValue();
 
-    ///
+    /**
+        Constructs a CIMParamValue object from the value of a specified
+        CIMParamValue object, so that both objects refer to the same data
+        copy.
+        @param x The CIMParamValue object from which to construct a new
+            CIMParamValue object.
+    */
     CIMParamValue(const CIMParamValue& x);
 
-    ///
+    /**
+        Assigns the value of the specified CIMParamValue object to this
+        object, so that both objects refer to the same data copy.
+        @param x The CIMParamValue object from which to assign this
+            CIMParamValue object.
+        @return A reference to this CIMParamValue object.
+    */
     CIMParamValue& operator=(const CIMParamValue& x);
 
-    ///
+    /**
+        Constructs a parameter value with the specified attributes.
+        @param parameterName A String containing the name of this parameter.
+        @param value A CIMValue containing the value of this parameter.
+        @param isTyped A Boolean indicating whether the type indicated in
+            the value is correct.  This is needed because the Specification
+            for the Representation of CIM in XML does not require type
+            information to be specified with a parameter value.
+        @exception UninitializedObjectException If the parameter name is an
+            empty String.
+    */
     CIMParamValue(
 	String parameterName,
 	CIMValue value,
 	Boolean isTyped=true);
 
-    ///
+    /**
+        Destructs the CIMParamValue object.
+    */
     ~CIMParamValue();
 
-    ///
+    /**
+        Gets the parameter name for the parameter value.
+        @return A String containing the parameter name.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     String getParameterName() const;
 
-    ///
+    /**
+        Gets the value for the parameter.
+        @return A CIMValue containing the parameter value.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     CIMValue getValue() const;
 
-    ///
+    /**
+        Checks whether the parameter value has the correct type.
+        @return True if the parameter value is known to have the correct
+            type, false otherwise.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     Boolean isTyped() const;
 
-    ///
+    /**
+        Sets the parameter name for the parameter value.
+        @param parameterName A String containing the parameter name.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     void setParameterName(String& parameterName);
 
-    ///
+    /**
+        Sets the value for the parameter.
+        @param value A CIMValue containing the parameter value.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     void setValue(CIMValue& value);
 
-    ///
+    /**
+        Sets a flag indicating whether the parameter value has the correct
+        type.
+        @param isTyped A Boolean indicating whether the parameter value is
+            known to have the correct type.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     void setIsTyped(Boolean isTyped);
 
-    ///
+    /**
+        Makes a deep copy of the object.  This creates a new copy of all
+        the object attributes.
+        @return A new copy of the CIMParamValue object.
+        @exception UninitializedObjectException If the object is not
+            initialized.
+    */
     CIMParamValue clone() const;
 
-    /** Determines if the object has not been initialized.
-        @return  true if the object has not been initialized,
-                 false otherwise.
-     */
+    /**
+        Determines whether the object has been initialized.
+        @return True if the object has not been initialized, false otherwise.
+    */
     Boolean isUninitialized() const;
 
 private:
--- src/Pegasus/Common/CIMScope.h
+++ src/Pegasus/Common/CIMScope.h
@@ -41,131 +41,132 @@
 PEGASUS_NAMESPACE_BEGIN
 
 /**
-    The CIMScope class implements the concept of the scope of a CIM qualifier 
-    object.  The scope of a qualifier defines the set of types of CIM objects 
-    with which the qualifier may be used.  The possible values are:
-    NONE, CLASS, ASSOCIATION, INDICATION, PROPERTY, REFERENCE, METHOD, 
-    PARAMETER, ANY.
-    The scope is a set of one or more of these possible values.
-    The value "NONE" implies a CIMScope object that has not yet been assigned a
-    value (uninitialized).  It is not a valid value for the scope of a 
-    qualifier object.
-    The value "ANY" means that the qualifier may be used with any of the CIM 
-    object types, and is equivalent to listing each of the object types in the 
-    scope.
+    The CIMScope class represents the DMTF standard CIM qualifier scope
+    definition.  The scope of a qualifier defines the types of CIM objects
+    with which the qualifier may be used.  A CIMScope contains zero or more
+    of these values:  CLASS, ASSOCIATION, INDICATION, PROPERTY, REFERENCE,
+    METHOD, PARAMETER.
 */
 class PEGASUS_COMMON_LINKAGE CIMScope
 {
 public:
 
-    /** Constructs a CIMScope object with no value set (default constructor).
-        The values are null.
+    /**
+        Constructs a CIMScope object with the value NONE.
     */
-    CIMScope ();
+    CIMScope();
 
-    /** Constructs a CIMScope object from an existing CIMScope object (copy 
-        constructor).
-        @param   scope   Specifies an instance of CIMScope object.
+    /**
+        Constructs a CIMScope object from the value of a specified
+        CIMScope object.
+        @param scope The CIMScope object from which to construct a new
+            CIMScope object.
     */
-    CIMScope (const CIMScope & scope);
+    CIMScope(const CIMScope& scope);
 
-    /** Assigns the value of one CIMScope object to another (assignment 
-        operator).
-        @param   scope Specifies the name of a CIMScope object that contains
-        the values that you want to assign to another CIMScope object.
-        @return  The CIMScope object with the values of the specified CIMScope
-        object. For example:
+    /**
+        Assigns the value of the specified CIMScope object to this object.
+        @param scope The CIMScope object from which to assign this
+            CIMScope object.
+        @return A reference to this CIMScope object.
+    */
+    CIMScope& operator=(const CIMScope& scope);
+
+    /**
+        Checks whether the scope contains specified scope values.
+        @param scope A CIMScope specifying the scope values to check.
+        @return True if the scope contains all the values in the specified
+            CIMScope object, false otherwise.
+    */
+    Boolean hasScope(const CIMScope& scope) const;
+
+    /**
+        Adds scopes value to the CIMScope object.
+        <p><b>Example:</b>
         <pre>
-            CIMScope s0;
-            CIMScope s1(s0);
+            CIMScope s;
+            s.addScope(CIMScope::INDICATION);
+            assert(s.hasScope(CIMScope::INDICATION));
         </pre>
-        The values in CIMScope s0 are assigned to the CIMScope s1 instance.
+        @param scope A CIMScope containing the scope values to add.
     */
-    CIMScope & operator= (const CIMScope & scope);
+    void addScope(const CIMScope & scope);
 
-    /** Determines if every value in the specified CIMScope object is included 
-        in this CIMScope object.
-        @param   scope - Specifies a name of a CIMScope object.
-        @return  True if every value in the specified CIMScope object is 
-                 included in the CIMScope object; otherwise, false. 
-    */
-    Boolean hasScope (const CIMScope & scope) const;
-
-    /** Adds the specified scope value to the CIMScope object.
-        @param   scope - Specifies a scope value. For example:
-        <pre>
-            CIMScope s0;
-            s0.addScope (CIMScope::INDICATION);
-            if(s0.hasScope (CIMScope::INDICATION))
-                ...				..
-        </pre>
-    */
-    void addScope (const CIMScope & scope);
-
-    /** Compares two CIMScope objects.
-        @param  scope - Specifies a CIMScope object.
-        @return True if the two CIMScope objects are equal; otherwise, false.
-        For example,
-        <pre>
-            CIMScope s0;
-            s0.addScope (CIMScope::CLASS + CIMScope::PARAMETER);
-            if(s0.hasScope (CIMScope::CLASS))
-                ...	..
-    </pre>
+    /**
+        Compares the CIMScope with a specified CIMScope.
+        @param scope The CIMScope to be compared.
+        @return True if this scope has the same set of values as the
+            specified scope, false otherwise.
     */
     Boolean equal (const CIMScope & scope) const;
 
-    /** Combines two CIMScope objects.
-        @param  scope - Specifies a CIMScope object to add.
-        @return A new CIMScope object that represents the combination of this
-                scope object with the specified scope object.
+    /**
+        Adds two scope values.
+        <p><b>Example:</b>
         <pre>
             CIMScope s0(CIMScope::CLASS);
             CIMScope s1(CIMScope::PARAMETER);
             CIMScope s3 = s0 + S1;
         </pre>
+        @param scope A CIMScope containing the scope value to add to this
+            scope.
+        @return A new CIMScope object containing a union of the values in the
+            two scope objects.
     */
-    CIMScope operator+ (const CIMScope & scope) const;
+    CIMScope operator+(const CIMScope & scope) const;
 
-    /** Returns a String representation of the CIMScope object.
+    /**
+        Returns a String representation of the CIMScope object.
         This method is for diagnostic purposes. The format of the output
         is subject to change.
+        @return A String containing a human-readable representation of the
+            scope value.
     */
-    String toString () const;
+    String toString() const;
 
-    /** Indicates that the CIMScope object has no value (is uninitialized).
+    /**
+        Indicates that the CIMScope object has no value (is uninitialized).
+        This is not a valid qualifier scope.
     */
     static const CIMScope NONE;
 
-    /** Indicates that the qualifier may be used with classes.
+    /**
+        Indicates that the qualifier may be used with classes.
     */
     static const CIMScope CLASS;
 
-    /** Indicates that the qualifier may be used with associations.
+    /**
+        Indicates that the qualifier may be used with associations.
     */
     static const CIMScope ASSOCIATION;
 
-    /** Indicates that the qualifier may be used with indications.
+    /**
+        Indicates that the qualifier may be used with indications.
     */
     static const CIMScope INDICATION;
 
-    /** Indicates that the qualifier may be used with properties.
+    /**
+        Indicates that the qualifier may be used with properties.
     */
     static const CIMScope PROPERTY;
 
-    /** Indicates that the qualifier may be used with references.
+    /**
+        Indicates that the qualifier may be used with references.
     */
     static const CIMScope REFERENCE;
 
-    /** Indicates that the qualifier may be used with methods.
+    /**
+        Indicates that the qualifier may be used with methods.
     */
     static const CIMScope METHOD;
 
-    /** Indicates that the qualifier may be used with parameters.
+    /**
+        Indicates that the qualifier may be used with parameters.
     */
     static const CIMScope PARAMETER;
 
-    /** Indicates that the qualifier may be used with any of the types
+    /**
+        Indicates that the qualifier may be used with any of the types
         of objects (classes, associations, indications, properties, references,
         methods, parameters).
     */
@@ -173,11 +174,9 @@ public:
 
 private:
 
-    // 
     CIMScope (const Uint32 scope);
-    // Private member for storing he CIMScope
     Uint32 cimScope;
-    
+
     friend class BinaryStreamer;
 };
 

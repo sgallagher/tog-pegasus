0486223b8853612fcc779be481338acd184b4d49
carson.hovey <carson.hovey>
2006-03-14 18:15:10 +0000

    BUG#: 4882 TITLE: 2.5.1: OpenVMS: Provider libraries should be in SYS$LIBRARY not /opt/wbem/providers/lib
    DESCRIPTION: Apply Bugzilla 4881 from Main branch to 2.5.1

--- src/Pegasus/Config/FixedPropertyTableVms.h
+++ src/Pegasus/Config/FixedPropertyTableVms.h
@@ -1,31 +1,39 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Jenny Yu, Hewlett-Packard Company (jenny.yu@hp.com)
 //
-//////////////////////////////////////////////////////////////////////////
+// Modified By: Sean Keenan, Hewlett-Packard Company (sean.keenan@hp.com)
+//              Nag Boranna, Hewlett-Packard Company (nagaraja.boranna@hp.com)
+//            
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -33,15 +41,11 @@
 #define Pegasus_FixedPropertyTableVms_h
 
 #ifdef PEGASUS_USE_RELEASE_CONFIG_OPTIONS
-#ifndef PEGASUS_FLAVOR
     {"httpPort",            "5988"},
     {"httpsPort",           "5989"},
-#endif
+    {"home",                ""},
     {"daemon",              "false"},
-#ifdef PEGASUS_ENABLE_SLP
     {"slp",                 "false"},
-#endif
-    {"enableAuthentication", "true"},
     {"enableIndicationService", "true"},
     {"sslClientVerificationMode", "disabled"},
     {"httpAuthType",        "Basic"},
@@ -49,25 +53,22 @@
 #endif
 
 #ifdef PEGASUS_USE_RELEASE_DIRS
-    {"traceFilePath",       "/wbem_var/opt/wbem/trace/cimserver.trc"},
+    {"traceFilePath",       "/wbem_var/opt/wbem/cimserver.trc"},
     {"passwordFilePath",    "/wbem_etc/opt/wbem/cimserver.passwd"},
-    {"sslCertificateFilePath", "/wbem_etc/opt/hp/sslshare/server.pem"},
+    {"sslCertificateFilePath", "/wbem_etc/opt/hp/sslshare/cert.pem"},
     {"sslKeyFilePath",      "/wbem_etc/opt/hp/sslshare/file.pem"},
 //
     {"sslTrustStore",       "/wbem_etc/opt/hp/sslshare/cimserver_trust"},
+    {"exportSSLTrustStore", "/wbem_etc/opt/hp/sslshare/indication_trust"},
     {"crlStore",            "/wbem_etc/opt/hp/sslshare/crl"},
 //
+    {"tempLocalAuthDir",    PEGASUS_LOCAL_AUTH_DIR},
     {"logdir",              "/wbem_var/opt/wbem/logs"},
     {"repositoryDir",       "/wbem_var/opt/wbem/repository"},
     {"providerDir",         "/wbem_lib"},
-    {"providerManagerDir",  "/wbem_lib"},
-#else
-    {"traceFilePath",       "trace/cimserver.trc"},
-    {"logdir",              "logs"},
 #endif
 
-#if !defined(PEGASUS_USE_RELEASE_CONFIG_OPTIONS) && \
-    !defined(PEGASUS_USE_RELEASE_DIRS)
+#if !defined(PEGASUS_USE_RELEASE_CONFIG_OPTIONS) && !defined(PEGASUS_USE_RELEASE_DIRS)
     {"bogus", "MyBogusValue"}      // Remove this line if others are added
 #endif
 

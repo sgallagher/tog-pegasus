fb826390a32f27727ae4ded2033bb6c5d2184193
kumpf <kumpf>
2006-03-16 01:33:51 +0000

    BUG#: 4682 TITLE: Update release version to 2.5.1 DESCRIPTION: Update the version definitions for the 2.5.1 release.

--- src/Pegasus/Common/PegasusVersion.h
+++ src/Pegasus/Common/PegasusVersion.h
@@ -1,40 +1,48 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Karl Schopmeyer (k.schopmeyer@opengroup.org)
 //
-//////////////////////////////////////////////////////////////////////////
-/*
+// Modified By: Jenny Yu (jenny_yu@hp.com)
+//              Warren Otsuka (warren.otsuka@hp.com)
+//              Roger Kumpf, Hewlett-Packard Company (roger_kumpf@hp.com)
 //
 //%/////////////////////////////////////////////////////////////////////////////
-*/
 
 #ifndef Pegasus_Version_h
 #define Pegasus_Version_h
 
 #include <Pegasus/Common/Config.h>
+
+PEGASUS_NAMESPACE_BEGIN
 
 #ifdef PEGASUS_OVERRIDE_PRODUCT_ID
 # include <Pegasus/Common/ProductVersion.h>
@@ -48,7 +56,7 @@
 //
 // The sequence would look like:
 //  # define PEGASUS_PRODUCT_VERSION "2.5"
-//  # define PEGASUS_PRODUCT_STATUS  ""       // Blank implies Released
+//  # define PEGASUS_PRODUCT_STATUS  ""       // Blank implies Released 
 //
 //  # define PEGASUS_PRODUCT_VERSION "2.5.1"
 //  # define PEGASUS_PRODUCT_STATUS  "Development"
@@ -57,7 +65,7 @@
 //  # define PEGASUS_PRODUCT_STATUS  "Pre-Release"
 //
 //  # define PEGASUS_PRODUCT_VERSION "2.5.1"
-//  # define PEGASUS_PRODUCT_STATUS  ""       // Blank implies Released
+//  # define PEGASUS_PRODUCT_STATUS  ""       // Blank implies Released 
 //
 //  # define PEGASUS_PRODUCT_VERSION "2.5.2"
 //  # define PEGASUS_PRODUCT_STATUS  "Development"
@@ -66,14 +74,14 @@
 //  # define PEGASUS_PRODUCT_STATUS  "Pre-Release"
 //
 //  # define PEGASUS_PRODUCT_VERSION "2.5.2"
-//  # define PEGASUS_PRODUCT_STATUS  ""       // Blank implies Released
-//
+//  # define PEGASUS_PRODUCT_STATUS  ""       // Blank implies Released 
+// 
 
 
 # define PEGASUS_PRODUCT_NAME    "CIM Server"
 
-# define PEGASUS_PRODUCT_STATUS  "Development"
-# define PEGASUS_PRODUCT_VERSION "2.14.0"
+# define PEGASUS_PRODUCT_VERSION "2.5.1"
+# define PEGASUS_PRODUCT_STATUS  ""
 
 # define PEGASUS_CIMOM_GENERIC_NAME "Pegasus"
 // If the following is non-zero length it becomes SLP description.
@@ -91,8 +99,8 @@
 #  define PLATFORM_COMPONENT_NAME  "Pegasus Component"
 # endif
 
-# ifndef PLATFORM_COMPONENT_VERSION
-#  define PLATFORM_COMPONENT_VERSION PEGASUS_PRODUCT_VERSION
+# ifndef PLATFORM_COMPONENT_VERSION 
+#  define PLATFORM_COMPONENT_VERSION PEGASUS_PRODUCT_VERSION 
 # endif
 
 # ifndef PLATFORM_COMPONENT_STATUS
@@ -103,27 +111,17 @@
 #  define COMPONENT_BUILD_DATE __DATE__
 # endif
 
-#if defined (PEGASUS_PLATFORM_HPUX_IA64_ACC) || \
-    defined (PEGASUS_PLATFORM_HPUX_IA64_GNU)
-#pragma VERSIONID \
-    "@(#)" PEGASUS_PRODUCT_NAME " " PEGASUS_PRODUCT_VERSION " "\
-        PEGASUS_PRODUCT_STATUS "(" PLATFORM_BUILD_DATE ")" "\n@(#)"\
-        PEGASUS_PRODUCT_NAME "-" PLATFORM_COMPONENT_NAME " "\
-        PLATFORM_COMPONENT_VERSION " " PLATFORM_COMPONENT_STATUS "("\
-        COMPONENT_BUILD_DATE ")"
-#else
 static const char *PLATFORM_EMBEDDED_IDENTIFICATION_STRING =
     "@(#)" PEGASUS_PRODUCT_NAME " " PEGASUS_PRODUCT_VERSION " "
         PEGASUS_PRODUCT_STATUS "(" PLATFORM_BUILD_DATE ")" "\n@(#)"
         PEGASUS_PRODUCT_NAME "-" PLATFORM_COMPONENT_NAME " "
         PLATFORM_COMPONENT_VERSION " " PLATFORM_COMPONENT_STATUS "("
         COMPONENT_BUILD_DATE ")";
-#endif
 
 #endif /* PEGASUS_OS_HPUX */
 
-// This macro can be examined by preprocessor directives (i.e., #if) to
-// make compile time branch decisions based on the Pegasus version number.
+// This macro can be examined by preprocessor directives (i.e., #if) to 
+// make compile time branch decisions based on the Pegasus version number. 
 // When read from left to right, the bytes have the following meaning:
 //
 //     Byte 0: major version number
@@ -145,9 +143,11 @@ static const char *PLATFORM_EMBEDDED_IDENTIFICATION_STRING =
 //      ..
 //    #endif
 //
-// CAUTION: always check whether PEGASUS_VERSION_NUMBER is defined when
+// CAUTION: always check whether PEGASUS_VERSION_NUMBER is defined when 
 // integrating with versions prior to 2.5.1.
 //
-#define PEGASUS_VERSION_NUMBER 0x02140000
+#define PEGASUS_VERSION_NUMBER 0x02050100
+
+PEGASUS_NAMESPACE_END
 
 #endif /* Pegasus_Version_h */

0e44359ff323bc02962103469d34d1f147a17493
marek <marek>
2006-06-09 17:22:22 +0000

    BUG#:5073 TITLE: Class Names on Reference, ReferenceNames, Assoc, AssocNames returned lower class
    DESCRIPTION:

--- src/Pegasus/Repository/AssocClassCache.cpp
+++ src/Pegasus/Repository/AssocClassCache.cpp
@@ -1,226 +1,172 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Robert Kieninger (kieningr@de.ibm.com)
 //
-//////////////////////////////////////////////////////////////////////////
+// Modified By:
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
 #include <Pegasus/Common/Config.h>
-#include <Pegasus/Common/ArrayInternal.h>
 #include "AssocClassCache.h"
 
 PEGASUS_USING_STD;
 
 PEGASUS_NAMESPACE_BEGIN
 
-AssocClassCacheManager::AssocClassCacheManager()
-{
-}
+#define ASSOC_CLASS_NAME_INDEX 0
+#define FROM_CLASS_NAME_INDEX 1
+#define FROM_PROPERTY_NAME_INDEX 2
+#define TO_CLASS_NAME_INDEX 3
+#define TO_PROPERTY_NAME_INDEX 4
+#define NUM_FIELDS 5
 
-AssocClassCacheManager::~AssocClassCacheManager()
-{
-    for (Uint32 i = _assocClassCacheList.size(); i > 0; i--)
-    {
-        delete _assocClassCacheList[i-1];
-        _assocClassCacheList.remove(i-1);
-    }
-}
+Array<AssocClassCache*> AssocClassCache::_assocClassCacheList;
 
-/**
-    Retrieves a singleton instance of the class cache for the given namespace.
+
+/** Retrieves a singleton instance of the class cache for the
+  * given namespace.
 */
-AssocClassCache* AssocClassCacheManager::getAssocClassCache(
-    const String& nameSpace)
+AssocClassCache* AssocClassCache::getAssocClassCache(const String& nameSpace)
 {
-    for (Uint32 i = 0; i < _assocClassCacheList.size(); i++)
-    {
-        if (nameSpace == _assocClassCacheList[i]->getNameSpace())
-        {
-            return _assocClassCacheList[i];
-        }
-    }
+   for (Uint16 idx=0; idx<_assocClassCacheList.size(); idx++)
+   {
+      if (nameSpace == _assocClassCacheList[idx]->_nameSpace)
+      {
+         return _assocClassCacheList[idx];
+      }
+   }
 
-    // If we got here, no cache exists for the given namespace so far,
-    // so we will create a new one.
-    AssocClassCache* newCache = new AssocClassCache(nameSpace);
-    _assocClassCacheList.append(newCache);
+   // If we got here, no cache exists for the the given namespace so far,
+   // so we will create a new one.
+   AssocClassCache * newCache = new AssocClassCache(nameSpace);
+   _assocClassCacheList.append(newCache);
 
-    return newCache;
+   return newCache;
 }
 
+void AssocClassCache::cleanupAssocClassCaches()
+{
+    for (Uint16 idx=_assocClassCacheList.size(); idx>0; idx--)
+    {
+        delete(_assocClassCacheList[idx-1]);
+        _assocClassCacheList.remove(idx-1);
+    }
+
+}
 
 /** Retrieve the list of entries for a from class through direct
-    access via the from class name.
+ * access via the from class name.
 */
-Boolean AssocClassCache::getAssocClassEntry(
-    const CIMName& fromClassName,
-    Array<ClassAssociation>& entryList)
+Boolean AssocClassCache::getAssocClassEntry(const String& fromClassName,
+                                            Array< Array<String> >& entryList)
 {
-    return _assocTable.lookup(fromClassName.getString(), entryList);
-}
+    String lowerCaseFromClassName = fromClassName;
+    lowerCaseFromClassName.toLower();
 
-Boolean AssocClassCache::getReferenceNames(
-    const Array<CIMName>& classList,
-    const Array<CIMName>& resultClassList,
-    const String& role,
-    Array<String>& referenceNames)
-{
-    Array<ClassAssociation> records;
-    Boolean found = false;
-
-    // For each of the target classes retrieve the list of matching
-    // association classes from the cache.
-    // The cache uses the from class name as an index and returns all
-    // association class records having that from class.
-
-    for (Uint16 idx = 0; idx < classList.size(); idx++)
-    {
-        String fromClassName = classList[idx].getString();
-        if (getAssocClassEntry(fromClassName, records))
-        {
-            for (Uint16 rx = 0; rx < records.size(); rx++)
-            {
-                if ((role.size() == 0) ||
-                    (records[rx].fromPropertyName == role))
-                {
-                    // Skip classes that do not appear in the result class list
-                    if ((resultClassList.size() != 0) &&
-                        (!Contains(resultClassList,
-                             records[rx].assocClassName)))
-                    {
-                        continue;
-                    }
-
-                    // This class qualifies; add it to the list (skipping
-                    // duplicates)
-                    if (!Contains(referenceNames,
-                            records[rx].assocClassName.getString()))
-                    {
-                        referenceNames.append(
-                            records[rx].assocClassName.getString());
-                    }
-                    found = true;
-                }
-            }
-        }
-    }
-
-    return found;
+   return _assocClassCache->lookup(lowerCaseFromClassName,entryList);
 }
 
 /** Add a new record to the association cache.
-    If an entry for the given from class name already exists,
-    the new entry is appended to the old entry. Otherwise a new entry
-    is added to the cache.
+ * If an entry for the given from class name already exists,
+ * the new entry is appended to the old entry. Otherwise a new entry
+ * is added to the cache.
 */
-Boolean AssocClassCache::addRecord(
-    const CIMName& fromClassName,
-    const ClassAssociation& assocClassRecord)
+Boolean AssocClassCache::addRecord(const String& fromClassName,
+                                   Array<String> assocClassRecord)
 {
-    Array<ClassAssociation> oldAssocClassEntryList;
+   Array< Array<String> > oldAssocClassEntryList;
+   String lowerCaseFromClassName = fromClassName;
+   lowerCaseFromClassName.toLower();
 
-    if (_assocTable.lookup(fromClassName.getString(), oldAssocClassEntryList))
-    {
-        _assocTable.remove(fromClassName.getString());
-    }
+   if (_assocClassCache->lookup(lowerCaseFromClassName, oldAssocClassEntryList))
+   {
+      _assocClassCache->remove(lowerCaseFromClassName);
+   }
 
-    oldAssocClassEntryList.append(assocClassRecord);
+   oldAssocClassEntryList.append(assocClassRecord);
 
-    return _assocTable.insert(
-        fromClassName.getString(), oldAssocClassEntryList);
+   return _assocClassCache->insert(lowerCaseFromClassName,oldAssocClassEntryList);
 }
 
-/** Remove an association record from the association cache specified by the
-    given from class name and association name.
+/** Remove an entry from the association cache specified by the given
+ *  from class name.
 */
-Boolean AssocClassCache::removeRecord(
-    const CIMName& fromClassName,
-    const CIMName& assocClassName)
+Boolean AssocClassCache::removeEntry(const String& fromClassName)
 {
-    Array<ClassAssociation> oldAssocClassEntryList;
+    String lowerCaseFromClassName = fromClassName;
+    lowerCaseFromClassName.toLower();
 
-    if (_assocTable.lookup(fromClassName.getString(), oldAssocClassEntryList))
-    {
-        for (Uint32 idx=0; idx < oldAssocClassEntryList.size(); idx++)
-        {
-            // Find the record for the association class and remove
-            // it from the cache entry.
-            if (oldAssocClassEntryList[idx].assocClassName == assocClassName)
-            {
-                _assocTable.remove(fromClassName.getString());
-                if (oldAssocClassEntryList.size() > 1)
-                {
-                    oldAssocClassEntryList.remove(idx);
-                    _assocTable.insert(
-                        fromClassName.getString(), oldAssocClassEntryList);
-                }
-                return true;
-            }
-        }
-    }
-
-    return false;
+   return _assocClassCache->remove(lowerCaseFromClassName);
 }
 
-/** Remove association records from the association cache specified by the
-    association class name.
+/** Remove an association record from the association cache specified by the given
+ *  from class name and association name.
 */
-Boolean AssocClassCache::removeAssocClassRecords(const CIMName& assocClassName)
+Boolean AssocClassCache::removeRecord(const String& fromClassName,
+                                      const String& assocClassName)
 {
-    Array<CIMName> fromClassNames;
+   Array< Array<String> > oldAssocClassEntryList;
+   String lowerCaseFromClassName = fromClassName;
+   lowerCaseFromClassName.toLower();
 
-    for (AssocClassCacheHashTableType::Iterator i = _assocTable.start(); i; i++)
-    {
-        Array<ClassAssociation> assocClassEntryList = i.value();
-        for (Uint32 j = 0; j < assocClassEntryList.size(); j++)
-        {
-            if (assocClassEntryList[j].assocClassName == assocClassName)
+   if (_assocClassCache->lookup(lowerCaseFromClassName, oldAssocClassEntryList))
+   {
+      for (Uint16 idx=0; idx < oldAssocClassEntryList.size(); idx++ )
+      {
+         // The first entry in each record is the association class
+         // name. Find the record for the association class and remove
+         // it from the cache entry.
+         if (String::equalNoCase(oldAssocClassEntryList[idx][ASSOC_CLASS_NAME_INDEX],
+                                 assocClassName))
+         {
+            _assocClassCache->remove(lowerCaseFromClassName);
+            if (oldAssocClassEntryList.size() > 1)
             {
-                // Note: We cannot remove an entry from the HashTable while
-                // iterating over it.
-                fromClassNames.append(assocClassEntryList[j].fromClassName);
-                break;
+               oldAssocClassEntryList.remove(idx);
+               _assocClassCache->insert(lowerCaseFromClassName,oldAssocClassEntryList);
             }
-        }
-    }
+            return true;
+         }
+      }
+   }
 
-    for (Uint32 i = 0; i < fromClassNames.size(); i++)
-    {
-        removeRecord(fromClassNames[i], assocClassName);
-    }
-
-    return fromClassNames.size();
+   return false;
 }
 
 /** Check if the cache is loaded with objects already.
 */
 Boolean AssocClassCache::isActive()
 {
-    return _isInitialized;
+   return _isInitialized;
 }
 
 void AssocClassCache::setActive(Boolean flag)
@@ -231,13 +177,14 @@ void AssocClassCache::setActive(Boolean flag)
 
 AssocClassCache::~AssocClassCache()
 {
+    delete(_assocClassCache);
 }
 
 AssocClassCache::AssocClassCache(const String& nameSpace)
-    : _nameSpace(nameSpace),
-      _isInitialized(false),
-      _assocTable(1000)
 {
+    _isInitialized = false;
+   _nameSpace = nameSpace;
+   _assocClassCache = new AssocClassCacheHashTableType(1000);
 }
 
 PEGASUS_NAMESPACE_END

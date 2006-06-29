cc659cd62c4f775f36782faa22278637ab705e18
mike <mike>
2006-06-29 17:31:36 +0000

    BUG#: 5191 TITLE: Queue Consolidation
    DESCRIPTION: Queue Consolidation

--- src/Pegasus/Common/List.cpp
+++ src/Pegasus/Common/List.cpp
@@ -1,99 +1,70 @@
-//%LICENSE////////////////////////////////////////////////////////////////
-//
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
-//
-//%/////////////////////////////////////////////////////////////////////////////
-
+#include <cassert>
 #include "List.h"
-#include "PegasusAssert.h"
+
+#ifdef PEGASUS_LINKABLE_SANITY
+# define LIST_ASSERT(COND) assert(COND)
+#else
+# define LIST_ASSERT(COND) /* empty */
+#endif
+
+#define PEGASUS_LIST_MAGIC 0x1234ABCD
 
 PEGASUS_NAMESPACE_BEGIN
 
-ListRep::ListRep(void (*destructor)(Linkable*))
-    : _front(0), _back(0), _size(0)
+ListRep::ListRep(void (*destructor)(Linkable*)) : 
+    _magic(PEGASUS_LIST_MAGIC), _front(0), _back(0), _size(0)
 {
     if (destructor)
-        _destructor = destructor;
+	_destructor = destructor;
     else
-        _destructor = 0;
+	_destructor = 0;
 }
 
 ListRep::~ListRep()
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
 
     clear();
-#ifdef PEGASUS_DEBUG
+#ifdef PEGASUS_LINKABLE_SANITY
     memset(this, 0xDD, sizeof(ListRep));
 #endif
 }
 
 void ListRep::clear()
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
 
     if (_destructor)
     {
-        // Reset _front, _back, and _size in case the destructor calls
-        // a method of List.
+	for (Linkable* p = _front; p; )
+	{
+	    LIST_ASSERT(p->magic == PEGASUS_LINKABLE_MAGIC);
+	    Linkable* next = p->next;
+	    _destructor(p);
+	    p = next;
+	}
 
-        Linkable* front = _front;
-
-        _front = 0;
-        _back = 0;
-        _size = 0;
-
-        for (Linkable* p = front; p; )
-        {
-            PEGASUS_DEBUG_ASSERT(p->magic);
-            Linkable* next = p->next;
-            p->list = 0;
-            _destructor(p);
-            p = next;
-        }
+	_front = 0;
+	_back = 0;
+	_size = 0;
     }
 }
 
 void ListRep::insert_front(Linkable* elem)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(elem != 0);
-    PEGASUS_DEBUG_ASSERT(elem->magic);
-    PEGASUS_DEBUG_ASSERT(elem->list == 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(elem != 0);
+    LIST_ASSERT(elem->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(elem->list == 0);
 
     elem->list = this;
     elem->next = _front;
     elem->prev = 0;
 
     if (_front)
-        _front->prev = elem;
+	_front->prev = elem;
     else
-        _back = elem;
+	_back = elem;
 
     _front = elem;
     _size++;
@@ -101,98 +72,98 @@ void ListRep::insert_front(Linkable* elem)
 
 void ListRep::insert_back(Linkable* elem)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(elem != 0);
-    PEGASUS_DEBUG_ASSERT(elem->magic);
-    PEGASUS_DEBUG_ASSERT(elem->list == 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(elem != 0);
+    LIST_ASSERT(elem->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(elem->list == 0);
 
     elem->list = this;
     elem->prev = _back;
     elem->next = 0;
 
     if (_back)
-        _back->next = elem;
+	_back->next = elem;
     else
-        _front = elem;
+	_front = elem;
 
     _back = elem;
     _size++;
 }
 
 void ListRep::insert_after(
-    Linkable* pos,
+    Linkable* pos, 
     Linkable* elem)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(pos != 0);
-    PEGASUS_DEBUG_ASSERT(elem != 0);
-    PEGASUS_DEBUG_ASSERT(elem->magic);
-    PEGASUS_DEBUG_ASSERT(pos->magic);
-    PEGASUS_DEBUG_ASSERT(elem->list == 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(pos != 0);
+    LIST_ASSERT(elem != 0);
+    LIST_ASSERT(elem->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(pos->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(elem->list == 0);
 
     elem->list = this;
     elem->prev = pos;
     elem->next = pos->next;
 
     if (pos->next)
-        pos->next->prev = elem;
+	pos->next->prev = elem;
 
     pos->next = elem;
 
     if (pos == _back)
-        _back = elem;
+	_back = elem;
 
     _size++;
 }
 
 void ListRep::insert_before(
-    Linkable* pos,
+    Linkable* pos, 
     Linkable* elem)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(pos != 0);
-    PEGASUS_DEBUG_ASSERT(elem != 0);
-    PEGASUS_DEBUG_ASSERT(pos->magic);
-    PEGASUS_DEBUG_ASSERT(elem->magic);
-    PEGASUS_DEBUG_ASSERT(elem->list == 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(pos != 0);
+    LIST_ASSERT(elem != 0);
+    LIST_ASSERT(pos->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(elem->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(elem->list == 0);
 
     elem->list = this;
     elem->next = pos;
     elem->prev = pos->prev;
 
     if (pos->prev)
-        pos->prev->next = elem;
+	pos->prev->next = elem;
 
     pos->prev = elem;
 
     if (pos == _front)
-        _front = elem;
+	_front = elem;
 
     _size++;
 }
 
 void ListRep::remove(Linkable* pos)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(pos != 0);
-    PEGASUS_DEBUG_ASSERT(pos->magic);
-    PEGASUS_DEBUG_ASSERT(pos->list == this);
-    PEGASUS_DEBUG_ASSERT(_size != 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(pos != 0);
+    LIST_ASSERT(pos->magic == PEGASUS_LINKABLE_MAGIC);
+    LIST_ASSERT(pos->list == this);
+    LIST_ASSERT(_size != 0);
 
     if (_size == 0)
-        return;
+	return;
 
     if (pos->prev)
-        pos->prev->next = pos->next;
+	pos->prev->next = pos->next;
 
     if (pos->next)
-        pos->next->prev = pos->prev;
+	pos->next->prev = pos->prev;
 
     if (pos == _front)
-        _front = pos->next;
+	_front = pos->next;
 
     if (pos == _back)
-        _back = pos->prev;
+	_back = pos->prev;
 
     pos->list = 0;
 
@@ -201,26 +172,30 @@ void ListRep::remove(Linkable* pos)
 
 bool ListRep::contains(const Linkable* elem)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(elem != 0);
-    PEGASUS_DEBUG_ASSERT(elem->magic);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(elem != 0);
+    LIST_ASSERT(elem->magic == PEGASUS_LINKABLE_MAGIC);
 
+    return elem && elem->list == this;
+
+#if 0
     for (const Linkable* p = _front; p; p = p->next)
     {
-        if (p == elem)
-            return true;
+	if (p == elem)
+	    return true;
     }
 
     // Not found!
     return false;
+#endif
 }
 
 Linkable* ListRep::remove_front()
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
 
     if (_size == 0)
-        return 0;
+	return 0;
 
     Linkable* elem = _front;
     remove(elem);
@@ -230,10 +205,8 @@ Linkable* ListRep::remove_front()
 
 Linkable* ListRep::remove_back()
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-
-    if (_size == 0)
-        return 0;
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(_size > 0);
 
     Linkable* elem = _back;
     remove(elem);
@@ -243,16 +216,16 @@ Linkable* ListRep::remove_back()
 
 Linkable* ListRep::find(ListRep::Equal equal, const void* client_data)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(equal != 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(equal != 0);
 
     for (Linkable* p = _front; p; p = p->next)
     {
-        if ((*equal)(p, client_data))
-        {
-            PEGASUS_DEBUG_ASSERT(p->magic);
-            return p;
-        }
+	if ((*equal)(p, client_data))
+	{
+	    LIST_ASSERT(p->magic == PEGASUS_LINKABLE_MAGIC);
+	    return p;
+	}
     }
 
     // Not found!
@@ -261,15 +234,15 @@ Linkable* ListRep::find(ListRep::Equal equal, const void* client_data)
 
 Linkable* ListRep::remove(ListRep::Equal equal, const void* client_data)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(equal != 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(equal != 0);
 
     Linkable* p = find(equal, client_data);
 
     if (p)
     {
-        PEGASUS_DEBUG_ASSERT(p->magic);
-        remove(p);
+	LIST_ASSERT(p->magic == PEGASUS_LINKABLE_MAGIC);
+	remove(p);
     }
 
     return p;
@@ -277,11 +250,11 @@ Linkable* ListRep::remove(ListRep::Equal equal, const void* client_data)
 
 void ListRep::apply(ListRep::Apply apply, const void* client_data)
 {
-    PEGASUS_DEBUG_ASSERT(_magic);
-    PEGASUS_DEBUG_ASSERT(apply != 0);
+    LIST_ASSERT(_magic == PEGASUS_LIST_MAGIC);
+    LIST_ASSERT(apply != 0);
 
     for (Linkable* p = _front; p; p = p->next)
-        (*apply)(p, client_data);
+	(*apply)(p, client_data);
 }
 
 PEGASUS_NAMESPACE_END
--- src/Pegasus/Common/List.h
+++ src/Pegasus/Common/List.h
@@ -1,46 +1,14 @@
-//%LICENSE////////////////////////////////////////////////////////////////
-//
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
-//
-//%/////////////////////////////////////////////////////////////////////////////
-
 #ifndef _Pegasus_Common_List_h
 #define _Pegasus_Common_List_h
 
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/Linkage.h>
 #include <Pegasus/Common/Linkable.h>
-#include <Pegasus/Common/Magic.h>
 
 PEGASUS_NAMESPACE_BEGIN
 
 /** The NullLock class can be passed as the LockType template parameter to the
-    List template class. The NullLock implements no-ops for the lock(),
+    List template class. The NullLock implements no-ops for the lock(), 
     try_lock() and unlock() operations.
 */
 class NullLock
@@ -53,7 +21,7 @@ public:
 
     /** no-op try_lock().
     */
-    Boolean try_lock() { return true; }
+    void try_lock() { }
 
     /** no-op unlock().
     */
@@ -75,9 +43,9 @@ public:
 
     void clear();
 
-    Uint32 size() const { return _size; }
+    size_t size() const { return _size; }
 
-    Boolean empty() const { return _size == 0; }
+    size_t empty() const { return _size == 0; }
 
     Linkable* front() { return _front; }
 
@@ -115,19 +83,19 @@ public:
 
 private:
 
-    ListRep(const ListRep&) { }
-    ListRep& operator=(const ListRep&) { return *this; }
+    ListRep(const ListRep& x) { }
+    ListRep& operator=(const ListRep& x) { return *this; }
 
-    Magic<0x6456FD0A> _magic;
+    Uint32 _magic;
     Linkable* _front;
     Linkable* _back;
-    Uint32 _size;
+    size_t _size;
     void (*_destructor)(Linkable*);
 };
 
 /** The List class implements an intrusive linked list. We say it is intrusive
-    because the element class must derive from Linkable, which provides the
-    link fields used by this implementation. An intrusive implementation has
+    because the element class must derive from Linkable, which provides the 
+    link fields used by this implementation. An intrusive implementation has 
     two main benefits: it is more efficient and uses less memory. Recall that
     a non-intrusive linked list must allocate an additional "node" object that
     contains the links fields as well as a pointer to the element. This implies
@@ -136,145 +104,145 @@ private:
 
     The List template takes two arguments: the element type and the lock type.
     Again, the element type must derive from Linkable. The lock type is used
-    to synchronize access to the list operations and can be any class that
+    to synchronize access to the list operations and can be any class that 
     implements these methods: lock(), try_lock(), unlock(). Here are three
     classes that can be used as the lock type.
 
         - NullLock - no locking at all.
         - Mutex - non-recursive mutex.
-        - Mutex - recursive mutex.
+        - RMutex - recursive mutex.
 
-    Now we consider an example. So you want to create a list of Person
+    Now we consider an example. So you want to create a list of Person 
     elements. First you must derive from the Linkable class as shown below.
 
-        \code
-        class Person : public Linkable
-        {
-            Person(const String& name);
-            .
-            .
-            .
-        }
-        \endcode
+	\code
+	class Person : public Linkable
+	{
+	    Person(const String& name);
+	    .
+	    .
+	    .
+	}
+	\endcode
 
     Linkable is a non-virtual base class and contains the link fields this
     implementation will use to place Person elements onto the list. Second,
     you must instantiate the List template. Here are three possibilities.
 
-        \code
-        List<Person, NullLink>; // Do no synchronization.
-        List<Person, Mutex>; // Use Mutex class to synchronize.
-        List<Person, Mutex>; // Use Mutex class to synchronize.
-        \endcode
+	\code
+	List<Person, NullLink>; // Do no synchronization.
+	List<Person, Mutex>; // Use Mutex class to synchronize.
+	List<Person, RMutex>; // Use RMutex class to synchronize.
+	\endcode
 
     Finally, use the list. The following example adds three Person objects
     to the back of the list and removes one from the front.
 
-        \code
-        typedef List<Person, Mutex> PersonList;
-        PersonList list;
+	\code
+	typedef List<Person, RMutex> PersonList;
+	PersonList list;
 
-        list.insert_back(new Person("John"));
-        list.insert_back(new Person("Mary"));
-        list.insert_back(new Person("Jane"));
+	list.insert_back(new Person("John"));
+	list.insert_back(new Person("Mary"));
+	list.insert_back(new Person("Jane"));
 
-        Person* person = list.remove_front();
-        \endcode
+	Person* person = list.remove_front();
+	\endcode
 
-    One recurring usage pattern is exhausting the elements of a list. For
+    One recurring usage pattern is exhausting the elements of a list. For 
     example (this is thread-safe).
 
-        \code
-        Person* person;
+	\code
+	Person* person;
 
-        while ((person = list.remove_front())
-        {
-            // Do something with person here.
-        }
-        \endcode
+	while ((person = list.remove_front())
+	{
+	    // Do something with person here.
+	}
+	\endcode
 
     Another pattern is to iterate the elements of the list, which requires a
     lock for the duration of the iteration. For example:
 
-        \code
-        list.lock();
+	\code
+	list.lock();
 
-        for (Person* p = list.front(); p; p = list.next_of(p))
-        {
-            // Use p here!
-        }
+	for (Person* p = list.front(); p; p = list.next_of(p))
+	{
+	    // Use p here!
+	}
 
-        list.unlock();
-        \endcode
+	list.unlock();
+	\endcode
 
     Beware that this pattern fails for non-recursive mutexes since the mutex
     is locked once by our example and again by front(). You may have noticed
     that if an exception is thrown before unlock(), that the mutex will
     remain locked. For this reason, the List class contains an AutoLock class
-    that will automatically release the mutex. We now rework the previous
+    that will automatically release the mutex. We now rework the previous 
     example to use an AutoLock object.
 
-        \code
-        {
-            PersonList::AutoLock autoLock(list);
+	\code
+	{
+	    PersonList::AutoLock autoLock(list);
 
-            for (Person* p = list.front(); p; p = list.next_of(p))
-            {
-                // Use p here!
-            }
-        }
-        \endcode
+	    for (Person* p = list.front(); p; p = list.next_of(p))
+	    {
+		// Use p here!
+	    }
+	}
+	\endcode
 
-    The List class provides a special find function to simplify recurring
+    The List class provides a special find function to simplify recurring 
     lookups of an element by some specific criteria. For example, lets add an
     equal function to our Person class as follows:
 
-        \code
-        class Person : public Linkable
-        {
-            Person(const String& name);
-            const String& name();
+	\code
+	class Person : public Linkable
+	{
+	    Person(const String& name);
+	    const String& name();
 
-            static bool equal(const Person* person, const char* client_data)
-            {
-                const String& name = *((const String*)client_data);
-                return person->name() == name;
-            }
-        }
-        \endcode
+	    static bool equal(const Person* person, const char* client_data)
+	    {
+		const String& name = *((const String*)client_data);
+		return person->name() == name;
+	    }
+	}
+	\endcode
 
     This equal function may now be passed to the find() function to lookup
     a specific person in a thread-safe way. For example:
 
-        \code
-        const String name = "John";
-        Person* person = list.find(Person::equal, &name);
+	\code
+	const String name = "John";
+	Person* person = list.find(Person::equal, &name);
 
-        if (person)
-        {
-            // Found John!
-        }
-        \endcode
+	if (person)
+	{
+	    // Found John!
+	}
+	\endcode
 
     Similary, we can delete "John" by using the equal-form of remove().
 
-        \code
-        const String name = "John";
-        Person* person = list.remove(Person::equal, &name);
-        \endcode
+	\code
+	const String name = "John";
+	Person* person = list.remove(Person::equal, &name);
+	\endcode
 
     Finally, we can "apply" a function to every element of the list using
     the apply method. For example:
 
-        \code
-        static void _print(Person* person, const void* client_data)
-        {
-            printf("%s\n", (const char*)client_data);
-            person->print();
-        }
+	\code
+	static void _print(Person* person, const void* client_data)
+	{
+	    printf("%s\n", (const char*)client_data);
+	    person->print();
+	}
 
-        list.apply(_print, "My List");
-        \endcode
+	list.apply(_print, "My List");
+	\endcode
 */
 template<class ElemType, class LockType>
 class List
@@ -289,7 +257,7 @@ public:
 
     /** Default constructor.
     */
-    List() : _rep(_destructor)
+    List() : _rep(_destructor) 
     {
     }
 
@@ -297,143 +265,143 @@ public:
     */
     ~List()
     {
-        AutoLock al(*this);
-        _rep.clear();
+	AutoLock al(*this);
+	_rep.clear();
     }
 
     /** Remove and delete all elements in this list. size() returns zero
-        after this is called.
+	after this is called.
     */
     void clear()
     {
-        AutoLock al(*this);
-        _rep.clear();
+	AutoLock al(*this);
+	_rep.clear();
     }
 
     /** Returns the number of elements in the list.
     */
-    Uint32 size() const
+    size_t size() const 
     {
-        AutoLock al(*this);
-        return _rep.size();
+	AutoLock al(*this);
+	return _rep.size(); 
     }
 
     /** Returns true if the list is empty (i.e., has zero elements).
     */
-    Boolean is_empty() const
-    {
-        AutoLock al(*this);
-        return _rep.empty();
+    size_t is_empty() const 
+    { 
+	AutoLock al(*this);
+	return _rep.empty(); 
     }
 
     /** Obtains a pointer to the element at the front of the list.
     */
-    ElemType* front()
-    {
-        AutoLock al(*this);
-        return static_cast<ElemType*>(_rep.front());
+    ElemType* front() 
+    { 
+	AutoLock al(*this);
+	return static_cast<ElemType*>(_rep.front()); 
     }
 
     /** Obtains a const-pointer to the element at the front of the list.
     */
     const ElemType* front() const
-    {
-        AutoLock al(*this);
-        return static_cast<const ElemType*>(_rep.front());
+    { 
+	AutoLock al(*this);
+	return static_cast<const ElemType*>(_rep.front()); 
     }
 
     /** Obtains a pointer to the element at the back of the list.
     */
-    ElemType* back()
-    {
-        AutoLock al(*this);
-        return static_cast<ElemType*>(_rep.back());
+    ElemType* back() 
+    { 
+	AutoLock al(*this);
+	return static_cast<ElemType*>(_rep.back()); 
     }
 
     /** Obtains a const-pointer to the element at the back of the list.
     */
     const ElemType* back() const
-    {
-        AutoLock al(*this);
-        return static_cast<const ElemType*>(_rep.back());
+    { 
+	AutoLock al(*this);
+	return static_cast<const ElemType*>(_rep.back()); 
     }
 
     /** Returns the element after elem (used to iterate list).
     */
-    ElemType* next_of(ElemType* elem)
+    ElemType* next_of(ElemType* elem) 
     {
-        return static_cast<ElemType*>(elem->next);
+	return static_cast<ElemType*>(elem->next);
     }
 
     /** Returns the element before elem (used to iterate list).
     */
-    ElemType* prev_of(ElemType* elem)
+    ElemType* prev_of(ElemType* elem) 
     {
-        return static_cast<ElemType*>(elem->prev);
+	return static_cast<ElemType*>(elem->prev);
     }
 
     /** Insert elem at the front of the list.
     */
     void insert_front(ElemType* elem)
     {
-        AutoLock al(*this);
-        _rep.insert_front(elem);
+	AutoLock al(*this);
+	_rep.insert_front(elem);
     }
 
     /** Insert elem at the back of the list.
     */
     void insert_back(ElemType* elem)
     {
-        AutoLock al(*this);
-        _rep.insert_back(elem);
+	AutoLock al(*this);
+	_rep.insert_back(elem);
     }
 
     /** Insert elem after pos.
     */
     void insert_after(ElemType* pos, ElemType* elem)
     {
-        AutoLock al(*this);
-        _rep.insert_after(pos, elem);
+	AutoLock al(*this);
+	_rep.insert_after(pos, elem);
     }
 
     /** Insert elem before pos.
     */
     void insert_before(ElemType* pos, ElemType* elem)
     {
-        AutoLock al(*this);
-        _rep.insert_before(pos, elem);
+	AutoLock al(*this);
+	_rep.insert_before(pos, elem);
     }
 
     /** Remove the given element.
     */
     void remove(ElemType* pos)
     {
-        AutoLock al(*this);
-        _rep.remove(pos);
+	AutoLock al(*this);
+	_rep.remove(pos);
     }
 
     /** Returns true if the list contains the given element.
     */
     bool contains(const ElemType* elem)
     {
-        AutoLock al(*this);
-        return _rep.contains(elem);
+	AutoLock al(*this);
+	return _rep.contains(elem);
     }
 
     /** Removes and returns the element at the front of the list.
     */
     ElemType* remove_front()
     {
-        AutoLock al(*this);
-        return static_cast<ElemType*>(_rep.remove_front());
+	AutoLock al(*this);
+	return static_cast<ElemType*>(_rep.remove_front());
     }
 
     /** Removes and returns the element at the back of the list.
     */
     ElemType* remove_back()
     {
-        AutoLock al(*this);
-        return static_cast<ElemType*>(_rep.remove_back());
+	AutoLock al(*this);
+	return static_cast<ElemType*>(_rep.remove_back());
     }
 
     /** Attempts to find an element in the list that satisfies the equal()
@@ -441,77 +409,72 @@ public:
     */
     ElemType* find(Equal equal, const void* client_data)
     {
-        AutoLock al(*this);
-        return static_cast<ElemType*>(
-            _rep.find(ListRep::Equal(equal), client_data));
+	AutoLock al(*this);
+	return static_cast<ElemType*>(
+	    _rep.find(ListRep::Equal(equal), client_data));
     }
 
     /** Attempts to remove the element in the list that satisfies the equal()
         predicate. Removes and returns the first element satisfying equal() or
-        null if none.
+	null if none.
     */
     ElemType* remove(Equal equal, const void* client_data)
     {
-        AutoLock al(*this);
-        return static_cast<ElemType*>(
-            _rep.remove(ListRep::Equal(equal), client_data));
+	AutoLock al(*this);
+	return static_cast<ElemType*>(
+	    _rep.remove(ListRep::Equal(equal), client_data));
     }
 
     /** Apply the given function to every element of the list.
     */
     void apply(Apply apply, const void* client_data)
     {
-        AutoLock al(*this);
-        _rep.apply(ListRep::Apply(apply), client_data);
+	AutoLock al(*this);
+	_rep.apply(ListRep::Apply(apply), client_data);
     }
 
     /** Lock the list.
     */
     void lock() { _lock.lock(); }
 
-    /** Try to lock the list without blocking.
-        @return A Boolean indicating whether the lock was acquired.
+    /** Try to lock the list.
     */
-    Boolean try_lock() { return _lock.try_lock(); }
+    void try_lock() { _lock.try_lock(); }
 
     /** Unlock the list.
     */
     void unlock() { _lock.unlock(); }
 
-    /** Get reference to lock type.
-    */
-    LockType& getLock() const { return ((This*)this)->_lock; }
-
     /** An instance of this class is used to lock this list on construction
-        and later unlock it on destruction.
+	and later unlock it on destruction.
     */
     class AutoLock
     {
     public:
 
-        /**
-        */
-        AutoLock(const This& list) : _lock(list.getLock())
-        {
-            _lock.lock();
-        }
+	/**
+	*/
+	AutoLock(const This& list) : _lock(((This*)&list)->_lock)
+	{
+	    _lock.lock();
+	}
 
-        /**
-        */
-        ~AutoLock()
-        {
-            _lock.unlock();
-        }
+	/**
+	*/
+	~AutoLock() 
+	{
+	    _lock.unlock();
+	}
 
     private:
-        LockType& _lock;
+	LockType& _lock;
     };
 
 private:
 
     static void _destructor(Linkable* ptr)
     {
-        delete static_cast<ElemType*>(ptr);
+	delete static_cast<ElemType*>(ptr);
     }
 
     ListRep _rep;

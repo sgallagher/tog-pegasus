aafd01eee435b23356f95c5996c099d5b6ffa3be
kumpf <kumpf>
2006-08-30 20:42:30 +0000

    BUG#: 5467 TITLE: Use ProviderMessageHandler for control providers DESCRIPTION: Back out OperationResponseHandler.cpp changes.

--- src/Pegasus/ProviderManager2/OperationResponseHandler.cpp
+++ src/Pegasus/ProviderManager2/OperationResponseHandler.cpp
@@ -29,6 +29,12 @@
 //
 //==============================================================================
 //
+// Author: Chip Vincent (cvincent@us.ibm.com)
+//
+// Modified By:
+//         Brian G. Campbell, EMC (campbell_brian@emc.com) - PEP140/phase2
+//         Roger Kumpf, Hewlett-Packard Company (roger_kumpf@hp.com)
+//
 //%/////////////////////////////////////////////////////////////////////////////
 
 #include "OperationResponseHandler.h"
@@ -132,40 +138,37 @@ Boolean OperationResponseHandler::isAsync(void) const
 // to go through. Only operation classes have a response pointer
 void OperationResponseHandler::send(Boolean isComplete)
 {
-    // It is possible to instantiate this class directly (not a derived
-    // class, which would also inherit from SimpleResponseHandler).
-    // The caller would do this only if the operation does not have any
-    // data to be returned.
+	// some handlers do not send async because their callers cannot handle
+	// partial responses. If this is the case, stop here.
 
-    SimpleResponseHandler* simpleP =
-        dynamic_cast<SimpleResponseHandler*>(this);
-    if (simpleP == 0)
-    {
-        // if there is no data to be returned, then the message should NEVER be
-        // incomplete (even on an error)
-        PEGASUS_ASSERT(isComplete);
-        return;
-    }
-
-    // some handlers do not send async because their callers cannot handle
-    // partial responses. If this is the case, stop here.
-
-    if (!isAsync())
-    {
-        // preserve traditional behavior
-        if (isComplete)
+	if (isAsync() == false)
+	{
+		// preserve tradional behavior
+		if (isComplete == true)
         {
-            if (_response->operationContext.contains(
-                    ContentLanguageListContainer::NAME))
-            {
-                _response->operationContext.set(
-                    ContentLanguageListContainer(simpleP->getLanguages()));
-            }
             transfer();
         }
 
         return;
-    }
+	}
+
+	SimpleResponseHandler *simpleP = dynamic_cast<SimpleResponseHandler*>(this);
+
+	// It is possible to instantiate this class directly (not derived)
+	// The caller would do this only if the operation does not have any data to
+	// be returned
+
+	if (! simpleP)
+	{
+		// if there is no data to be returned, then the message should NEVER be
+		// incomplete (even on an error)
+		if (isComplete == false)
+        {
+            PEGASUS_ASSERT(false);
+        }
+
+        return;
+	}
 
 	SimpleResponseHandler &simple = *simpleP;
 	PEGASUS_ASSERT(_response);

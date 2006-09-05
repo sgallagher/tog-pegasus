785aeba5c6e0cffca3a4e78c6877266b5b1ef1d6
karl <karl>
2006-09-05 19:59:33 +0000

    BUG#: 5499 TITLE: static const defintions in CommonUTF.h
    DESCRIPTION: Moved the static const definitions to CommonUTF.cpp which
    already exists.

--- src/Pegasus/Common/CommonUTF.h
+++ src/Pegasus/Common/CommonUTF.h
@@ -52,31 +52,20 @@ PEGASUS_NAMESPACE_BEGIN
 #define MAX_BYTE              (Uint32)0x0000FFFF
 #define MAX_UTF16             (Uint32)0x0010FFFF
 
-static const Uint32 halfBase = 0x0010000UL;
-static const Uint32 halfMask = 0x3FFUL;
-static const int halfShift  = 10;
-static const Uint8 firstByteMark[7] = { 0x00, 0x00, 0xC0, 0xE0, 0xF0, 0xF8, 0xFC };
+PEGASUS_COMMON_LINKAGE extern const Uint32 halfBase;
+PEGASUS_COMMON_LINKAGE extern  const Uint32 halfMask;
+PEGASUS_COMMON_LINKAGE extern  const int halfShift;
+PEGASUS_COMMON_LINKAGE extern  const Uint8 firstByteMark[];
 
-static const Uint32 offsetsFromUTF8[6] = { 0x00000000UL, 0x00003080UL, 0x000E2080UL,
-             0x03C82080UL, 0xFA082080UL, 0x82082080UL };
+PEGASUS_COMMON_LINKAGE extern  const Uint32 offsetsFromUTF8[];
 
-static const char trailingBytesForUTF8[256] = {
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2, 3,3,3,3,3,3,3,3,4,4,4,4,5,5,5,5
-};
+PEGASUS_COMMON_LINKAGE extern  const char trailingBytesForUTF8[];
 
 #define UTF_8_COUNT_TRAIL_BYTES(leadByte) (trailingBytesForUTF8[(Uint8)leadByte])
 
 #define UTF8_NEXT(s, i) { \
     (i)=((i) + UTF_8_COUNT_TRAIL_BYTES((s)[(i)]) + 1); \
 }
-
 
 PEGASUS_COMMON_LINKAGE Boolean isValid_U8(const Uint8 *src,int size);
 

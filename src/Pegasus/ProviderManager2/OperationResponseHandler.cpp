3320fded50a5e5a8e5e67235fd450efb5ecae14a
venkat.puvvada <venkat.puvvada>
2006-09-15 10:43:48 +0000

    BUG#: 5464 TITLE: CMPI Get Instance does not return error correctly

--- src/Pegasus/ProviderManager2/OperationResponseHandler.cpp
+++ src/Pegasus/ProviderManager2/OperationResponseHandler.cpp
@@ -17,7 +17,7 @@
 // rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
 // sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
 // ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
 // "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
@@ -344,7 +344,8 @@ void GetInstanceResponseHandler::complete(void)
         MessageLoaderParms message(
             "Server.OperationResponseHandler.TOO_FEW_OBJECTS_DELIVERED",
             "Too few objects delivered.");
-
+        // Provider claims success, no instances returned. -V see Bug #4104
+        setStatus(CIM_ERR_NOT_FOUND);
         throw CIMException(CIM_ERR_FAILED, message);
     }
 

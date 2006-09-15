69f7033d9fb8a632b2451fab412a15b2741c8adf
marek <marek>
2006-09-15 17:47:23 +0000

    BUG#:5337 TITLE: Pegasus needs a Test for static usage of the CIMClient interface
    DESCRIPTION:

---
+++ src/Pegasus/Client/tests/TestStaticClient/TestStaticClient.cpp
@@ -0,0 +1,93 @@
+//%2006////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+//%////////////////////////////////////////////////////////////////////////////
+//
+// This CIM client program is used to test if a static version of the
+// CIM Client works. Focus lies espescially on constructor, destructor and
+// automatic disconnect from the server.
+//
+// ==========================================================================
+
+#include <Pegasus/Client/CIMClient.h>
+#include <cstring>
+
+PEGASUS_USING_STD;
+PEGASUS_USING_PEGASUS;
+
+// For the resolution of Bug#4590
+#ifdef PEGASUS_OS_DARWIN
+CIMClient& getCIMClientRef()
+{
+        static CIMClient client;
+        return client;
+}
+#define client getCIMClientRef()
+#else
+        static CIMClient client;
+#endif
+
+////////////////////////////////////////////////////////////////////////////
+//  _errorExit
+////////////////////////////////////////////////////////////////////////////
+
+void _errorExit(String message)
+{
+    cerr << "TestStaticClient error: " << message << endl;
+    exit(1);
+}
+
+// =========================================================================
+//    Main
+// =========================================================================
+
+int main(int argc, char** argv)
+{
+    // Connect to server
+    try
+    {
+        client.connectLocal();
+    }
+    catch (Exception& e)
+    {
+        _errorExit(e.getMessage());
+    }
+
+    try
+    {
+        cout << "\n+++++ passed all tests" << endl;
+    }
+    catch(Exception)
+    {
+        cout << "\n----- Test Static Client Failed" << endl;
+    }
+    return(0);
+}

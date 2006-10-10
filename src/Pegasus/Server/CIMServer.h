2e48dce590d065946ac13df424ba4996e39141e8
mreddy <mreddy>
2006-10-10 04:44:25 +0000

    PEP#: 267 TITLE: External SLP SA Support

--- src/Pegasus/Server/CIMServer.h
+++ src/Pegasus/Server/CIMServer.h
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -34,17 +36,25 @@
 
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/InternalException.h>
-#include <Pegasus/Common/Monitor.h>
+#include <Pegasus/Common/Cimom.h>
 #include <Pegasus/Common/SSLContext.h>
 #include <Pegasus/Repository/CIMRepository.h>
 #include <Pegasus/ProviderManager2/Default/ProviderMessageHandler.h>
 #include <Pegasus/Server/CIMServerState.h>
 #include <Pegasus/Server/HTTPAuthenticatorDelegator.h>
 #include <Pegasus/Server/Linkage.h>
-#include <Pegasus/Common/SCMOClassCache.h>
 
 PEGASUS_NAMESPACE_BEGIN
 
+struct ServerRep;
+#ifdef PEGASUS_ENABLE_SLP
+#include <slp/slp_client/src/cmd-utils/slp_client/lslp-linux.h>
+#include <slp/slp_client/src/cmd-utils/slp_client/lslp.h>
+#include <slp/slp_client/src/cmd-utils/slp_client/lslp-common-defs.h>
+#include <slp/slp_client/src/cmd-utils/slp_client/slp_client.h>
+#endif
+
+class Monitor;
 class CIMOperationRequestDispatcher;
 class CIMOperationResponseEncoder;
 class CIMOperationRequestDecoder;
@@ -52,11 +62,6 @@ class CIMOperationRequestAuthorizer;
 class CIMExportRequestDispatcher;
 class CIMExportResponseEncoder;
 class CIMExportRequestDecoder;
-class WsmProcessor;
-class RsProcessor;
-#ifdef PEGASUS_ENABLE_PROTOCOL_WEB
-class WebServer;
-#endif /* PEGASUS_ENABLE_PROTOCOL_WEB */
 class HTTPAcceptor;
 class CIMRepository;
 
@@ -65,9 +70,11 @@ class IndicationHandlerService;
 class IndicationService;
 class ProviderManagerService;
 class ProviderRegistrationManager;
+class BinaryMessageHandler;
 class SSLContextManager;
-
-
+#ifdef PEGASUS_SLP_REG_TIMEOUT
+int _deregPegasus();
+#endif
 class PEGASUS_SERVER_LINKAGE CIMServer
 {
 public:
@@ -75,30 +82,32 @@ public:
     enum Protocol { PROPRIETARY, STANDARD };
 
     /** Constructor - Creates a CIMServer object.
+        The CIM Server objects establishes a repository object,
+        a dispatcher object, and creates a channnel factory and
+        acceptor for the Server.
+        @param monitor	  monitor object for the server.
+        @exception - ATTN
     */
-    CIMServer();
+    CIMServer(Monitor* monitor);
 
     ~CIMServer();
 
     /** Adds a connection acceptor for the specified listen socket.
-        @param connectionType specifying whether the acceptor should
-               listen on LOCAL_CONNECTION or IPV4_CONNECTION or
-               IPV6_CONNECTION socket. portNumber is ignored if
-               connectionType is LOCAL_CONNECTION.
+        @param localConnection Boolean specifying whether the acceptor should
+               listen on a local-system-only connection.
         @param portNumber Port number that should be used by the listener.
                This parameter is ignored if localConnection=true.
         @param useSSL Boolean specifying whether SSL should be used for
                connections created by this acceptor.
     */
     void addAcceptor(
-        Uint16 connectionType,
+        Boolean localConnection,
         Uint32 portNumber,
-        Boolean useSSL,
-        HostAddress *ipAddress = 0);
+        Boolean useSSL);
 
     /** Bind the acceptors to the specified listen sockets.
-        @exception - This function may receive exceptions from
-        Channel specific subfunctions.
+	@exception - This function may receive exceptions from
+	Channel specific subfunctions.
     */
     void bind();
 
@@ -135,12 +144,6 @@ public:
 
     Uint32 getOutstandingRequestCount();
 
-    /** Performs any initializations required before accepting the requests.
-        Currently this method is used for IndicationService to send the
-        CIMSubscriptionInitCompleteRequestMessage.
-    */
-    void initComplete();
-
     /** Signal to shutdown
     */
     static void shutdownSignal();
@@ -151,20 +154,19 @@ public:
     */
     void startSLPProvider();
 
-    /**
-        This function gets the current environment variables, the current
-        configuration properties, and the currently registered provider
-        modules whent the CIM Server starts with the configuration
-        property "enableAuditLog" set to true.
-    */
-    static void auditLogInitializeCallback();
-
 private:
     Boolean _dieNow;
 
-    AutoPtr<Monitor> _monitor;
-    CIMRepository* _repository;
+#ifdef PEGASUS_ENABLE_SLP
+    Boolean _runSLP;
+# ifdef PEGASUS_SLP_REG_TIMEOUT
+    Boolean _startAdvThread;
+    Thread *SLPThread;
+# endif
+#endif
 
+    Monitor* _monitor;
+    CIMRepository* _repository;
     CIMOperationRequestDispatcher* _cimOperationRequestDispatcher;
     CIMOperationResponseEncoder* _cimOperationResponseEncoder;
     CIMOperationRequestDecoder* _cimOperationRequestDecoder;
@@ -175,30 +177,19 @@ private:
     CIMExportRequestDecoder* _cimExportRequestDecoder;
     HTTPAuthenticatorDelegator* _httpAuthenticatorDelegator;
 
-    RsProcessor* _rsProcessor;
-#ifdef PEGASUS_ENABLE_PROTOCOL_WEB
-    WebServer* _webServer;
-#endif
-#ifdef PEGASUS_ENABLE_PROTOCOL_WSMAN
-    WsmProcessor* _wsmProcessor;
-#endif
-
     Array<HTTPAcceptor*> _acceptors;
     Array<ProviderMessageHandler*> _controlProviders;
-    AutoPtr<CIMServerState> _serverState;
+    AutoPtr<CIMServerState> _serverState; //PEP101
 
     ModuleController* _controlService;
     IndicationHandlerService* _handlerService;
     IndicationService* _indicationService;
     ProviderManagerService* _providerManager;
     ProviderRegistrationManager* _providerRegistrationManager;
+    BinaryMessageHandler *_binaryMessageHandler;
     SSLContextManager* _sslContextMgr;
-
-    static SCMOClass _scmoClassCache_GetClass(
-        const CIMNamespaceName& nameSpace,
-        const CIMName& className);
     
-    void _init();
+    void _init(void);
     SSLContext* _getSSLContext();
 };
 

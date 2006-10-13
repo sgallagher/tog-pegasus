65577374ec7858cebfd48adef4c2a9dd94709275
gs.keenan <gs.keenan>
2006-10-13 18:08:18 +0000

    BUG#: 5681 TITLE: OpenVMS - providerDir default points to the wrong location.
    DESCRIPTION: Add vms only code to define 'bin' instead of 'lib' as default.

--- src/Pegasus/Config/ProviderDirPropertyOwner.cpp
+++ src/Pegasus/Config/ProviderDirPropertyOwner.cpp
@@ -1,37 +1,39 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
 
 ///////////////////////////////////////////////////////////////////////////////
-//
+// 
 // This file has implementation for the providerDir property owner class.
 //
 ///////////////////////////////////////////////////////////////////////////////
@@ -41,7 +43,7 @@
 #include <Pegasus/Common/FileSystem.h>
 #include <Pegasus/Config/ConfigManager.h>
 #include "ProviderDirPropertyOwner.h"
-#include "ConfigExceptions.h"
+
 
 PEGASUS_NAMESPACE_BEGIN
 
@@ -54,17 +56,14 @@ PEGASUS_NAMESPACE_BEGIN
 
 static struct ConfigPropertyRow properties[] =
 {
-#if defined(PEGASUS_OS_TYPE_WINDOWS)
-    {"providerDir", "lib;bin", IS_DYNAMIC, IS_VISIBLE},
-#elif defined(PEGASUS_OS_ZOS)
-    {"providerDir", "lib:provider", IS_DYNAMIC, IS_VISIBLE},
-#elif defined(PEGASUS_OS_PASE) && defined(PEGASUS_USE_RELEASE_DIRS)
-    {"providerDir", "/QOpenSys/QIBM/ProdData/UME/Pegasus/provider",
-        IS_DYNAMIC, IS_VISIBLE}
+#if defined(PEGASUS_PLATFORM_WIN32_IX86_MSVC)
+    {"providerDir", "lib;bin", IS_STATIC, 0, 0, IS_VISIBLE},
+#elif defined(PEGASUS_PLATFORM_ZOS_ZSERIES_IBM)
+    {"providerDir", "lib:provider", IS_STATIC, 0, 0, IS_VISIBLE},
 #elif defined(PEGASUS_OS_VMS)
-    {"providerDir", "/wbem_lib", IS_DYNAMIC, IS_VISIBLE},
+    {"providerDir", "bin", IS_STATIC, 0, 0, IS_VISIBLE},
 #else
-    {"providerDir", "lib", IS_DYNAMIC, IS_VISIBLE},
+    {"providerDir", "lib", IS_STATIC, 0, 0, IS_VISIBLE},
 #endif
 };
 
@@ -84,56 +83,59 @@ ProviderDirPropertyOwner::~ProviderDirPropertyOwner()
 }
 
 /**
-    Checks if the given directory is existing and writable
+Checks if the given directory is existing and writable
 */
 Boolean isProviderDirValid(const String& dirName)
 {
-    String path;
-    Uint32 pos=0;
-    Uint32 token=0;
+      String temp = dirName;
+      String path = String::EMPTY;
+      Uint32 pos =0;
+      Uint32 token=0;
 
-    // make multiple, relative paths absolute for check
-    String temp = ConfigManager::getHomedPath(dirName);
-    do
-    {
-        if (( pos = temp.find(FileSystem::getPathDelimiter())) == PEG_NOT_FOUND)
-        {
-            pos = temp.size();
-            token = 0;
-        }
-        else
-        {
-            token = 1;
-        }
-        path = temp.subString(0,pos);
-        if (!FileSystem::isDirectory(path))
-        {
-            Logger::put_l(
-                Logger::ERROR_LOG, System::CIMSERVER, Logger::SEVERE,
-                MessageLoaderParms(
-                    "Config.ProviderDirPropertyOwner.NOT_A_DIRECTORY",
-                    "providerDir configuration value $0 is not a directory.",
-                    path));
-            return false;
-        }
-        if (!FileSystem::canRead(path))
-        {
-            Logger::put_l(
-                Logger::ERROR_LOG, System::CIMSERVER, Logger::SEVERE,
-                MessageLoaderParms(
-                    "Config.ProviderDirPropertyOwner.DIRECTORY_NOT_READABLE",
-                    "providerDir configuration directory $0 is not readable.",
-                    path));
-            return false;
-        }
-        temp.remove(0,pos+token);
-    }
-    while ( temp.size() > 0 );
+      do {
+	if (( pos = temp.find(FileSystem::getPathDelimiter())) == PEG_NOT_FOUND) {
+		pos = temp.size();
+		token = 0;
+	}
+	else {
+		token = 1;
+	}
+	path = temp.subString(0,pos);
+#ifdef PEGASUS_PLATFORM_ZOS_ZSERIES_IBM
+    path.assign(ConfigManager::getHomedPath(path));
+#endif
+	if (FileSystem::canWrite(path)) {
+		Logger::put_l(Logger::ERROR_LOG,System::CIMSERVER,
+                        Logger::WARNING,
+                        "$0 is writeable! Possible security risk.",
+                        path);
+	}
+	if ( !FileSystem::isDirectory(path)  ||
+	     !FileSystem::canRead(path)) {
+		if (!FileSystem::isDirectory(path)) {
+ 			Logger::put_l(Logger::ERROR_LOG,System::CIMSERVER,
+                        Logger::SEVERE,
+                        "$0 is not a directory!",
+                        path);
+		}
+		if (!FileSystem::canRead(path)) {
+		 	Logger::put_l(Logger::ERROR_LOG,System::CIMSERVER,
+                        Logger::SEVERE,
+                        "Cannot $0 is not readable!",
+                        path);
+		}
+		//cerr << "Not a good directory.\n";
+		return false;
+	}	
+	temp.remove(0,pos+token);	
+      }
+      while ( temp.size() > 0 );	
+      
     return true;
 }
-
+ 
 /**
-    Initialize the config properties.
+Initialize the config properties.
 */
 void ProviderDirPropertyOwner::initialize()
 {
@@ -142,13 +144,16 @@ void ProviderDirPropertyOwner::initialize()
         //
         // Initialize the properties with default values
         //
-        if (String::equal(properties[i].propertyName, "providerDir"))
+        if (String::equalNoCase(properties[i].propertyName, "providerDir"
+))
         {
             _providerDir->propertyName = properties[i].propertyName;
             _providerDir->defaultValue = properties[i].defaultValue;
             _providerDir->currentValue = properties[i].defaultValue;
             _providerDir->plannedValue = properties[i].defaultValue;
             _providerDir->dynamic = properties[i].dynamic;
+            _providerDir->domain = properties[i].domain;
+            _providerDir->domainSize = properties[i].domainSize;
             _providerDir->externallyVisible = properties[i].externallyVisible;
         }
     }
@@ -157,7 +162,7 @@ void ProviderDirPropertyOwner::initialize()
 struct ConfigProperty* ProviderDirPropertyOwner::_lookupConfigProperty(
     const String& name) const
 {
-    if (String::equal(_providerDir->propertyName, name))
+    if (String::equalNoCase(_providerDir->propertyName, name))
     {
         return _providerDir;
     }
@@ -167,20 +172,41 @@ struct ConfigProperty* ProviderDirPropertyOwner::_lookupConfigProperty(
     }
 }
 
-/**
-    Get information about the specified property.
+/** 
+Get information about the specified property.
 */
 void ProviderDirPropertyOwner::getPropertyInfo(
-    const String& name,
+    const String& name, 
     Array<String>& propertyInfo) const
 {
+    propertyInfo.clear();
     struct ConfigProperty * configProperty = _lookupConfigProperty(name);
 
-    buildPropertyInfo(name, configProperty, propertyInfo);
+    propertyInfo.append(configProperty->propertyName);
+    propertyInfo.append(configProperty->defaultValue);
+    propertyInfo.append(configProperty->currentValue);
+    propertyInfo.append(configProperty->plannedValue);
+    if (configProperty->dynamic)
+    {
+        propertyInfo.append(STRING_TRUE);
+    }
+    else
+    {
+        propertyInfo.append(STRING_FALSE);
+    }
+    if (configProperty->externallyVisible)
+    {
+        propertyInfo.append(STRING_TRUE);
+    }
+    else
+    {
+        propertyInfo.append(STRING_FALSE);
+    }
 }
 
+
 /**
-    Get default value of the specified property.
+Get default value of the specified property.
 */
 String ProviderDirPropertyOwner::getDefaultValue(const String& name) const
 {
@@ -189,19 +215,18 @@ String ProviderDirPropertyOwner::getDefaultValue(const String& name) const
     return configProperty->defaultValue;
 }
 
-/**
-    Get current value of the specified property.
+/** 
+Get current value of the specified property.
 */
 String ProviderDirPropertyOwner::getCurrentValue(const String& name) const
 {
     struct ConfigProperty * configProperty = _lookupConfigProperty(name);
 
-    AutoMutex lock(_providerDirMutex);
     return configProperty->currentValue;
 }
 
-/**
-    Get planned value of the specified property.
+/** 
+Get planned value of the specified property.
 */
 String ProviderDirPropertyOwner::getPlannedValue(const String& name) const
 {
@@ -210,89 +235,83 @@ String ProviderDirPropertyOwner::getPlannedValue(const String& name) const
     return configProperty->plannedValue;
 }
 
-/**
-    Init current value of the specified property to the specified value.
+/** 
+Init current value of the specified property to the specified value.
 */
 void ProviderDirPropertyOwner::initCurrentValue(
-    const String& name,
+    const String& name, 
     const String& value)
 {
     struct ConfigProperty* configProperty = _lookupConfigProperty(name);
-    AutoMutex lock(_providerDirMutex);
     configProperty->currentValue = value;
 }
 
 
-/**
-    Init planned value of the specified property to the specified value.
+/** 
+Init planned value of the specified property to the specified value.
 */
 void ProviderDirPropertyOwner::initPlannedValue(
-    const String& name,
+    const String& name, 
     const String& value)
 {
     struct ConfigProperty* configProperty = _lookupConfigProperty(name);
     configProperty->plannedValue = value;
 }
 
-/**
-    Update current value of the specified property to the specified value.
+/** 
+Update current value of the specified property to the specified value.
 */
 void ProviderDirPropertyOwner::updateCurrentValue(
-    const String& name,
-    const String& value,
-    const String& userName,
-    Uint32 timeoutSeconds)
+    const String& name, 
+    const String& value) 
 {
-   struct ConfigProperty * configProperty = _lookupConfigProperty(name);
-
     //
     // make sure the property is dynamic before updating the value.
     //
-    if (configProperty->dynamic != IS_DYNAMIC)
+    if (!isDynamic(name))
     {
-        throw NonDynamicConfigProperty(name);
+        throw NonDynamicConfigProperty(name); 
     }
 
-    AutoMutex lock(_providerDirMutex);
+    struct ConfigProperty* configProperty = _lookupConfigProperty(name);
     configProperty->currentValue = value;
 }
 
 
-/**
-    Update planned value of the specified property to the specified value.
+/** 
+Update planned value of the specified property to the specified value.
 */
 void ProviderDirPropertyOwner::updatePlannedValue(
-    const String& name,
+    const String& name, 
     const String& value)
 {
     struct ConfigProperty* configProperty = _lookupConfigProperty(name);
     configProperty->plannedValue = value;
 }
 
-/**
-    Checks to see if the given value is valid or not.
+/** 
+Checks to see if the given value is valid or not.
 */
-Boolean ProviderDirPropertyOwner::isValid(
-    const String& name,
-    const String& value) const
+Boolean ProviderDirPropertyOwner::isValid(const String& name, 
+                                const String& value) const
 {
 
     if (!isProviderDirValid( value ))
     {
         throw InvalidPropertyValue(name, value);
     }
-
+ 
     return true;
 }
 
-/**
-    Checks to see if the specified property is dynamic or not.
+/** 
+Checks to see if the specified property is dynamic or not.
 */
 Boolean ProviderDirPropertyOwner::isDynamic(const String& name) const
 {
     struct ConfigProperty * configProperty = _lookupConfigProperty(name);
 
-    return (configProperty->dynamic == IS_DYNAMIC);
+    return (configProperty->dynamic==IS_DYNAMIC);
 }
 
 

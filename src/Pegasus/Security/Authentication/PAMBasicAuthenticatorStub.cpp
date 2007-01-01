036ba01edb2bc64cc10cfd7239ffa7340dd3447b
mike <mike>
2007-01-01 16:43:13 +0000

    PEP#: 286 TITLE: Privilege Separation
    DESCRIPTION: Ongoing privilege separation work.

--- src/Pegasus/Security/Authentication/PAMBasicAuthenticatorStub.cpp
+++ src/Pegasus/Security/Authentication/PAMBasicAuthenticatorStub.cpp
@@ -1,31 +1,38 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// Author: Nag Boranna, Hewlett-Packard Company(nagaraja_boranna@hp.com)
 //
-//////////////////////////////////////////////////////////////////////////
+// Modified By: Josephine Eskaline Joyce (jojustin@in.ibm.com) for PEP#101
+//            Sushma Fernandes, Hewlett-Packard Company(sushma_fernandes@hp.com)
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -40,70 +47,80 @@ PEGASUS_USING_STD;
 
 PEGASUS_NAMESPACE_BEGIN
 
+
 /**
     Constant representing the Basic authentication challenge header.
 */
-static const String BASIC_CHALLENGE_HEADER = "WWW-Authenticate: Basic ";
+static const String BASIC_CHALLENGE_HEADER = "WWW-Authenticate: Basic \"";
+
 
 /* constructor. */
-PAMBasicAuthenticator::PAMBasicAuthenticator()
-{
+PAMBasicAuthenticator::PAMBasicAuthenticator() 
+{ 
     PEG_METHOD_ENTER(TRC_AUTHENTICATION,
         "PAMBasicAuthenticator::PAMBasicAuthenticator()");
 
-    // Build Authentication parameter realm required for Basic Challenge
-    // e.g. realm="HostName"
+    //
+    // get the local system name
+    //
+    _realm.assign(System::getHostName());
 
-    _realm.assign("realm=");
-    _realm.append(Char16('"'));
-    _realm.append(System::getHostName());
-    _realm.append(Char16('"'));
+    //
+    // get the configured port number
+    //
+    ConfigManager* configManager = ConfigManager::getInstance();
+
+    String port = configManager->getCurrentValue("httpPort");
+
+    //
+    // Create realm that will be used for Basic challenges
+    //
+    _realm.append(":");
+    _realm.append(port);
 
     PEG_METHOD_EXIT();
 }
 
 /* destructor. */
-PAMBasicAuthenticator::~PAMBasicAuthenticator()
-{
+PAMBasicAuthenticator::~PAMBasicAuthenticator() 
+{ 
     PEG_METHOD_ENTER(TRC_AUTHENTICATION,
         "PAMBasicAuthenticator::~PAMBasicAuthenticator()");
 
     PEG_METHOD_EXIT();
 }
 
-AuthenticationStatus PAMBasicAuthenticator::authenticate(
-    const String& userName,
-    const String& password,
-    AuthenticationInfo* authInfo)
+Boolean PAMBasicAuthenticator::authenticate(
+    const String& userName, 
+    const String& password) 
 {
     PEG_METHOD_ENTER(TRC_AUTHENTICATION,
         "PAMBasicAuthenticator::authenticate()");
 
-    // FUTURE: PAM authentication is not implemented on non Unix platforms,
-    // hence this function return false as default indicating authentication
-    // failure.
+    //FUTURE: PAM authentication is not implemented on non Unix platforms,
+    //hence this function return false as default indicating authentication failure.
 
     Boolean authenticated = false;
 
     PEG_METHOD_EXIT();
-    return AuthenticationStatus(authenticated);
+
+    return (authenticated);
 }
 
-AuthenticationStatus PAMBasicAuthenticator::validateUser(
-    const String& userName,
-    AuthenticationInfo* authInfo)
+Boolean PAMBasicAuthenticator::validateUser(
+    const String& userName)
 {
     PEG_METHOD_ENTER(TRC_AUTHENTICATION,
         "PAMBasicAuthenticator::validateUser()");
 
-    // FUTURE: PAM authentication is not implemented on non Unix platforms,
-    // hence this function return false as default indicating validation
-    // failure.
+    //FUTURE: PAM authentication is not implemented on non Unix platforms,
+    //hence this function return false as default indicating validation failure.
 
     Boolean authenticated = false;
 
     PEG_METHOD_EXIT();
-    return AuthenticationStatus(authenticated);
+
+    return (authenticated);
 }
 
 //
@@ -114,14 +131,16 @@ String PAMBasicAuthenticator::getAuthResponseHeader()
     PEG_METHOD_ENTER(TRC_AUTHENTICATION,
         "PAMBasicAuthenticator::getAuthResponseHeader()");
 
-    //
+    // 
     // build response header using realm
     //
     String responseHeader = BASIC_CHALLENGE_HEADER;
     responseHeader.append(_realm);
+    responseHeader.append("\"");
 
     PEG_METHOD_EXIT();
-    return responseHeader;
+
+    return (responseHeader);
 }
 
 PEGASUS_NAMESPACE_END

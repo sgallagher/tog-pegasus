0199b35fbb8c00924cc56a71bef1fb1c926d69ec
kumpf <kumpf>
2007-05-08 20:04:05 +0000

    PEP#: 286 TITLE: Privilege Separation DESCRIPTION: Merge with updates in the main branch.

--- src/Service/ServerProcessUnix.cpp
+++ src/Service/ServerProcessUnix.cpp
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -33,7 +35,6 @@
 #include <unistd.h>
 
 #include <Pegasus/Common/Signal.h>
-#include <Pegasus/Common/Executor.h>
 
 #define MAX_WAIT_TIME 240
 
@@ -51,7 +52,7 @@ void sigUsr1Handler(int s_n, PEGASUS_SIGINFO_T * s_info, void * sig)
 void sigTermHandler(int s_n, PEGASUS_SIGINFO_T * s_info, void * sig)
 {
     graveError= handleSigUsr1=true;
-}
+} 
 
 
 //constructor
@@ -63,12 +64,12 @@ ServerProcess::~ServerProcess() {}
 // no-ops
 void ServerProcess::cimserver_set_process(void* p) {}
 void ServerProcess::cimserver_exitRC(int rc) {}
-int ServerProcess::cimserver_initialize() { return 1; }
+int ServerProcess::cimserver_initialize(void) { return 1; }
 
 // for all OSes supporting signals provide a cimserver_wait function
 // that waits to be awakened by signal PEGASUS_SIGTERM or PEGASUS_SIGHUP
 #ifdef PEGASUS_HAS_SIGNALS
-int ServerProcess::cimserver_wait()
+int ServerProcess::cimserver_wait(void)
 {
     int sig = -1;
     sigset_t set;
@@ -78,7 +79,7 @@ int ServerProcess::cimserver_wait()
     errno = 0;
     do
     {
-#if defined(PEGASUS_OS_ZOS)
+#if defined(PEGASUS_OS_ZOS) || defined(PEGASUS_OS_SOLARIS)
         sig = sigwait(&set);
 #else // else for platforms = LINUX, HPUX, AIX
         sigwait(&set, &sig);
@@ -87,70 +88,71 @@ int ServerProcess::cimserver_wait()
     return sig;
 }
 #else
-int ServerProcess::cimserver_wait() { return 1; }
+int ServerProcess::cimserver_wait(void) { return 1; }
 #endif
 
-String ServerProcess::getHome() { return String::EMPTY; }
+String ServerProcess::getHome(void) { return String::EMPTY; }
 
 // daemon_init , RW Stevens, "Advance UNIX Programming"
 
-int ServerProcess::cimserver_fork()
-{
-    umask(S_IRWXG | S_IRWXO);
-
-    if (Executor::detectExecutor() == 0)
-    {
-        // We don't need to fork if we're running with Privilege Separation
-        return 0;
-    }
+int ServerProcess::cimserver_fork(void) 
+{ 
+#if defined(PEGASUS_ENABLE_PRIVILEGE_SEPARATION)
 
     getSigHandle()->registerHandler(SIGTERM, sigTermHandler);
     getSigHandle()->activate(SIGTERM);
-    getSigHandle()->registerHandler(PEGASUS_SIGUSR1, sigUsr1Handler);
-    getSigHandle()->activate(PEGASUS_SIGUSR1);
-
-    pid_t pid;
-    if( (pid = fork() ) < 0)
-    {
-        getSigHandle()->deactivate(PEGASUS_SIGUSR1);
-        getSigHandle()->deactivate(SIGTERM);
-        return -1;
-    }
-    else if (pid != 0)
-    {
-        //
-        // parent wait for child
-        // if there is a problem with signal, parent process terminates
-        // when waitTime expires
-        //
-        Uint32 waitTime = MAX_WAIT_TIME;
-
-        while (!handleSigUsr1 && waitTime > 0)
-        {
-            sleep(1);
-            waitTime--;
-        }
-
-        if (!handleSigUsr1)
-        {
-            MessageLoaderParms parms(
-                "src.Service.ServerProcessUnix.CIMSERVER_START_TIMEOUT",
-                "The cimserver command timed out waiting for the CIM server "
-                    "to start.");
-            PEGASUS_STD(cerr) << MessageLoader::getMessage(parms) <<
-                PEGASUS_STD(endl);
-        }
-        exit(graveError);
-    }
-
-    setsid();
+    server_pid = getpid();
     umask(S_IRWXG | S_IRWXO);
 
-    // spawned daemon process doesn't need the old signal handlers of its parent
-    getSigHandle()->deactivate(PEGASUS_SIGUSR1);
-    getSigHandle()->deactivate(SIGTERM);
-
     return 0;
+
+#else /* !defined(PEGASUS_ENABLE_PRIVILEGE_SEPARATION) */
+
+    getSigHandle()->registerHandler(PEGASUS_SIGUSR1, sigUsr1Handler);
+    getSigHandle()->activate(PEGASUS_SIGUSR1);
+    getSigHandle()->registerHandler(SIGTERM, sigTermHandler);
+    getSigHandle()->activate(SIGTERM);
+ 
+  pid_t pid;
+  if( (pid = fork() ) < 0) 
+  {
+      getSigHandle()->deactivate(PEGASUS_SIGUSR1);
+      getSigHandle()->deactivate(SIGTERM);
+      return(-1);
+  }
+  else if (pid != 0)
+  {
+      //
+      // parent wait for child
+      // if there is a problem with signal, parent process terminates
+      // when waitTime expires
+      //
+      Uint32 waitTime = MAX_WAIT_TIME;
+
+      while(!handleSigUsr1 && waitTime > 0)
+      {
+        sleep(1);
+        waitTime--;
+      }
+      if( !handleSigUsr1 )
+        {
+        MessageLoaderParms parms("src.Service.ServerProcessUnix.CIMSERVER_START_TIMEOUT",
+          "The cimserver command timed out waiting for the CIM server to start.");
+        PEGASUS_STD(cerr) << MessageLoader::getMessage(parms) << PEGASUS_STD(endl);
+      }
+      exit(graveError);
+  }
+  
+  setsid();
+  umask(S_IRWXG | S_IRWXO );
+
+  // spawned daemon process doesn't need the old signal handlers of its parent
+  getSigHandle()->deactivate(PEGASUS_SIGUSR1);
+  getSigHandle()->deactivate(SIGTERM);
+
+  return(0);
+
+#endif /* !defined(PEGASUS_ENABLE_PRIVILEGE_SEPARATION) */
 }
 
 
@@ -158,22 +160,11 @@ int ServerProcess::cimserver_fork()
 // is ready to serve CIM requests.
 void ServerProcess::notify_parent(int id)
 {
-    pid_t ppid = getppid();
-    if (id)
-    {
-        kill(ppid, SIGTERM);
-    }
-    else
-    {
-        if (Executor::detectExecutor() == 0)
-        {
-            Executor::daemonizeExecutor();
-        }
-        else
-        {
-            kill(ppid, PEGASUS_SIGUSR1);
-        }
-    }
+  pid_t ppid = getppid();
+  if (id)
+   kill(ppid, SIGTERM);
+  else
+   kill(ppid, PEGASUS_SIGUSR1); 
 }
 
 

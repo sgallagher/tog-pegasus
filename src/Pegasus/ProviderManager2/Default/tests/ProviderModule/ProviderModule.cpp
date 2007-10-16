2b858348c305e42f18ddd6ead84c264fda10ced5
carson.hovey <carson.hovey>
2007-10-16 20:04:14 +0000

    BUG#: 7026 TITLE: OpenVMS: BuildLibraryFilename should append .exe to filename.
    DESCRIPTION: This is the 2.6.2 patch for bug 6927.

--- src/Pegasus/ProviderManager2/Default/tests/ProviderModule/ProviderModule.cpp
+++ src/Pegasus/ProviderManager2/Default/tests/ProviderModule/ProviderModule.cpp
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -49,9 +51,9 @@ static const String NONEXISTENT_PROVIDER_LIBRARY_NAME = "BogusLibrary";
 // Uses the DynLib library from Common/tests
 static const String MISSING_ENTRY_POINT_PROVIDER_LIBRARY_NAME = "TestDynLib";
 
-static Boolean verbose;
+const char* verbose = 0;
 
-String getLibraryFileName(const String& libraryName) {
+String getLibraryFileName(const String& libraryName) { 
 #if defined(PEGASUS_OS_VMS)
     String prefixDir;
 # if defined(PEGASUS_USE_RELEASE_DIRS)
@@ -64,7 +66,7 @@ String getLibraryFileName(const String& libraryName) {
     }
 # endif
     return prefixDir +
-           FileSystem::buildLibraryFileName(libraryName);
+           FileSystem::buildLibraryFileName(libraryName); 
 # else
     return FileSystem::buildLibraryFileName(libraryName);
 #endif
@@ -78,12 +80,11 @@ void testGoodProvider()
         getLibraryFileName(GOOD_PROVIDER_LIBRARY_NAME));
 
     CIMProvider* provider = module.load(GOOD_PROVIDER_NAME);
-    PEGASUS_TEST_ASSERT(provider != 0);
-    provider->terminate();
+    PEGASUS_ASSERT(provider != 0);
     module.unloadModule();
 
     provider = module.load(GOOD_PROVIDER_NAME);
-    PEGASUS_TEST_ASSERT(provider != 0);
+    PEGASUS_ASSERT(provider != 0);
 
     // Load incorrect provider name while another provider is loaded
     {
@@ -106,13 +107,10 @@ void testGoodProvider()
     }
 
     // Load the same provider a second time
-    CIMProvider* provider1 = module.load(GOOD_PROVIDER_NAME);
-    PEGASUS_TEST_ASSERT(provider1 != 0);
-
-    provider->terminate();
+    provider = module.load(GOOD_PROVIDER_NAME);
+    PEGASUS_ASSERT(provider != 0);
     module.unloadModule();
 
-    provider1->terminate();
     module.unloadModule();
 
     // Load incorrect provider name while no other provider is loaded
@@ -182,9 +180,9 @@ void testMissingProviderEntryPoint()
     PEGASUS_TEST_ASSERT(caughtException);
 }
 
-int main(int, char** argv)
+int main(int argc, char** argv)
 {
-    verbose = getenv ("PEGASUS_TEST_VERBOSE") ? true : false;
+    verbose = getenv ("PEGASUS_TEST_VERBOSE");
 
     try
     {
---
+++ src/Pegasus/ProviderManagerService/tests/ProviderManagerModule/ProviderManagerModule.cpp
@@ -0,0 +1,162 @@
+//%2006////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//==============================================================================
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#include <Pegasus/Common/Config.h>
+#include <Pegasus/Common/ArrayInternal.h>
+#include <Pegasus/Common/FileSystem.h>
+
+#include <Pegasus/ProviderManagerService/ProviderManagerModule.h>
+
+#include <iostream>
+
+PEGASUS_USING_PEGASUS;
+PEGASUS_USING_STD;
+
+static String fileName;
+
+void test1()
+{
+    ProviderManagerModule module(fileName);
+
+    module.load();
+
+    if (!module.isLoaded())
+    {
+        cout << "failed to load " << module.getFileName() << endl;
+
+        throw 0;
+    }
+
+    ProviderManager * p = module.getProviderManager("TEST");
+
+    if (p == 0)
+    {
+        module.unload();
+
+        cout << "failed to get provider manager from " <<
+            module.getFileName() << endl;
+
+        throw 0;
+    }
+
+    delete p;
+
+    module.unload();
+}
+
+void test2()
+{
+    ProviderManagerModule module(fileName);
+
+    module.load();
+
+    {
+        ProviderManagerModule module2(module);
+
+        if (module2.isLoaded() != module.isLoaded())
+        {
+            cout << "failed to preserve module state in copy " <<
+                module.getFileName() << endl;
+        }
+
+        module2.unload();
+    }
+
+    module.unload();
+}
+
+// array behavior experiment
+void test3()
+{
+    Array<ProviderManagerModule> modules;
+
+    for (Uint32 i = 0, n = 3; i < n; i++)
+    {
+        cout << "creating ProviderManagerModule object for " << fileName <<
+            endl;
+
+        ProviderManagerModule module(fileName);
+
+        modules.append(module);
+    }
+
+    for (Uint32 i = 0, n = modules.size(); i < n; i++)
+    {
+        cout << "loading ProviderManagerModule object for " <<
+            modules[i].getFileName() << endl;
+
+        modules[i].load();
+    }
+
+    for (Uint32 i = 0, n = modules.size(); i < n; i++)
+    {
+        cout << "unloading ProviderManagerModule object for " <<
+            modules[i].getFileName() << endl;
+
+        modules[i].unload();
+    }
+
+    while (modules.size() != 0)
+    {
+        cout << "removing (destroying) ProviderManagerModule object for " <<
+            modules[0].getFileName() << endl;
+
+        modules.remove(0);
+    }
+}
+
+int main(int argc, char** argv)
+{
+    const char * verbose = getenv("PEGASUS_TEST_VERBOSE");
+
+    const String FILE_NAME =
+        FileSystem::buildLibraryFileName("TestProviderManager");
+
+    // Use "bin" directory for Windows, to be consistent with the default
+    // providerDir value in Config/ProviderDirPropertyOwner.cpp.
+#if defined(PEGASUS_PLATFORM_WIN32_IX86_MSVC)
+    fileName = String(getenv("PEGASUS_HOME")) + String("/bin/") + FILE_NAME;
+#elif defined(PEGASUS_OS_VMS)
+    fileName = String(getenv("PEGASUS_HOME")) + String("/bin/") + FILE_NAME;
+#else
+    fileName = String(getenv("PEGASUS_HOME")) + String("/lib/") + FILE_NAME;
+#endif
+    
+    test1();
+    test2();
+    test3();
+
+    cout << argv[0] << " +++++ passed all tests" << endl;
+
+    return 0;
+}

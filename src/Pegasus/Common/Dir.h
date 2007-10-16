51cea1b2cc95c412b8e197fe62fd82d76a2ba2cc
carson.hovey <carson.hovey>
2007-10-16 17:56:54 +0000

    BUG#: 6434 TITLE: OpenVMS: turn on PEGASUS_HAS_READDIR_R
    DESCRIPTION: Remove ifdef for OpenVMS which disables readdir_r for OpenVMS platforms.

--- src/Pegasus/Common/Dir.h
+++ src/Pegasus/Common/Dir.h
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -38,50 +40,63 @@
 #include <Pegasus/Common/Linkage.h>
 #include <Pegasus/Common/AutoPtr.h>
 
-#if defined(PEGASUS_OS_SOLARIS)
+#if defined(PEGASUS_PLATFORM_SOLARIS_SPARC_GNU)
 # include <sys/param.h>
+# include <dirent.h>
 #endif
-
-#if defined(PEGASUS_OS_TYPE_WINDOWS)
-# include <io.h>
-#else // if defined(PEGASUS_OS_TYPE_UNIX) || defined (PEGASUS_OS_VMS)
+#ifdef PEGASUS_OS_ZOS
 # include <dirent.h>
 #endif
 
 PEGASUS_NAMESPACE_BEGIN
 
 #if defined(PEGASUS_OS_TYPE_WINDOWS)
-
-typedef struct
-{
+# include <io.h>
+ typedef struct
+ {
 # if _MSC_VER < 1300
     long file;
 # else
     intptr_t file;
 # endif
     struct _finddata_t findData;
-} DirRep;
+ } DirRep;
+#elif defined(PEGASUS_OS_TYPE_UNIX) || defined (PEGASUS_OS_VMS)
+# if defined(PEGASUS_OS_SOLARIS)
+#  include <sys/param.h>
+# endif /* if defined(PEGASUS_OS_SOLARIS) */
 
-#else // if defined(PEGASUS_OS_TYPE_UNIX) || defined (PEGASUS_OS_VMS)
+# include <dirent.h>
 
-struct DirRep
-{
+# define PEGASUS_HAS_READDIR_R
+
+ struct DirRep
+ {
     DIR* dir;
+# ifdef PEGASUS_OS_OS400
+    struct dirent_lg* entry;
+# else /* ifdef PEGASUS_OS_OS400 */
     struct dirent* entry;
-
-# ifdef PEGASUS_OS_SOLARIS
+# endif /* ifdef PEGASUS_OS_OS400 */
+# ifdef PEGASUS_HAS_READDIR_R
+#  ifdef PEGASUS_OS_OS400
+     struct dirent_lg buffer;
+#  else /* ifdef PEGASUS_OS_OS400 */
+#   ifdef PEGASUS_OS_SOLARIS
 private:
-    char buf[sizeof(dirent) + MAXNAMELEN];
+        char buf[sizeof(dirent) + MAXNAMELEN];
 public:
-    struct dirent& buffer;
-    inline DirRep()
-        : buffer(*reinterpret_cast<struct dirent *>(buf))
-    { }
-# else /* ifdef PEGASUS_OS_SOLARIS */
+        struct dirent &buffer;
+        inline DirRep()
+                : buffer(*reinterpret_cast<struct dirent *>(buf))
+        { }
+#   else /* ifdef PEGASUS_OS_SOLARIS */
     struct dirent buffer;
-# endif /* ifdef PEGASUS_OS_SOLARIS */
+#   endif /* ifdef PEGASUS_OS_SOLARIS */
+#  endif /* ifdef PEGASUS_OS_OS400 */
+# endif /* ifdef PEGASUS_HAS_READDIR_R */
 };
-#endif
+#endif /* elif defined(PEGASUS_OS_TYPE_UNIX) || defined (PEGASUS_OS_VMS) */
 
 /** The Dir class provides a platform independent way of iterating the
     files in a directory.
@@ -128,6 +143,7 @@ public:
 private:
 
     Boolean _more;
+    Boolean _isValid;
     String _path;
     DirRep _dirRep;
 };

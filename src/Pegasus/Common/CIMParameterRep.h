eb2919b9f36139062e3aebaf0150241822734a2a
kumpf <kumpf>
2007-11-07 20:40:17 +0000

    BUG#: 7092 TITLE: 'type qualifier on return type is meaningless' warnings on HP-UX IPF DESCRIPTION: Remove the meaningless const qualifiers on simple data return types.

--- src/Pegasus/Common/CIMParameterRep.h
+++ src/Pegasus/Common/CIMParameterRep.h
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -34,13 +36,12 @@
 
 #include <Pegasus/Common/Config.h>
 #include <Pegasus/Common/Linkage.h>
-#include <Pegasus/Common/Constants.h>
 #include <Pegasus/Common/InternalException.h>
 #include <Pegasus/Common/String.h>
+#include <Pegasus/Common/Sharable.h>
 #include <Pegasus/Common/CIMName.h>
 #include <Pegasus/Common/CIMQualifier.h>
 #include <Pegasus/Common/CIMQualifierList.h>
-#include <Pegasus/Common/OrderedSet.h>
 
 PEGASUS_NAMESPACE_BEGIN
 
@@ -48,7 +49,7 @@ class DeclContext;
 class CIMConstParameter;
 class CIMParameter;
 
-class CIMParameterRep
+class CIMParameterRep : public Sharable
 {
 public:
 
@@ -59,26 +60,11 @@ public:
         Uint32 arraySize,
         const CIMName& referenceClassName);
 
+    ~CIMParameterRep();
+
     const CIMName& getName() const
     {
         return _name;
-    }
-
-    Uint32 getNameTag() const
-    {
-        return _nameTag;
-    }
-
-    void increaseOwnerCount()
-    {
-        _ownerCount++;
-        return;
-    }
-
-    void decreaseOwnerCount()
-    {
-        _ownerCount--;
-        return;
     }
 
     void setName(const CIMName& name);
@@ -123,11 +109,7 @@ public:
         return _qualifiers.getQualifier(index);
     }
 
-    void removeQualifier (Uint32 index)
-    {
-        _qualifiers.removeQualifier (index);
-    }
-
+    void removeQualifier (Uint32 index);
 
     Uint32 getQualifierCount() const
     {
@@ -136,6 +118,10 @@ public:
 
     void resolve(DeclContext* declContext, const CIMNamespaceName& nameSpace);
 
+    void toXml(Buffer& out) const;
+
+    void toMof(Buffer& out) const;
+
     Boolean identical(const CIMParameterRep* x) const;
 
     CIMParameterRep* clone() const
@@ -143,23 +129,19 @@ public:
         return new CIMParameterRep(*this);
     }
 
-    void Inc()
-    {
-        _refCounter++;
-    }
-
-    void Dec()
-    {
-        if (_refCounter.decAndTestIfZero())
-            delete this;
-    }
-
 private:
+
+    CIMParameterRep();
 
     CIMParameterRep(const CIMParameterRep& x);
 
-    CIMParameterRep();    // Unimplemented
-    CIMParameterRep& operator=(const CIMParameterRep& x);    // Unimplemented
+    // This method is declared and made private so that the compiler does
+    // not implicitly define a default copy constructor.
+    CIMParameterRep& operator=(const CIMParameterRep& x)
+    {
+        //PEGASUS_ASSERT(0);
+        return *this;
+    }
 
     CIMName _name;
     CIMType _type;
@@ -167,14 +149,6 @@ private:
     Uint32 _arraySize;
     CIMName _referenceClassName;
     CIMQualifierList _qualifiers;
-    Uint32 _nameTag;
-
-    // reference counter as member to avoid
-    // virtual function resolution overhead
-    AtomicInt _refCounter;
-    Uint32 _ownerCount;
-
-    friend class CIMBuffer;
 };
 
 PEGASUS_NAMESPACE_END

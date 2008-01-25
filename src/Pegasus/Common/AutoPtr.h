a6f688423e96c3c5abfa71a8ebe504dd3201aa55
marek <marek>
2008-01-25 19:00:45 +0000

    BUG#:7298 TITLE: CMPI provider manager does not compile (CMPI_Broker.h)
    DESCRIPTION:

--- src/Pegasus/Common/AutoPtr.h
+++ src/Pegasus/Common/AutoPtr.h
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -47,72 +49,115 @@ struct DeletePtr
     }
 };
 
-/** Function object for freeing (rather than deleting) pointer to char*. For
-    example:
-
-        <pre>
-        char* ptr = (char*)malloc(80);
-        AutoPtr<char, FreeCharPtr> ap(ptr);
-        </pre>
-*/
-struct FreeCharPtr
-{
-    void operator()(char* ptr)
-    {
-        free(ptr);
-    }
-};
-
-/** Function object for deleting a pointer referring to an array.
-*/
-template<class T>
-struct DeleteArrayPtr
-{
-    void operator()(T* ptr)
-    {
-        delete [] ptr;
-    }
-};
-
 /**
-    This class provides a convenient way of disposing of a heap object.
+    This class provides a convenient way of disposing off a heap object.
     It automatically deletes the enclosed pointer on destruction. For
     example:
 
     <pre>
         A* a = new A;
-        AutoPtr<A> dummy(a);
+        AutoPtr<A> dummy = a;
     </pre>
 
     When the AutoPtr object destructs, it frees the instance of A.
-    This is particularly useful when a function has multiple returns or
-    exception conditions.
+    This is particularly useful when a function has multiple returns.
 
     There are two classes here: AutoPtr<> and AutoArrayPtr<>. The
     AutoArrayPtr<> class is used when a pointer must be deleted using the
-    array form of the delete operator ("delete []").
+    array form as shown below:
+
+    <pre>
+        delete [] ptr;
+    <pre>
 */
 
-template<class X, class D = DeletePtr<X> > class AutoPtr
-{
-public:
+template<class X, class D = DeletePtr<X> > class AutoPtr;
 
+template<class X> class AutoPtrRef {
+public:
+    inline AutoPtrRef(AutoPtr<X> &ref) : _ref(ref)
+    {
+        // This block intentionally left blank
+    }
+
+    inline AutoPtr<X> get()
+    {
+        return _ref;
+    }
+
+private:
+    AutoPtr<X> &_ref;
+};
+
+
+template<class X> class AutoArrayPtr;
+
+template<class X> class AutoArrayPtrRef {
+public:
+    inline AutoArrayPtrRef(AutoArrayPtr<X> &ref) : _ref(ref)
+    {
+       // This block intentionally left blank
+    }
+
+    inline AutoArrayPtr<X> get()
+    {
+        return _ref;
+    }
+
+private:
+    AutoArrayPtr<X> &_ref;
+};
+
+
+
+template<class X, class D > class AutoPtr {
+public:
     // This constructor helps this AutoPtr to take ownership of the memory
     // object pointed by p. It also acts as a default constructor (if no
     // argument is passed, it assigns a value of "0" to _ptr.
     // Example:  AutoPtr<SSLContext> sslContextA(new SSLContext());
     //           AutoPtr<SSLContext> sslContextB;
     //   sslContextB here has _ptr set to "0".
-    explicit AutoPtr(X* p = 0) throw()
+    inline explicit AutoPtr(X* p = 0) throw()
         : _ptr(p)
     {
+        // This block intentionally left blank
     }
 
     // Destructor makes sure to delete the object pointed by _ptr thus
     // avoiding memory leaks
-    ~AutoPtr() throw()
+    inline ~AutoPtr() throw()
     {
         d(_ptr);
+    }
+
+    // The copy constructor takes the ownership of the heap object from
+    // the source AutoPtr object. And since there should be only one
+    // legitimate owner, it sets the _ptr of the source to "0"
+    inline AutoPtr(AutoPtrRef<X> &a) throw()
+        : _ptr((a.get()).release())
+    {
+        //a._ptr = 0;
+    }
+
+    inline AutoPtr(AutoPtr<X> &a) throw() : _ptr(a.release())
+    {
+    }
+
+    // Overloading of "=" operator makes sure that the ownership of the memory
+    // gets transferred properly to 'this' AutoPtr object.
+    // Example:   AutoPtr<HTTPConnection> httpConnectionB = httpConnectionA;
+    AutoPtr<X> &operator=(AutoPtr<X>& a) throw()
+    {
+        if ( this != &a )
+            reset(a.release());
+        return *this;
+    }
+
+    inline AutoPtr<X> &operator=(AutoPtrRef<X> &a) throw()
+    {
+        reset((a.get()).release());
+        return *this;
     }
 
     // This method can be used to get the pointer to heap object encapsulated
@@ -121,13 +166,13 @@ public:
     //            func1(objA.get(), NULL);
     // Here func1() is a function which takes first argument as pointer to the
     // object of classA.
-    inline X* get() const throw()
+    inline X *get() const throw()
     {
         return _ptr;
     }
 
     // Returns the heap object itself (not the pointer to it)
-    inline X& operator*() const throw()
+    inline X &operator*() const throw()
     {
         return *_ptr;
     }
@@ -140,7 +185,7 @@ public:
     // funcA() is a function in the classA. Although objA is an AutoPtr, still
     // "->" operator would result in calling _ptr->funcA() because of this
     // overloading only.
-    inline X* operator->() const throw()
+    inline X *operator->() const throw()
     {
         return _ptr;
     }
@@ -149,13 +194,13 @@ public:
     // Return the pointer to the heap object and set _ptr to "0".
     inline X* release() throw()
     {
-        X* t = _ptr;
+        X *t = _ptr;
         _ptr = 0;
         return t;
     }
 
     // Delete the heap object and thus release ownership
-    inline void reset(X* p = 0) throw()
+    inline void reset(X *p = 0) throw()
     {
         if (p != _ptr)
         {
@@ -164,41 +209,120 @@ public:
         }
     }
 
-private:
-    AutoPtr(const AutoPtr<X>&);
-    AutoPtr<X>& operator=(const AutoPtr<X>&);
+    AutoPtr(AutoPtrRef<X> obj) throw()
+        : _ptr((obj.get()).get())
+    {
+        // This block intentionally left blank
+    }
 
+    template<class Y> operator AutoPtrRef<Y>() throw()
+    {
+        return AutoPtrRef<Y>(*this);
+    }
+
+    template<class Y> operator AutoPtr<Y>() throw()
+    {
+        return AutoPtr<Y>(release());
+    }
+
+private:
     // An object that knows how to delete the dynamic memory correctly
     D d;
 
     // A pointer to the heap object
+     X* _ptr;
+};
+
+
+template<class X> class AutoArrayPtr {
+public:
+    inline explicit AutoArrayPtr(X* p = 0) throw()
+        : _ptr(p)
+    {
+        // This block intentionally left blank
+    }
+
+    inline ~AutoArrayPtr() throw()
+    {
+        delete [] _ptr;
+    }
+
+    inline AutoArrayPtr(AutoArrayPtrRef<X> &a) throw()
+        : _ptr((a.get()).release())
+    {
+        //a._ptr = 0;
+    }
+
+    inline AutoArrayPtr(AutoArrayPtr<X> &a) throw() : _ptr(a.release())
+    {
+    }
+
+    AutoArrayPtr<X> &operator=(AutoArrayPtr<X>& a) throw()
+    {
+        reset(a.release());
+        return *this;
+    }
+
+    inline AutoArrayPtr<X> &operator=(AutoArrayPtrRef<X> &a) throw()
+    {
+        if (this != a)
+            reset((a.get()).release());
+        return *this;
+    }
+
+    inline X *get() const throw()
+    {
+        return _ptr;
+    }
+
+    inline X &operator*() const throw()
+    {
+        return *_ptr;
+    }
+
+    inline X *operator->() const throw()
+    {
+        return _ptr;
+    }
+
+    inline X* release() throw()
+    {
+        X *t = _ptr;
+        _ptr = 0;
+        return t;
+    }
+
+    inline void reset(X *p = 0) throw()
+    {
+        if (p != _ptr)
+        {
+            // use "[]" since _ptr is pointer to an array
+            delete [] _ptr;
+            _ptr = p;
+        }
+    }
+
+    AutoArrayPtr(AutoArrayPtrRef<X> obj) throw()
+        : _ptr((obj.get()).get())
+    {
+        // This block intentionally left blank
+    }
+
+    template<class Y> operator AutoArrayPtrRef<Y>() throw()
+    {
+        return AutoArrayPtrRef<Y>(*this);
+    }
+
+    template<class Y> operator AutoArrayPtr<Y>() throw()
+    {
+        return AutoArrayPtr<Y>(release());
+    }
+
+
+private:
     X* _ptr;
 };
 
-
-template<class X> class AutoArrayPtr : public AutoPtr<X, DeleteArrayPtr<X> >
-{
-public:
-
-    explicit AutoArrayPtr(X* p = 0) throw()
-        : AutoPtr<X, DeleteArrayPtr<X> >(p)
-    {
-    }
-
-    X& operator[](Uint32 index) throw()
-    {
-        return this->get()[index];
-    }
-
-    const X& operator[](Uint32 index) const throw()
-    {
-        return this->get()[index];
-    }
-
-private:
-    AutoArrayPtr(const AutoArrayPtr<X>&);
-    AutoArrayPtr<X>& operator=(const AutoArrayPtr<X>&);
-};
 
 PEGASUS_NAMESPACE_END
 

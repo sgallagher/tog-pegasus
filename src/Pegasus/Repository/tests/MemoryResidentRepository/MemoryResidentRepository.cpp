831593db258abd1ec642413429f7f1790df78816
mike <mike>
2008-04-25 00:57:50 +0000

    PEP#: 305 TITLE: VxWorks Port
    DESCRIPTION: Ongoing VxWorks porting

---
+++ src/Pegasus/Repository/tests/MemoryResidentRepository/MemoryResidentRepository.cpp
@@ -0,0 +1,39 @@
+#include <Pegasus/Repository/CIMRepository.h>
+#include <Pegasus/Repository/MemoryResidentRepository.h>
+#include "root_cimv2_namespace.h"
+
+PEGASUS_USING_PEGASUS;
+
+int main()
+{
+    MemoryResidentRepository::addNameSpace(&root_cimv2_namespace);
+
+    CIMRepository cr("/", CIMRepository::MODE_DEFAULT);
+
+    Array<CIMName> names;
+    names.append("CreationClassName");
+    CIMPropertyList pl(names);
+
+    for (Uint32 i = 0; i < 1000; i++)
+    {
+        try
+        {
+            CIMClass cc = cr.getClass(
+                "root/cimv2", 
+                "CIM_ComputerSystem",
+                false,
+                true,
+                true,
+                pl);
+        }
+        catch (...)
+        {
+            fprintf(stderr, "ERROR\n");
+            exit(1);
+        }
+    }
+
+    printf("+++++ passed all tests\n");
+
+    return 0;
+}

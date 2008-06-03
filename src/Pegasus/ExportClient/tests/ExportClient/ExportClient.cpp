ba8561cb574839979c68d2197c0b39850e796bbf
mike <mike>
2008-06-03 19:31:24 +0000

    PEP#: 328 TITLE: SOLARIS_IX86_CC Port
    DESCRIPTION: SOLARIS_IX86_CC Port

--- src/Pegasus/ExportClient/tests/ExportClient/ExportClient.cpp
+++ src/Pegasus/ExportClient/tests/ExportClient/ExportClient.cpp
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -101,15 +103,15 @@ static void TestTimeout()
     Monitor monitor;
     HTTPConnector httpConnector(&monitor);
     CIMExportClient client(&monitor, &httpConnector);
-    const int TIMEOUT_MILLISECONDS =
+    const int TIMEOUT_MILLISECONDS = 
         PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS * 2;
 
-    PEGASUS_TEST_ASSERT(client.getTimeout() ==
+    PEGASUS_TEST_ASSERT(client.getTimeout() == 
                         PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS);
     client.setTimeout(TIMEOUT_MILLISECONDS);
     PEGASUS_TEST_ASSERT(client.getTimeout() == Uint32(TIMEOUT_MILLISECONDS));
     client.setTimeout(PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS);
-    PEGASUS_TEST_ASSERT(client.getTimeout() ==
+    PEGASUS_TEST_ASSERT(client.getTimeout() == 
                         PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS);
 
     //
@@ -118,12 +120,12 @@ static void TestTimeout()
     Uint32 port =
         System::lookupPort(WBEM_HTTP_SERVICE_NAME, WBEM_DEFAULT_HTTP_PORT);
     client.connect("localhost", port);
-    PEGASUS_TEST_ASSERT(client.getTimeout() ==
+    PEGASUS_TEST_ASSERT(client.getTimeout() == 
                         PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS);
     client.setTimeout(TIMEOUT_MILLISECONDS);
     PEGASUS_TEST_ASSERT(client.getTimeout() == Uint32(TIMEOUT_MILLISECONDS));
     client.setTimeout(PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS);
-    PEGASUS_TEST_ASSERT(client.getTimeout() ==
+    PEGASUS_TEST_ASSERT(client.getTimeout() == 
                         PEGASUS_DEFAULT_CLIENT_TIMEOUT_MILLISECONDS);
 }
 
@@ -243,7 +245,11 @@ static void testSSLConnect()
     {
         client.connect("nonexistant-zxcvqw3r", port, *sslContext);
     }
+#if defined(PEGASUS_PLATFORM_SOLARIS_IX86_CC)
+    catch(...)
+#else
     catch(const InvalidLocatorException&)
+#endif
     {
         invalidLocatorCaught = true;
     }

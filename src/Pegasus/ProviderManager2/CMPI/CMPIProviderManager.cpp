9407f26bd0a9ffc65e5d2e690c8bb93be08afb8d
venkat.puvvada <venkat.puvvada>
2008-07-17 11:57:00 +0000

    BUG#: 7805 TITLE: cimserver hangs/crashes when CMPI provider with pending requests is unloaded DESCRIPTION: Give grace time before terminating the unload pending providers.

--- src/Pegasus/ProviderManager2/CMPI/CMPIProviderManager.cpp
+++ src/Pegasus/ProviderManager2/CMPI/CMPIProviderManager.cpp
@@ -2731,12 +2731,11 @@ Message * CMPIProviderManager::handleDisableModuleRequest(
 
     PEGASUS_ASSERT(request != 0);
 
+    //Set to false when provider refused to unload due to pending operations.
+    Boolean disableModuleOk = true;
+
     // get provider module name
     Boolean disableProviderOnly = request->disableProviderOnly;
-
-    Array<Uint16> operationalStatus;
-    // Assume success.
-    operationalStatus.append(CIM_MSE_OPSTATUS_VALUE_STOPPED);
 
     //
     // Unload providers
@@ -2761,7 +2760,18 @@ Message * CMPIProviderManager::handleDisableModuleRequest(
         if (!providerManager.isProviderActive(providerName))
         {
             continue;
-        }  
+        }
+ 
+        Boolean unloadOk = providerManager.unloadProvider(
+            physicalName, 
+            _pInstances[i].getProperty(_pInstances[i].findProperty("Name")).
+                getValue ().toString ());
+
+        if (!unloadOk)
+        {
+            disableModuleOk = false;
+            continue;
+        }
         //
         //  Reset the indication provider's count of current
         //  subscriptions since it has been disabled
@@ -2785,11 +2795,17 @@ Message * CMPIProviderManager::handleDisableModuleRequest(
                 }
             }
         }
-        providerManager.unloadProvider(
-            physicalName, 
-            _pInstances[i].getProperty(
-            _pInstances[i].findProperty("Name")
-            ).getValue ().toString ());
+    }
+
+    Array<Uint16> operationalStatus;
+
+    if (disableModuleOk)
+    {
+        operationalStatus.append(CIM_MSE_OPSTATUS_VALUE_STOPPED);
+    }
+    else
+    {
+        operationalStatus.append(CIM_MSE_OPSTATUS_VALUE_OK);
     }
 
     CIMDisableModuleResponseMessage * response =

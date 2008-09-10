ac8ce5e4fc467b79a216c1cf3c6a64090a4a1891
kumpf <kumpf>
2008-09-10 22:47:00 +0000

    BUG#: 7897 TITLE: wbemexec/HttpConstants.h defines data DESCRIPTION: Change static variable definitions to macro definitions.

--- src/Clients/wbemexec/HttpConstants.h
+++ src/Clients/wbemexec/HttpConstants.h
@@ -37,14 +37,10 @@
 
 PEGASUS_NAMESPACE_BEGIN
 
-// REVIEW: Using static declarations will cause multiple declarations of these
-// REVIEW: strings. Either use #define (best) or use PEGASUS_COMMON_LINKAGE and
-// REVIEW: eliminate use of statics.
-
 //
-//  This include file contains constants related to the HTTP encapsulation 
+//  This include file contains constants related to the HTTP encapsulation
 //  of an XML-encoded CIM operation, including HTTP request elements, HTTP
-//  header names and values, and special characters used in headers and 
+//  header names and values, and special characters used in headers and
 //  CIMObject values.
 //
 
@@ -52,58 +48,55 @@ PEGASUS_NAMESPACE_BEGIN
 //  HTTP Request Line elements
 //
 
-// ATTN: note: this use of static will cause the string to be created in
-// every compiliation unit which includes it.
-
 /**
  *  A string representing the HTTP M-POST method.
  */
-static const char HTTP_METHOD_MPOST [] = "M-POST";
+#define HTTP_METHOD_MPOST "M-POST"
 
 /**
  *  A string representing the HTTP POST method.
  */
-static const char HTTP_METHOD_POST [] = "POST";
+#define HTTP_METHOD_POST "POST"
 
 /**
  *  A string representing a bogus HTTP method, to allow testing of CIM server
  *  handling of unknown HTTP methods.
  */
-static const char HTTP_METHOD_BOGUS [] = "BOGUS";
+#define HTTP_METHOD_BOGUS "BOGUS"
 
 /**
  *  A string representing the request URI cimom.
  */
-static const char HTTP_REQUEST_URI_CIMOM [] = "/cimom";
+#define HTTP_REQUEST_URI_CIMOM "/cimom"
 
 /**
  *  A string representing the protocol HTTP
  */
-static const char HTTP_PROTOCOL [] = "HTTP/";
+#define HTTP_PROTOCOL "HTTP/"
 
 /**
  *  A string representing the HTTP/1.0 version.
  */
-static const char HTTP_VERSION_10 [] = "1.0";
+#define HTTP_VERSION_10 "1.0"
 
 /**
  *  A string representing the HTTP/1.1 version.
  */
-static const char HTTP_VERSION_11 [] = "1.1";
+#define HTTP_VERSION_11 "1.1"
 
 /**
  *  A string representing basic authentication
  */
-static const char HTTP_AUTH_BASIC [] = "Basic";
+#define HTTP_AUTH_BASIC "Basic"
 /**
  *  A string representing the required CRLF sequence.
  */
-static const char HTTP_CRLF [] = "\r\n";
+#define HTTP_CRLF "\r\n"
 
 /**
  *  A character representing the required SP separator.
  */
-static const char HTTP_SP [] = " ";
+#define HTTP_SP " "
 
 
 //
@@ -113,93 +106,89 @@ static const char HTTP_SP [] = " ";
 /**
  *  The name of the HTTP header specifying the authentication type and value
  */
-static const char HEADER_NAME_AUTHORIZATION [] =
-                                    "Authorization";
+#define HEADER_NAME_AUTHORIZATION "Authorization"
 /**
  *  The name of the HTTP header specifying the Internet host of the resource
  *  being requested.
  */
-static const char HEADER_NAME_HOST [] = "Host";
+#define HEADER_NAME_HOST "Host"
 
 /**
  *  The name of the HTTP header specifying the media type of the underlying
  *  data.
  */
-static const char HEADER_NAME_CONTENTTYPE [] = "Content-Type";
+#define HEADER_NAME_CONTENTTYPE "Content-Type"
 
 /**
  *  The name of the HTTP header specifying the size of the entity-body.
  */
-static const char HEADER_NAME_CONTENTLENGTH [] = "Content-Length";
+#define HEADER_NAME_CONTENTLENGTH "Content-Length"
 
 /**
- *  The name of the HTTP header specifying a mandatory extension 
+ *  The name of the HTTP header specifying a mandatory extension
  *  declaration.
  */
-static const char HEADER_NAME_MAN [] = "Man";
+#define HEADER_NAME_MAN "Man"
 
 /**
  *  The name of the HTTP extension header identifying the version of the
  *  CIM mapping onto HTTP being used by the sending entity.
  */
-static const char HEADER_NAME_CIMPROTOCOLVERSION [] = "CIMProtocolVersion";
+#define HEADER_NAME_CIMPROTOCOLVERSION "CIMProtocolVersion"
 
 /**
  *  The name of the HTTP extension header identifying the message as a CIM
  *  operation request or response.
  */
-static const char HEADER_NAME_CIMOPERATION [] = "CIMOperation";
+#define HEADER_NAME_CIMOPERATION "CIMOperation"
 
 /**
  *  The name of the HTTP extension header identifying the name of the CIM
  *  method to be invoked.
  */
-static const char HEADER_NAME_CIMMETHOD [] = "CIMMethod";
+#define HEADER_NAME_CIMMETHOD "CIMMethod"
 
 /**
  *  The name of the HTTP extension header identifying the name of the CIM
  *  object on which the method is to be invoked.
  */
-static const char HEADER_NAME_CIMOBJECT [] = "CIMObject";
+#define HEADER_NAME_CIMOBJECT "CIMObject"
 
 /**
  *  The name of the HTTP extension header identifying the encapsulated
  *  Operation Request Message as containing multiple method invocations.
  */
-static const char HEADER_NAME_CIMBATCH [] = "CIMBatch";
+#define HEADER_NAME_CIMBATCH "CIMBatch"
 
 //
 //  Header values
 //
 
 /**
- *  The value of the HTTP Content-type header specifying that the media 
+ *  The value of the HTTP Content-type header specifying that the media
  *  type of the underlying data is application data in XML format.
  */
-static const char HEADER_VALUE_CONTENTTYPE [] = 
-                           "application/xml; charset=\"utf-8\"";
+#define HEADER_VALUE_CONTENTTYPE "application/xml; charset=\"utf-8\""
 
 #ifdef PEGASUS_ENABLE_PROTOCOL_WSMAN
 /**
  *  The value of the WS-Management HTTP Content-type header specifying that the
  *  media type of the underlying data is application data in soap+xml format.
  */
-static const char WSMAN_HEADER_VALUE_CONTENTTYPE[] =
-    "application/soap+xml; charset=utf-8";
+# define WSMAN_HEADER_VALUE_CONTENTTYPE "application/soap+xml; charset=utf-8"
 #endif
 
 /**
  *  The value of the HTTP Man header specifying the CIM mapping onto HTTP
  *  extension header name space URI.
  */
-static const char HEADER_VALUE_MAN [] = 
-                           "http://www.dmtf.org/cim/mapping/http/v1.0;ns=";
+#define HEADER_VALUE_MAN "http://www.dmtf.org/cim/mapping/http/v1.0;ns="
 
 /**
  *  The value of the HTTP CIMOperation header identifying the message as
  *  carrying a CIM operation request.
  */
-static const char HEADER_VALUE_CIMOPERATION [] = "MethodCall";
+#define HEADER_VALUE_CIMOPERATION "MethodCall"
 
 
 //
@@ -209,12 +198,12 @@ static const char HEADER_VALUE_CIMOPERATION [] = "MethodCall";
 /**
  *  The character used to separate the header name and header value.
  */
-static const char HEADER_SEPARATOR = ':';
+#define HEADER_SEPARATOR ':'
 
 /**
  *  The character used to delimit the header prefix and extension header.
  */
-static const char HEADER_PREFIX_DELIMITER = '-';
+#define HEADER_PREFIX_DELIMITER '-'
 
 
 PEGASUS_NAMESPACE_END

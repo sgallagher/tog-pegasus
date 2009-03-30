18b13164be3fbf2673021e6bddd100c6567cc3dd
venkat.puvvada <venkat.puvvada>
2009-03-30 14:04:28 +0000

    BUG#: 8466 TITLE: TestOOPModuleFailure fails DESCRIPTION:

--- src/Pegasus/ProviderManagerService/ProviderAgent/ProviderAgent.cpp
+++ src/Pegasus/ProviderManagerService/ProviderAgent/ProviderAgent.cpp
@@ -110,6 +110,7 @@ ProviderAgent::ProviderAgent(
     _providerAgent = this;
     _subscriptionInitComplete = false;
     _isInitialised = false;
+    _providersStopped = false;
 
     PEG_METHOD_EXIT();
 }
@@ -179,21 +180,25 @@ void ProviderAgent::run()
 
         if (_terminating)
         {
-            //
-            // Stop all providers
-            //
-            CIMStopAllProvidersRequestMessage stopRequest("0", QueueIdStack(0));
-            AutoPtr<Message> stopResponse(_processRequest(&stopRequest));
-
-            // If there are agent threads running exit from here. If provider
-            // is not responding cimprovagt may loop forever in ThreadPool
-            // destructor waiting for running threads to become idle.
-            if (_threadPool.runningCount())
+            if (!_providersStopped)
             {
-                PEG_TRACE_CSTRING(TRC_PROVIDERAGENT,
-                    Tracer::LEVEL1,
-                    "Agent threads are running, terminating forcibly.");
-                exit(1);
+                //
+                // Stop all providers
+                //
+                CIMStopAllProvidersRequestMessage 
+                    stopRequest("0", QueueIdStack(0));
+                AutoPtr<Message> stopResponse(_processRequest(&stopRequest));
+
+                // If there are agent threads running exit from here.If provider
+                // is not responding cimprovagt may loop forever in ThreadPool
+                // destructor waiting for running threads to become idle.
+                if (_threadPool.runningCount())
+                { 
+                    PEG_TRACE_CSTRING(TRC_PROVIDERAGENT,
+                        Tracer::LEVEL1,
+                        "Agent threads are running, terminating forcibly.");
+                    exit(1);
+                }
             }
         }
         else if (!active)
@@ -483,6 +488,7 @@ Boolean ProviderAgent::_readAndProcessRequest()
              (respMsg->cimException.getCode() == CIM_ERR_SUCCESS)))
         {
             // Operation is successful. End the agent process.
+            _providersStopped = true;
             _terminating = true;
         }
 
--- src/Pegasus/ProviderManagerService/ProviderAgent/ProviderAgent.h
+++ src/Pegasus/ProviderManagerService/ProviderAgent/ProviderAgent.h
@@ -1,31 +1,33 @@
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//==============================================================================
 //
 //%/////////////////////////////////////////////////////////////////////////////
 
@@ -33,13 +35,11 @@
 #define Pegasus_ProviderAgent_h
 
 #include <Pegasus/Common/Config.h>
-#include <Pegasus/Common/Semaphore.h>
 #include <Pegasus/Common/AnonymousPipe.h>
 #include <Pegasus/Common/Thread.h>
 #include <Pegasus/Common/ThreadPool.h>
 #include <Pegasus/Common/Signal.h>
-#include <Pegasus/Common/SCMOClassCache.h>
-#include <Pegasus/ProviderManagerRouter/BasicProviderManagerRouter.h>
+#include <Pegasus/ProviderManagerService/BasicProviderManagerRouter.h>
 
 PEGASUS_NAMESPACE_BEGIN
 
@@ -126,21 +126,13 @@ private:
     static void _indicationCallback(
         CIMProcessIndicationRequestMessage* request);
 
-    static void _handleIndicationDeliveryResponse(
-        CIMProcessIndicationResponseMessage *response);
-
     /**
         Callback function to which all response chunks are sent for processing.
      */
     static void _responseChunkCallback(
         CIMRequestMessage* request, CIMResponseMessage* response);
 
-    /*
-     *Tries to unload idle providers giving a timeout of value shutdown timeout
-     * return true if unloaded successfuly
-     */
-    Boolean _unloadIdleProviders();
-
+    void _unloadIdleProviders();
     static ThreadReturnType PEGASUS_THREAD_CDECL
         _unloadIdleProvidersHandler(void* arg) throw();
 
@@ -212,34 +204,17 @@ private:
     CIMInstance _providerModuleCache;
 
     /**
+        Indicates whether the Indication Service has completed initialization.
+
+        For more information, please see the description of the
+        ProviderManagerRouter::_subscriptionInitComplete member variable.
+     */
+    Boolean _subscriptionInitComplete;
+
+    /**
         A thread pool used for asynchronous processing of provider operations.
      */
     ThreadPool _threadPool;
-
-
-    /**
-     * Call back for the SCMOClassCache.
-     */
-    static SCMOClass _scmoClassCache_GetClass(
-        const CIMNamespaceName& nameSpace,
-        const CIMName& className);
-
-    void _processGetSCMOClassResponse(
-        ProvAgtGetScmoClassResponseMessage* response);
-
-    void _processStopAllProvidersRequest(CIMRequestMessage* request);
-
-    static void _indicationDeliveryRoutine(
-        CIMProcessIndicationRequestMessage* message);
-
-    /**
-     * Condition variable and transger pointer for the provider agend to
-     * the SCMOClassCache.
-     **/
-    static Semaphore  _scmoClassDelivered;
-    static SCMOClass* _transferSCMOClass;
-    static Mutex       _transferSCMOClassMutex;
-    static String      _transferSCMOClassRspMsgID;
 
     /**
        Indicates if the provider agent has been successful initialised already.
@@ -247,7 +222,7 @@ private:
     Boolean _isInitialised;
 
     /**
-        Indicates whether the provider module is disabled successfully or
+        Indicates whether the provider module is disabled successfully or 
         CIMStopAllProvidersRequestMessage is processed successfully.
     */
     Boolean _providersStopped;

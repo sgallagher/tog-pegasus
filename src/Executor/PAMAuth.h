f3deb187141f26a840f2762659bca3cb567856e1
venkat.puvvada <venkat.puvvada>
2009-06-08 11:28:32 +0000

    BUG#: 8530 TITLE: cleanup CimserveraProcessOperation() method DESCRIPTION:

--- src/Executor/PAMAuth.h
+++ src/Executor/PAMAuth.h
@@ -1,32 +1,36 @@
 /*
-//%LICENSE////////////////////////////////////////////////////////////////
+//%2006////////////////////////////////////////////////////////////////////////
 //
-// Licensed to The Open Group (TOG) under one or more contributor license
-// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
-// this work for additional information regarding copyright ownership.
-// Each contributor licenses this file to you under the OpenPegasus Open
-// Source License; you may not use this file except in compliance with the
-// License.
+// Copyright (c) 2000, 2001, 2002 BMC Software; Hewlett-Packard Development
+// Company, L.P.; IBM Corp.; The Open Group; Tivoli Systems.
+// Copyright (c) 2003 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation, The Open Group.
+// Copyright (c) 2004 BMC Software; Hewlett-Packard Development Company, L.P.;
+// IBM Corp.; EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2005 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; VERITAS Software Corporation; The Open Group.
+// Copyright (c) 2006 Hewlett-Packard Development Company, L.P.; IBM Corp.;
+// EMC Corporation; Symantec Corporation; The Open Group.
 //
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// THE ABOVE COPYRIGHT NOTICE AND THIS PERMISSION NOTICE SHALL BE INCLUDED IN
+// ALL COPIES OR SUBSTANTIAL PORTIONS OF THE SOFTWARE. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-// The above copyright notice and this permission notice shall be included
-// in all copies or substantial portions of the Software.
+//==============================================================================
 //
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
-//////////////////////////////////////////////////////////////////////////
+//%/////////////////////////////////////////////////////////////////////////////
 */
 
 #ifndef Executor_PAMAuth_h
@@ -48,12 +52,6 @@
 #include <security/pam_appl.h>
 #include <Executor/Defines.h>
 #include <Executor/Socket.h>
-
-#ifdef PEGASUS_FLAVOR
-# define PAM_CONFIG_FILE "wbem" PEGASUS_FLAVOR
-#else
-# define PAM_CONFIG_FILE "wbem"
-#endif
 
 /*
 **==============================================================================
@@ -195,9 +193,7 @@ CimserveraRequest;
 
 typedef struct CimserveraResponseStruct
 {
-    /* '-1' means authentication failed / something went wrong in the 
-            communication
-      '>=0'  means PAM return code. */
+    /* '0' means authentication successful. '-1' means authentication failed. */
     int status;
 }
 CimserveraResponse;
@@ -256,9 +252,12 @@ static int CimserveraProcessOperation(
             break;
         }
 
-        /* Store status. */
+        /* Check status. */
 
-        status = response.status;
+        if (response.status != 0)
+        {
+            status = -1;
+        }
     }
     while (0);
 
@@ -402,34 +401,30 @@ static int PAMAuthenticateInProcess(
     PAMData data;
     struct pam_conv pconv;
     pam_handle_t* handle;
-    int pam_rc;
 
     data.password = password;
     pconv.conv = PAMAuthenticateCallback;
     pconv.appdata_ptr = &data;
 
-    /* commented out statement in place to allow triggering a Http 500 Error */
-    /* intentionally for testing purposes */
-    /* return PAM_SERVICE_ERR; */
 
-    pam_rc = pam_start(PAM_CONFIG_FILE, username, &pconv, &handle);
+    if (pam_start("wbem", username, &pconv, &handle) != PAM_SUCCESS)
+        return -1;
 
-    if (pam_rc != PAM_SUCCESS)
-    {
-        return pam_rc;
-    }
-
-    pam_rc = pam_authenticate(handle, 0);
-    if (pam_rc != PAM_SUCCESS)
+    if (pam_authenticate(handle, 0) != PAM_SUCCESS)
     {
         pam_end(handle, 0);
-        return pam_rc;
+        return -1;
     }
 
-    pam_rc = pam_acct_mgmt(handle, 0);
+    if (pam_acct_mgmt(handle, 0) != PAM_SUCCESS)
+    {
+        pam_end(handle, 0);
+        return -1;
+    }
 
     pam_end(handle, 0);
-    return pam_rc;
+
+    return 0;
 }
 
 /*
@@ -447,21 +442,22 @@ static int PAMValidateUserInProcess(const char* username)
     PAMData data;
     struct pam_conv pconv;
     pam_handle_t* phandle;
-    int pam_rc;
 
     pconv.conv = PAMValidateUserCallback;
     pconv.appdata_ptr = &data;
 
-    pam_rc = pam_start(PAM_CONFIG_FILE, username, &pconv, &phandle);
-    if (pam_rc != PAM_SUCCESS)
+    if (pam_start("wbem", username, &pconv, &phandle) != PAM_SUCCESS)
+        return -1;
+
+    if (pam_acct_mgmt(phandle, 0) != PAM_SUCCESS)
     {
-        return pam_rc;
+        pam_end(phandle, 0);
+        return -1;
     }
 
-    pam_rc = pam_acct_mgmt(phandle, 0);
-
     pam_end(phandle, 0);
-    return pam_rc;
+
+    return 0;
 }
 
 /*

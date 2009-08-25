f5935c6322ed2fa2872770e1ba6721ee031f6a35
sahana.prabhakar <sahana.prabhakar>
2009-08-25 05:11:17 +0000

    BUG#: 8553 TITLE: Nightly builds on Linux fail with a core backtrace pointing to getenv. DESCRIPTION: Core is generated during cimserver shutdown. Memory corruption is occuring on sending CIM_STOP_ALL_PROVIDERS_REQUEST_MESSAGE in the ProviderManagerService destructor.

--- src/Pegasus/ProviderManagerService/ProviderManagerService.cpp
+++ src/Pegasus/ProviderManagerService/ProviderManagerService.cpp
@@ -121,11 +121,6 @@ ProviderManagerService::ProviderManagerService(
 
 ProviderManagerService::~ProviderManagerService(void)
 {
-    CIMStopAllProvidersRequestMessage stopAllProvidersRequest(
-        XmlWriter::getNextMessageId(), QueueIdStack(0));
-    Message* stopAllProvidersResponse =
-        _processMessage(&stopAllProvidersRequest);
-    delete stopAllProvidersResponse;
     delete _basicProviderManagerRouter;
     delete _oopProviderManagerRouter;
     providerManagerService=NULL;

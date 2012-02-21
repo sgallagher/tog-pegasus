dad09a770d27da6ad346549e793c7e2fb8b7a4fb
marek <marek>
2012-02-21 11:36:15 +0000

    BUG#:9173 TITLE: keybindings are missing from enumeration operations that use classPath as input
    DESCRIPTION:

--- src/Pegasus/Common/SCMO.cpp
+++ src/Pegasus/Common/SCMO.cpp
@@ -2487,6 +2487,9 @@ void SCMOInstance::buildKeyBindingsFromProperties()
     SCMBValue* theInstPropNodeArray=
         (SCMBValue*)&inst.base[inst.hdr->propertyArray.start];
 
+    inst.hdr->numberKeyBindings =
+        inst.hdr->theClass.ptr->cls.hdr->keyBindingSet.number;
+
     for (Uint32 i = 0, k = inst.hdr->numberKeyBindings; i < k; i++)
     {
         // If the keybinding is not set.
@@ -4332,6 +4335,8 @@ void SCMOInstance::clearKeyBindings()
     // Clear the keybindings after the allocation. Setting the keybindings
     // later causes this value to be reinitialized.
     inst.hdr->numberKeyBindings = 0;
+
+    markAsCompromised();
 }
 
 Uint32 SCMOInstance::getKeyBindingCount() const

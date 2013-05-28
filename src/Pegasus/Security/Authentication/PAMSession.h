c429667e072618e5f0b15286c76f3752739cabc2
marek <marek>
2013-05-28 12:36:50 +0000

    BUG#:9642 TITLE:Remote session tracking through PAM mechanism in a common approach
    DESCRIPTION: Missed adding the new files on first commit. Committing missing files.

---
+++ src/Pegasus/Security/Authentication/PAMSession.h
@@ -0,0 +1,345 @@
+/*
+//%LICENSE////////////////////////////////////////////////////////////////
+//
+// Licensed to The Open Group (TOG) under one or more contributor license
+// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
+// this work for additional information regarding copyright ownership.
+// Each contributor licenses this file to you under the OpenPegasus Open
+// Source License; you may not use this file except in compliance with the
+// License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//////////////////////////////////////////////////////////////////////////
+*/
+
+#ifndef PAMSession_h
+#define PAMSession_h
+
+#include <security/pam_appl.h>
+#include <Pegasus/Security/Authentication/Linkage.h>
+#include <Pegasus/Common/Logger.h>
+#include <Pegasus/Common/AuthenticationInfo.h>
+
+
+#ifdef PEGASUS_FLAVOR
+# define PAM_CONFIG_FILE "wbem" PEGASUS_FLAVOR
+#else
+# define PAM_CONFIG_FILE "wbem"
+#endif
+
+PEGASUS_NAMESPACE_BEGIN
+
+/*
+**==============================================================================
+**
+** struct PAMData
+**
+**     Client data passed to PAM routines.
+**
+**==============================================================================
+*/
+
+typedef struct PAMDataStruct
+{
+    const char* password;
+}
+PAMData;
+
+
+static void _freePAMMessage(int numMsg, struct pam_response** rsp)
+{
+    // rsp is initialized to zero's, can call free() since it does check for
+    // Null Pointer and a failed malloc does not change pointer value
+    for (int i = 0; i < numMsg; i++)
+    {
+        free(rsp[i]->resp);
+    }
+    free(rsp);
+}
+
+
+/*
+**==============================================================================
+**
+** PAMAuthenticateCallback()
+**
+**     Callback used by PAMAuthenticate().
+**
+**==============================================================================
+*/
+
+static int PAMAuthenticateCallback(
+    int num_msg,
+#if defined(PEGASUS_OS_LINUX)
+    const struct pam_message** msg,
+#else
+    struct pam_message** msg,
+#endif
+    struct pam_response** resp,
+    void* appdata_ptr)
+{
+    PAMData* data = (PAMData*)appdata_ptr;
+    int i;
+
+    if (num_msg > 0)
+    {
+        *resp = (struct pam_response*)calloc(
+            num_msg, sizeof(struct pam_response));
+
+        if (*resp == NULL)
+            return PAM_BUF_ERR;
+    }
+    else
+    {
+        return PAM_CONV_ERR;
+    }
+
+    for (i = 0; i < num_msg; i++)
+    {
+        switch (msg[i]->msg_style)
+        {
+            case PAM_PROMPT_ECHO_OFF:
+            {
+                resp[i]->resp = (char*)malloc(PAM_MAX_MSG_SIZE);
+                if (resp[i]->resp == NULL)
+                {
+                    _freePAMMessage(num_msg, resp);
+                    return PAM_BUF_ERR;
+                }
+                strncpy(resp[i]->resp, data->password, PAM_MAX_MSG_SIZE);
+                resp[i]->resp_retcode = 0;
+                break;
+            }
+
+            default:
+            {
+                _freePAMMessage(num_msg, resp);
+                return PAM_CONV_ERR;
+            }
+        }
+    }
+    return PAM_SUCCESS;
+}
+
+
+/*
+**==============================================================================
+**
+** PAMValidateUserCallback()
+**
+**     Callback used by PAMValidateUser().
+**
+**==============================================================================
+*/
+
+static int PAMValidateUserCallback(
+    int num_msg,
+#if defined(PEGASUS_OS_LINUX)
+    const struct pam_message** msg,
+#else
+    struct pam_message** msg,
+#endif
+    struct pam_response** resp,
+    void* appdata_ptr)
+{
+    msg = 0;
+
+    appdata_ptr = 0;
+
+    if (num_msg > 0)
+    {
+        *resp = (struct pam_response*)calloc(
+            num_msg, sizeof(struct pam_response));
+
+        if (*resp == NULL)
+            return PAM_BUF_ERR;
+    }
+    else
+    {
+        return PAM_CONV_ERR;
+    }
+
+    return PAM_SUCCESS;
+}
+
+static void _logPAMError(
+    pam_handle_t* handle,
+    const char* functionName,
+    int pamrc)
+{
+    const char * errorText;
+    if (0 == handle)
+    {
+        errorText = "";
+    }
+    else
+    {
+        errorText = pam_strerror(handle, pamrc);
+    }
+    Logger::put_l(Logger::STANDARD_LOG, System::CIMSERVER, Logger::WARNING,
+        MessageLoaderParms(
+            "Security.Authentication.PAMSession.PAM_FUNCTION_FAILED",
+            "Error in PAM function $0 (errorCode=$1, errorText=\'$2\')",
+            functionName,
+            pamrc,
+            errorText));
+}
+
+static int _preparePAM(
+    bool authenticate,
+    pam_handle_t** handle,
+    PAMData* data,
+    const char* username,
+    const char* password,
+    AuthenticationInfo * authInfo)
+{
+    struct pam_conv pconv;
+    int pam_rc;
+
+    if (authenticate)
+    {
+        pconv.conv = PAMAuthenticateCallback;
+        data->password = password;
+    }
+    else
+    {
+        pconv.conv = PAMValidateUserCallback;
+    }
+    pconv.appdata_ptr = data;
+
+    pam_rc = pam_start(PAM_CONFIG_FILE,username,&pconv,handle);
+
+    if (pam_rc != PAM_SUCCESS)
+    {
+        _logPAMError(0, "pam_start", pam_rc);
+        return pam_rc;
+    }
+
+    CString ipAddress = authInfo->getIpAddress().getCString();
+    
+    PEG_TRACE((TRC_AUTHENTICATION,Tracer::LEVEL3,
+        "Setting PAM_RHOST for user %s to %s",
+        username,
+        (const char*) ipAddress));
+
+    pam_rc = pam_set_item(
+        *handle,
+        PAM_RHOST,
+        (const char*) ipAddress);
+
+    if (pam_rc !=PAM_SUCCESS)
+    {
+        _logPAMError(*handle, "pam_set_item(PAM_RHOST)", pam_rc);
+        pam_end(*handle, 0);
+        return pam_rc;
+    }
+    return PAM_SUCCESS;
+}
+
+/*
+**==============================================================================
+**
+** _PAMAuthenticate()
+**
+**     Performs basic PAM authentication on the given username and password.
+**
+**==============================================================================
+*/
+static int _PAMAuthenticate(
+    const char* username,
+    const char* password,
+    AuthenticationInfo * authInfo)
+{
+    pam_handle_t* handle;
+    int pam_rc;
+    PAMData data;
+
+    pam_rc = _preparePAM(true, &handle, &data, username, password, authInfo);
+    if (pam_rc != PAM_SUCCESS)
+    {
+        return pam_rc;
+    }
+
+    pam_rc = pam_authenticate(handle, 0);
+    if (pam_rc != PAM_SUCCESS)
+    {
+        _logPAMError(handle, "pam_authenticate", pam_rc);
+        pam_end(handle, 0);
+        return pam_rc;
+    }
+
+    pam_rc = pam_acct_mgmt(handle, 0);
+    if (pam_rc != PAM_SUCCESS)
+    {
+        _logPAMError(handle, "pam_acct_mgmt", pam_rc);
+        pam_end(handle, 0);
+        return pam_rc;
+    }
+
+    // Delayed on success: -> pam_end(handle, 0);
+    // Cleanup will happen through destroy() function of AuthHandle in
+    // HTTPConnection
+    AuthHandle myAuth;
+    myAuth.hdl = handle;
+    authInfo->setAuthHandle(myAuth);
+
+    return pam_rc;
+}
+
+/*
+**==============================================================================
+**
+** _PAMValidateUser()
+**
+**     Validate that the *username* refers to a valid PAM user.
+**
+**==============================================================================
+*/
+
+static int _PAMValidateUser(
+    const char* username,
+    AuthenticationInfo * authInfo)
+{
+    pam_handle_t* handle;
+    PAMData data;
+    int pam_rc;
+
+    pam_rc = _preparePAM(false, &handle, &data, username, 0, authInfo);
+    if (pam_rc != PAM_SUCCESS)
+    {
+        return pam_rc;
+    }
+
+    pam_rc = pam_acct_mgmt(handle, 0);
+    if (pam_rc != PAM_SUCCESS)
+    {
+        _logPAMError(handle, "pam_acct_mgmt", pam_rc);
+        pam_end(handle, 0);
+        return pam_rc;
+    }
+
+    pam_end(handle, 0);
+
+    return pam_rc;
+}
+
+PEGASUS_NAMESPACE_END
+
+#endif /* PAMSession_h */
---
+++ src/Pegasus/Security/Authentication/PAMSessionBasicAuthenticator.cpp
@@ -0,0 +1,120 @@
+//%LICENSE////////////////////////////////////////////////////////////////
+//
+// Licensed to The Open Group (TOG) under one or more contributor license
+// agreements.  Refer to the OpenPegasusNOTICE.txt file distributed with
+// this work for additional information regarding copyright ownership.
+// Each contributor licenses this file to you under the OpenPegasus Open
+// Source License; you may not use this file except in compliance with the
+// License.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//////////////////////////////////////////////////////////////////////////
+//
+//%/////////////////////////////////////////////////////////////////////////////
+
+#include <Pegasus/Common/Tracer.h>
+#include "PAMSessionBasicAuthenticator.h"
+#include "PAMSession.h"
+
+PEGASUS_USING_STD;
+
+PEGASUS_NAMESPACE_BEGIN
+
+PAMSessionBasicAuthenticator::PAMSessionBasicAuthenticator()
+{
+    PEG_METHOD_ENTER(TRC_AUTHENTICATION,
+        "PAMSessionBasicAuthenticator::PAMSessionBasicAuthenticator()");
+
+    // Build Authentication parameter realm required for Basic Challenge
+    // e.g. realm="HostName"
+
+    _realm.assign("realm=");
+    _realm.append(Char16('"'));
+    _realm.append(System::getHostName());
+    _realm.append(Char16('"'));
+
+    PEG_METHOD_EXIT();
+}
+
+PAMSessionBasicAuthenticator::~PAMSessionBasicAuthenticator()
+{
+    PEG_METHOD_ENTER(TRC_AUTHENTICATION,
+        "PAMSessionBasicAuthenticator::~PAMSessionBasicAuthenticator()");
+
+    PEG_METHOD_EXIT();
+}
+
+Boolean PAMSessionBasicAuthenticator::authenticate(
+    const String& userName,
+    const String& password,
+    AuthenticationInfo* authInfo)
+{
+
+    PEG_METHOD_ENTER(TRC_AUTHENTICATION,
+        "PAMSessionBasicAuthenticator::authenticate()");
+
+    if (PAM_SUCCESS != _PAMAuthenticate(
+        userName.getCString(),
+        password.getCString(),
+        authInfo))
+    {
+        PEG_METHOD_EXIT();
+        return false;
+    }
+    
+    PEG_METHOD_EXIT();
+    return true;
+}
+
+Boolean PAMSessionBasicAuthenticator::validateUser(
+    const String& userName,
+    AuthenticationInfo* authInfo)
+{
+    PEG_METHOD_ENTER(TRC_AUTHENTICATION,
+        "PAMSessionBasicAuthenticator::validateUser()");
+
+    if (PAM_SUCCESS != _PAMValidateUser(
+        userName.getCString(),
+        authInfo))
+    {
+        PEG_METHOD_EXIT();
+        return false;
+    }
+
+    PEG_METHOD_EXIT();
+    return true;
+}
+
+
+String PAMSessionBasicAuthenticator::getAuthResponseHeader()
+{
+    PEG_METHOD_ENTER(TRC_AUTHENTICATION,
+        "PAMSessionBasicAuthenticator::getAuthResponseHeader()");
+
+    // Build response header: WWW-Authenticate: Basic realm="HostName"
+
+    String responseHeader = "WWW-Authenticate: Basic ";
+    responseHeader.append(_realm);
+
+    PEG_METHOD_EXIT();
+    return responseHeader;
+}
+
+PEGASUS_NAMESPACE_END
